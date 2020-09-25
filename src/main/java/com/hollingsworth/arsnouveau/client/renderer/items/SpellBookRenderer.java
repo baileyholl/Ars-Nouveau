fd38b730193a6a7325d67ce49fccfdda6fc6213d
Bailey Hollingsworth <Bailey.A.Hollingsworth-1@ou.edu>
2020-09-24 21:22:10 -0500

    Final book models and textures

--- src/main/java/com/hollingsworth/arsnouveau/client/renderer/items/SpellBookRenderer.java
+++ src/main/java/com/hollingsworth/arsnouveau/client/renderer/items/SpellBookRenderer.java
@@ -2,6 +2,8 @@ package com.hollingsworth.arsnouveau.client.renderer.items;
 
 import com.hollingsworth.arsnouveau.ArsNouveau;
 import com.hollingsworth.arsnouveau.client.renderer.tile.RelaySplitterModel;
+import com.hollingsworth.arsnouveau.common.items.ModItem;
+import com.hollingsworth.arsnouveau.setup.ItemsRegistry;
 import com.mojang.blaze3d.matrix.MatrixStack;
 import com.mojang.blaze3d.vertex.IVertexBuilder;
 import net.minecraft.client.renderer.IRenderTypeBuffer;
@@ -12,16 +14,20 @@ import net.minecraft.util.ResourceLocation;
 
 public class SpellBookRenderer extends ItemStackTileEntityRenderer {
     public final SpellBookModel model = new SpellBookModel();
-    public static final ResourceLocation texture = new ResourceLocation(ArsNouveau.MODID + ":textures/entity/spellbook_archmage_final.png");
+    public static final ResourceLocation arch = new ResourceLocation(ArsNouveau.MODID + ":textures/entity/spellbook_archmage_final.png");
+    public static final ResourceLocation apprentice = new ResourceLocation(ArsNouveau.MODID + ":textures/entity/spellbook_mage_final.png");
+    public static final ResourceLocation novice = new ResourceLocation(ArsNouveau.MODID + ":textures/entity/spellbook_novice_final.png");
+
     public SpellBookRenderer(){ }
 
     @Override
-    public void render(ItemStack p_228364_1_, MatrixStack ms, IRenderTypeBuffer buffers, int light, int overlay) {
+    public void render(ItemStack stack, MatrixStack ms, IRenderTypeBuffer buffers, int light, int overlay) {
         ms.push();
         ms.translate(0.5, 1, 1.0);
         ms.scale(0.5f, 0.5f, 0.5f);
         ms.rotate(Vector3f.XP.rotation(180));
-        IVertexBuilder buffer = buffers.getBuffer(model.getRenderType(texture));
+        ResourceLocation location = stack.getItem() == ItemsRegistry.noviceSpellBook ? novice : stack.getItem() == ItemsRegistry.apprenticeSpellBook ? apprentice : arch;
+        IVertexBuilder buffer = buffers.getBuffer(model.getRenderType(location));
         model.render(ms, buffer, light, overlay, 1, 1, 1, 1, 1);
 
         ms.pop();
