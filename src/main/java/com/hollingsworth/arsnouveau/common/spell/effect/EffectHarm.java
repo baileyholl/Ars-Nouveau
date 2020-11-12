6f5d63f86854a35295111446655ba13ee522916b
Bailey Hollingsworth <Bailey.A.Hollingsworth-1@ou.edu>
2020-11-11 20:55:19 -0600

    Fixes item duping with harm

--- src/main/java/com/hollingsworth/arsnouveau/common/spell/effect/EffectHarm.java
+++ src/main/java/com/hollingsworth/arsnouveau/common/spell/effect/EffectHarm.java
@@ -8,7 +8,6 @@ import com.hollingsworth.arsnouveau.api.util.LootUtil;
 import com.hollingsworth.arsnouveau.common.spell.augment.AugmentFortune;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.MobEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -22,6 +21,7 @@ import net.minecraft.util.math.EntityRayTraceResult;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.World;
 import net.minecraft.world.server.ServerWorld;
+
 import javax.annotation.Nullable;
 import java.util.List;
 
@@ -34,19 +34,18 @@ public class EffectHarm extends AbstractEffect {
         if(rayTraceResult instanceof EntityRayTraceResult){
             float damage = 5.0f +3.0f * getAmplificationBonus(augments);
             Entity entity = ((EntityRayTraceResult) rayTraceResult).getEntity();
-            if(entity instanceof MobEntity){
-                MobEntity mob = (MobEntity) entity;
-                if(mob.getHealth() <= damage && mob.getHealth() > 0 && hasBuff(augments, AugmentFortune.class)){
+            if(entity instanceof LivingEntity){
+                LivingEntity mob = (LivingEntity) entity;
+                entity.attackEntityFrom(DamageSource.causePlayerDamage((PlayerEntity) shooter), damage);
+                if(mob.getHealth() <= 0 && !mob.removed && hasBuff(augments, AugmentFortune.class)){
                     int looting = getBuffCount(augments, AugmentFortune.class);
                     LootContext.Builder lootContext = LootUtil.getLootingContext((ServerWorld)world,shooter, mob, looting, DamageSource.causePlayerDamage((PlayerEntity) shooter));
                     ResourceLocation lootTable = mob.getLootTableResourceLocation();
                     LootTable loottable = world.getServer().getLootTableManager().getLootTableFromLocation(lootTable);
                     List<ItemStack> items = loottable.generate(lootContext.build(LootParameterSets.GENERIC));
                     items.forEach(mob::entityDropItem);
-
                 }
             }
-            entity.attackEntityFrom(DamageSource.causePlayerDamage((PlayerEntity) shooter), damage);
 
         }
     }
