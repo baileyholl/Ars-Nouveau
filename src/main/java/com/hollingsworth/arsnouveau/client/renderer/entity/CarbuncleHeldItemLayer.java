9eacf5d775ea3cf51bb599aea997dcda69536e4c
Bailey Hollingsworth <Bailey.A.Hollingsworth-1@ou.edu>
2020-11-13 15:46:01 -0600

    Adds rotations

--- src/main/java/com/hollingsworth/arsnouveau/client/renderer/entity/CarbuncleHeldItemLayer.java
+++ src/main/java/com/hollingsworth/arsnouveau/client/renderer/entity/CarbuncleHeldItemLayer.java
@@ -10,16 +10,15 @@ import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.vector.Quaternion;
 import net.minecraft.util.math.vector.Vector3f;
-import net.minecraftforge.client.model.geometry.IModelGeometryPart;
 import software.bernie.geckolib3.core.processor.IBone;
 import software.bernie.geckolib3.renderers.geo.GeoLayerRenderer;
 import software.bernie.geckolib3.renderers.geo.IGeoRenderer;
 
 public class CarbuncleHeldItemLayer extends GeoLayerRenderer<EntityCarbuncle> {
-    public CarbuncleRenderer rend;
+
     public CarbuncleHeldItemLayer(IGeoRenderer<EntityCarbuncle> entityRendererIn) {
         super(entityRendererIn);
-        rend = entityRendererIn;
+
     }
 
     @Override
@@ -31,31 +30,18 @@ public class CarbuncleHeldItemLayer extends GeoLayerRenderer<EntityCarbuncle> {
         IBone tail = model.getBone("tail");
         IBone carbuncle = ((CarbuncleModel)getEntityModel()).getBone("carbuncle");
 
-        matrixStackIn.translate((tail.getPositionX())/64f, -(carbuncle.getPositionY())/10f ,
+        matrixStackIn.translate((tail.getPositionX())/64f, -(tail.getPositionY())/64f ,
                 (tail.getPositionZ())/64f);
 
-//        matrixStackIn.rotate(Vector3f.XP.rotation((float) Math.cos(this.getEntityModel().carbuncle.positionOffsetY)));
-//        matrixStackIn.rotate(Vector3f.XP.rotationDegrees(this.getEntityModel().carbuncle.rotateAngleX));
-//        matrixStackIn.rotate(Vector3f.ZP.rotationDegrees(this.getEntityModel().carbuncle.rotateAngleZ));
-//        matrixStackIn.rotate(Vector3f.XP.rotationDegrees(headPitch));
-
-        matrixStackIn.translate((double)0f, (double)1.0f, .2D);
+        matrixStackIn.translate((double)0f, (double)0.8f, .2D);
         matrixStackIn.scale(0.75f, 0.75f, 0.75f);
 
-//        matrixStackIn.rotate(Vector3f.XP.rotationDegrees(this.getEntityModel().carbuncle.rotateAngleX));
-//        System.out.println(this.getEntityModel().carbuncle.rotateAngleX*180f);
         Quaternion quaternion = Vector3f.XP.rotationDegrees(carbuncle.getRotationX()  +180f );
-//        quaternion.conjugate();
+
         matrixStackIn.rotate(quaternion);
         matrixStackIn.rotate(Vector3f.YP.rotationDegrees(180f));
-//        matrixStackIn.rotate(Vector3f.XP.rotationDegrees(this.getEntityModel().carbuncle.rotateAngleX));
-//        matrixStackIn.rotate(Vector3f.YP.rotationDegrees( (float)Math.cos(this.getEntityModel().carbuncle.rotateAngleY) *360f));
-//        matrixStackIn.rotate(Vector3f.ZP.rotationDegrees( (float)Math.cos(this.getEntityModel().carbuncle.rotateAngleZ)));
-//
 
-//        ItemStack itemstack = entitylivingbaseIn.getItemStackFromSlot(EquipmentSlotType.MAINHAND);
         ItemStack itemstack = entitylivingbaseIn.getHeldStack();
-//        Minecraft.getInstance().getFirstPersonRenderer().renderItemSide(entitylivingbaseIn, itemstack, ItemCameraTransforms.TransformType.GROUND, false, matrixStackIn, bufferIn, packedLightIn);
         Minecraft.getInstance().getItemRenderer().renderItem(itemstack, ItemCameraTransforms.TransformType.GROUND, packedLightIn, OverlayTexture.NO_OVERLAY , matrixStackIn, bufferIn);
         matrixStackIn.pop();
     }
--- src/main/java/com/hollingsworth/arsnouveau/client/renderer/entity/SylphModel.java
+++ src/main/java/com/hollingsworth/arsnouveau/client/renderer/entity/SylphModel.java
@@ -3,8 +3,24 @@ package com.hollingsworth.arsnouveau.client.renderer.entity;
 import com.hollingsworth.arsnouveau.ArsNouveau;
 import com.hollingsworth.arsnouveau.common.entity.EntitySylph;
 import net.minecraft.util.ResourceLocation;
+import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
+import software.bernie.geckolib3.core.processor.IBone;
 import software.bernie.geckolib3.model.AnimatedGeoModel;
+import software.bernie.geckolib3.model.provider.data.EntityModelData;
+
+import javax.annotation.Nullable;
+
 public class SylphModel extends AnimatedGeoModel<EntitySylph> {
+
+
+    @Override
+    public void setLivingAnimations(EntitySylph entity, Integer uniqueID, @Nullable AnimationEvent customPredicate) {
+        super.setLivingAnimations(entity, uniqueID, customPredicate);
+        IBone head = this.getAnimationProcessor().getBone("head");
+        EntityModelData extraData = (EntityModelData) customPredicate.getExtraDataOfType(EntityModelData.class).get(0);
+        head.setRotationX(extraData.headPitch * 0.010453292F);
+        head.setRotationY(extraData.netHeadYaw * 0.015453292F);
+    }
 
     @Override
     public ResourceLocation getModelLocation(EntitySylph entitySylph) {
