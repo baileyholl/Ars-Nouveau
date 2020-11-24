92423a94e9a49ad6bfaf41ca606bf4837c2cf98e
Bailey Hollingsworth <Bailey.A.Hollingsworth-1@ou.edu>
2020-11-24 09:18:53 -0600

    Adds pagination to the effects list

--- src/main/java/com/hollingsworth/arsnouveau/client/gui/book/GuiSpellBook.java
+++ src/main/java/com/hollingsworth/arsnouveau/client/gui/book/GuiSpellBook.java
@@ -1,6 +1,7 @@
 package com.hollingsworth.arsnouveau.client.gui.book;
 
 import com.hollingsworth.arsnouveau.api.ArsNouveauAPI;
+import com.hollingsworth.arsnouveau.api.spell.AbstractAugment;
 import com.hollingsworth.arsnouveau.api.spell.AbstractCastMethod;
 import com.hollingsworth.arsnouveau.api.spell.AbstractEffect;
 import com.hollingsworth.arsnouveau.api.spell.AbstractSpellPart;
@@ -13,11 +14,14 @@ import com.hollingsworth.arsnouveau.client.particle.ParticleColor;
 import com.hollingsworth.arsnouveau.common.items.SpellBook;
 import com.hollingsworth.arsnouveau.common.network.Networking;
 import com.hollingsworth.arsnouveau.common.network.PacketUpdateSpellbook;
+import com.hollingsworth.arsnouveau.setup.Config;
 import com.hollingsworth.arsnouveau.setup.ItemsRegistry;
 import com.mojang.blaze3d.matrix.MatrixStack;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.widget.TextFieldWidget;
+import net.minecraft.client.gui.widget.Widget;
 import net.minecraft.client.gui.widget.button.Button;
+import net.minecraft.client.gui.widget.button.ChangePageButton;
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.util.text.StringTextComponent;
@@ -26,6 +30,7 @@ import vazkii.patchouli.api.PatchouliAPI;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class GuiSpellBook extends BaseBook {
 
@@ -39,8 +44,12 @@ public class GuiSpellBook extends BaseBook {
     public GuiSpellSlot selected_slot;
     public int max_spell_tier; // Used to load spells that are appropriate tier
     List<CraftingButton> craftingCells;
-
-    public ArrayList<AbstractSpellPart>unlockedSpells;
+    public List<AbstractSpellPart>unlockedSpells;
+    public List<AbstractSpellPart> effects;
+    public List<Widget> effectButtons;
+    public int page = 0;
+    ChangePageButton nextButton;
+    ChangePageButton previousButton;
     public GuiSpellBook(ArsNouveauAPI api, CompoundNBT tag, int tier, String unlockedSpells) {
         super();
         this.api = api;
@@ -49,6 +58,8 @@ public class GuiSpellBook extends BaseBook {
         this.max_spell_tier = tier;
         this.spell_book_tag = tag;
         this.unlockedSpells = SpellRecipeUtil.getSpellsFromString(unlockedSpells);
+        this.effects = this.unlockedSpells.stream().filter(a -> a instanceof AbstractEffect).collect(Collectors.toList());
+        effectButtons = new ArrayList<>();
     }
 
     @Override
@@ -69,8 +80,9 @@ public class GuiSpellBook extends BaseBook {
         }
         updateCraftingSlots(selected_slot_ind);
 
-        addSpellParts();
+        addSpellParts(0);
         addButton(new GuiImageButton(bookRight - 70, bookBottom - 28, 0,0,46, 18, 46, 18, "textures/gui/create_button.png", this::onCreateClick));
+
         spell_name = new TextFieldWidget(minecraft.fontRenderer, bookLeft + 16, bookTop + FULL_HEIGHT - 25,
                 115, 12, null, new StringTextComponent("Spell Name"));
         int mode = SpellBook.getMode(spell_book_tag);
@@ -93,19 +105,38 @@ public class GuiSpellBook extends BaseBook {
 
         addButton(new GuiImageButton(bookLeft - 15, bookTop, 0, 0, 16, 16, 16,16, "textures/items/worn_notebook.png",this::onDocumentationClick));
         addButton(new GuiImageButton(bookLeft - 15, bookTop + 15, 0, 0, 16, 16, 16,16, "textures/gui/color_wheel.png",this::onColorClick));
-
+        this.nextButton = addButton(new ChangePageButton(bookRight -20, bookBottom -10, true, this::onPageIncrease, true));
+        this.previousButton = addButton(new ChangePageButton(bookLeft - 5 , bookBottom -10, false, this::onPageDec, true));
+        System.out.println(effects.size());
+        if(effects.size() < 36){
+            nextButton.visible = false;
+            nextButton.active = false;
+        }else{
+            nextButton.visible = true;
+            nextButton.active = true;
+        }
+        previousButton.active = false;
+        previousButton.visible = false;
     }
 
-    public void addSpellParts(){
+    public void addSpellParts(int page){
+        System.out.println("adding");
+        for(Widget w : effectButtons) {
+            buttons.remove(w);
+            children.remove(w);
+        }
+        effectButtons.clear();
         Collections.sort(unlockedSpells);
 
+        List<AbstractSpellPart> displayedEffects = effects.subList(36 * page, Math.min(effects.size(), 36 * (page + 1)));
+        System.out.println(effects.size());
         //Adding spell parts
         int numCast = 0;
         int numEffect = 0;
         int numAugment = 0;
         for(AbstractSpellPart key  : unlockedSpells){
             AbstractSpellPart spell = this.api.getSpell_map().get(key.tag);
-            GlyphButton cell;
+            GlyphButton cell = null;
             if(spell.getTier().ordinal() > max_spell_tier)
                 continue; //Skip spells too high of a tier
 
@@ -114,24 +145,56 @@ public class GuiSpellBook extends BaseBook {
                 int yOffset = (numCast / 6) * 20 ;
                 cell = new GlyphButton(this, bookLeft + 15 + xOffset, bookTop + 20 + yOffset, false, spell.getIcon(), spell.tag);
                 numCast++;
-            }else if(spell instanceof AbstractEffect){
-                int xOffset = 20 * (numEffect % 6 );
-                int yOffset = (numEffect / 6) * 18 ;
-                cell = new GlyphButton(this, bookLeft + 140 + xOffset, bookTop + 20 +  yOffset, false, spell.getIcon(), spell.tag);
-                numEffect ++;
-            }else{
+            }else if(spell instanceof AbstractAugment){
                 int xOffset = 20 * (numAugment % 6 );
                 int yOffset = (numAugment / 6) * 20 ;
                 cell = new GlyphButton(this, bookLeft + 15 + xOffset, bookTop + 70 +  yOffset, false, spell.getIcon(), spell.tag);
                 numAugment++;
+            }else{
+                continue;
             }
             addButton(cell);
         }
+        for(AbstractSpellPart s : displayedEffects){
+            AbstractEffect spell = (AbstractEffect)s;
+            if(!Config.enabledSpells.get(spell.tag).get())
+                continue;
+            GlyphButton cell;
+            int xOffset = 20 * (numEffect % 6 );
+            int yOffset = (numEffect / 6) * 18 ;
+            cell = new GlyphButton(this, bookLeft + 140 + xOffset, bookTop + 20 +  yOffset, false, spell.getIcon(), spell.tag);
+            numEffect ++;
+            effectButtons.add(addButton(cell));
+        }
+    }
+
+    public void onPageIncrease(Button button){
+        page++;
+        if(effects.size() < 36 * (page + 1)){
+            nextButton.visible = false;
+            nextButton.active = false;
+        }
+        previousButton.active = true;
+        previousButton.visible = true;
+        addSpellParts(page);
+    }
+
+    public void onPageDec(Button button){
+        page--;
+        if(page == 0){
+            previousButton.active = false;
+            previousButton.visible = false;
+        }
+
+        if(effects.size() > 36 * (page + 1)){
+            nextButton.visible = true;
+            nextButton.active = true;
+        }
+        addSpellParts(page);
     }
 
     public void onDocumentationClick(Button button){
         PatchouliAPI.instance.openBookGUI(Registry.ITEM.getKey(ItemsRegistry.wornNotebook));
-
     }
 
     public void onColorClick(Button button){
