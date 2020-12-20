ba15a54b20731c6f2aa55df896e0324bc8eb2e10
Bailey Hollingsworth <Bailey.A.Hollingsworth-1@ou.edu>
2020-12-20 16:46:14 -0600

    Conjure water now extinguishes fire on an entity

--- src/main/java/com/hollingsworth/arsnouveau/common/spell/effect/EffectConjureWater.java
+++ src/main/java/com/hollingsworth/arsnouveau/common/spell/effect/EffectConjureWater.java
@@ -7,12 +7,14 @@ import com.hollingsworth.arsnouveau.api.spell.SpellContext;
 import com.hollingsworth.arsnouveau.api.util.SpellUtil;
 import com.hollingsworth.arsnouveau.common.spell.augment.AugmentAOE;
 import net.minecraft.block.Blocks;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.fluid.Fluids;
 import net.minecraft.item.Item;
 import net.minecraft.item.Items;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
+import net.minecraft.util.math.EntityRayTraceResult;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.World;
 
@@ -26,10 +28,15 @@ public class EffectConjureWater extends AbstractEffect {
 
     @Override
     public void onResolve(RayTraceResult rayTraceResult, World world, @Nullable LivingEntity shooter, List<AbstractAugment> augments, SpellContext spellContext) {
+        if(rayTraceResult instanceof EntityRayTraceResult){
+            Entity entity = ((EntityRayTraceResult) rayTraceResult).getEntity();
+            if(entity.isBurning()){
+                entity.extinguish();
+            }
+        }
+
         if(!(rayTraceResult instanceof BlockRayTraceResult))
             return;
-
-
         int aoeBuff = getBuffCount(augments, AugmentAOE.class);
         List<BlockPos> posList = SpellUtil.calcAOEBlocks(shooter, ((BlockRayTraceResult) rayTraceResult).getPos(), (BlockRayTraceResult)rayTraceResult,1 + aoeBuff, 1 + aoeBuff, 1, -1);
         BlockRayTraceResult result = (BlockRayTraceResult) rayTraceResult;
