18566f5538f27a7045d8d770b48ba14c5a072d7f
Bailey Hollingsworth <Bailey.A.Hollingsworth-1@ou.edu>
2021-01-13 18:36:03 -0600

    Adds information to ICaster

--- src/main/java/com/hollingsworth/arsnouveau/common/items/Wand.java
+++ src/main/java/com/hollingsworth/arsnouveau/common/items/Wand.java
@@ -1,21 +1,34 @@
 package com.hollingsworth.arsnouveau.common.items;
 
 import com.hollingsworth.arsnouveau.ArsNouveau;
-import com.hollingsworth.arsnouveau.api.item.ICaster;
-import com.hollingsworth.arsnouveau.api.spell.AbstractCastMethod;
-import com.hollingsworth.arsnouveau.api.spell.AbstractSpellPart;
-import com.hollingsworth.arsnouveau.api.spell.ISpellCaster;
-import com.hollingsworth.arsnouveau.api.spell.Spell;
+import com.hollingsworth.arsnouveau.api.item.ICasterTool;
+import com.hollingsworth.arsnouveau.api.spell.*;
+import com.hollingsworth.arsnouveau.api.util.MathUtil;
 import com.hollingsworth.arsnouveau.client.renderer.item.WandRenderer;
+import com.hollingsworth.arsnouveau.common.block.tile.IntangibleAirTile;
+import com.hollingsworth.arsnouveau.common.block.tile.PhantomBlockTile;
+import com.hollingsworth.arsnouveau.common.block.tile.ScribesTile;
 import com.hollingsworth.arsnouveau.common.lib.LibItemNames;
 import com.hollingsworth.arsnouveau.common.spell.augment.AugmentAccelerate;
 import com.hollingsworth.arsnouveau.common.spell.method.MethodProjectile;
 import com.hollingsworth.arsnouveau.common.util.PortUtil;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemUseContext;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Hand;
+import net.minecraft.util.Util;
+import net.minecraft.util.math.BlockRayTraceResult;
+import net.minecraft.util.math.EntityRayTraceResult;
+import net.minecraft.util.math.RayTraceResult;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraft.world.World;
 import software.bernie.geckolib3.core.IAnimatable;
 import software.bernie.geckolib3.core.PlayState;
 import software.bernie.geckolib3.core.builder.AnimationBuilder;
@@ -24,9 +37,11 @@ import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
 import software.bernie.geckolib3.core.manager.AnimationData;
 import software.bernie.geckolib3.core.manager.AnimationFactory;
 
+import javax.annotation.Nullable;
 import java.util.ArrayList;
+import java.util.List;
 
-public class Wand extends ModItem  implements IAnimatable, ICaster {
+public class Wand extends ModItem  implements IAnimatable, ICasterTool {
     public AnimationFactory factory = new AnimationFactory(this);
     public Wand() {
         super(new Item.Properties().maxStackSize(1).group(ArsNouveau.itemGroup).setISTER(() -> WandRenderer::new), LibItemNames.WAND);
@@ -37,6 +52,47 @@ public class Wand extends ModItem  implements IAnimatable, ICaster {
     {
         event.getController().setAnimation(new AnimationBuilder().addAnimation("wand_gem_spin", true));
         return PlayState.CONTINUE;
+    }
+
+    @Override
+    public ActionResult<ItemStack> onItemRightClick(World worldIn, PlayerEntity playerIn, Hand handIn) {
+        ItemStack stack = playerIn.getHeldItem(handIn);
+        ISpellCaster caster = getSpellCaster(stack);
+
+        if(caster == null || worldIn.isRemote)
+            return super.onItemRightClick(worldIn, playerIn, handIn);
+
+        RayTraceResult result = playerIn.pick(5, 0, false);
+        if(result instanceof BlockRayTraceResult && worldIn.getTileEntity(((BlockRayTraceResult) result).getPos()) instanceof ScribesTile)
+            return new ActionResult<>(ActionResultType.SUCCESS, stack);
+        if(result instanceof BlockRayTraceResult && !playerIn.isSneaking()){
+            if(worldIn.getTileEntity(((BlockRayTraceResult) result).getPos()) != null &&
+                    !(worldIn.getTileEntity(((BlockRayTraceResult) result).getPos()) instanceof IntangibleAirTile
+                            ||(worldIn.getTileEntity(((BlockRayTraceResult) result).getPos()) instanceof PhantomBlockTile))) {
+                return new ActionResult<>(ActionResultType.SUCCESS, stack);
+            }
+        }
+
+        if(caster.getSpell() == null) {
+            playerIn.sendMessage(new StringTextComponent("Invalid Spell."), Util.DUMMY_UUID);
+            return new ActionResult<>(ActionResultType.CONSUME, stack);
+        }
+        SpellResolver resolver = new SpellResolver(caster.getSpell().recipe, new SpellContext(caster.getSpell(), playerIn));
+        EntityRayTraceResult entityRes = MathUtil.getLookedAtEntity(playerIn, 25);
+
+        if(entityRes != null && entityRes.getEntity() instanceof LivingEntity){
+            resolver.onCastOnEntity(stack, playerIn, (LivingEntity) entityRes.getEntity(), handIn);
+            return new ActionResult<>(ActionResultType.CONSUME, stack);
+        }
+
+        if(result instanceof BlockRayTraceResult){
+            ItemUseContext context = new ItemUseContext(playerIn, handIn, (BlockRayTraceResult) result);
+            resolver.onCastOnBlock(context);
+            return new ActionResult<>(ActionResultType.CONSUME, stack);
+        }
+
+        resolver.onCast(stack,playerIn,worldIn);
+        return new ActionResult<>(ActionResultType.CONSUME, stack);
     }
 
     @Override
@@ -68,6 +124,12 @@ public class Wand extends ModItem  implements IAnimatable, ICaster {
         recipe.add(new AugmentAccelerate());
         recipe.addAll(spell.recipe);
         spell.recipe = recipe;
-        return ICaster.super.setSpell(caster, player, hand, stack, spell);
+        return ICasterTool.super.setSpell(caster, player, hand, stack, spell);
+    }
+
+    @Override
+    public void addInformation(ItemStack stack, @Nullable World worldIn, List<ITextComponent> tooltip2, ITooltipFlag flagIn) {
+        getInformation(stack, worldIn, tooltip2, flagIn);
+        super.addInformation(stack, worldIn, tooltip2, flagIn);
     }
 }
