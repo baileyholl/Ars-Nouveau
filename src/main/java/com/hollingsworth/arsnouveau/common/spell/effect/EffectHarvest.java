934c953da79af249cddea78b7e7b39c101ec8bb7
Bailey Hollingsworth <Bailey.A.Hollingsworth-1@ou.edu>
2021-01-15 20:58:50 -0600

    Adds Fell

--- src/main/java/com/hollingsworth/arsnouveau/common/spell/effect/EffectHarvest.java
+++ src/main/java/com/hollingsworth/arsnouveau/common/spell/effect/EffectHarvest.java
@@ -1,26 +1,23 @@
 package com.hollingsworth.arsnouveau.common.spell.effect;
 
-import com.hollingsworth.arsnouveau.ArsNouveau;
 import com.hollingsworth.arsnouveau.ModConfig;
 import com.hollingsworth.arsnouveau.api.spell.AbstractAugment;
 import com.hollingsworth.arsnouveau.api.spell.AbstractEffect;
 import com.hollingsworth.arsnouveau.api.spell.SpellContext;
-import com.hollingsworth.arsnouveau.api.util.BlockUtil;
 import com.hollingsworth.arsnouveau.api.util.LootUtil;
 import com.hollingsworth.arsnouveau.api.util.SpellUtil;
-import com.hollingsworth.arsnouveau.setup.BlockRegistry;
 import com.hollingsworth.arsnouveau.common.spell.augment.AugmentAOE;
-import com.hollingsworth.arsnouveau.common.spell.augment.AugmentExtract;
 import com.hollingsworth.arsnouveau.common.spell.augment.AugmentFortune;
-import net.minecraft.block.*;
+import com.hollingsworth.arsnouveau.setup.BlockRegistry;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.CropsBlock;
+import net.minecraft.block.FarmlandBlock;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.item.ItemEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
-import net.minecraft.tags.BlockTags;
-import net.minecraft.tags.ITag;
-import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.util.math.RayTraceResult;
@@ -28,11 +25,9 @@ import net.minecraft.world.World;
 import net.minecraft.world.server.ServerWorld;
 
 import javax.annotation.Nullable;
-import java.util.*;
+import java.util.List;
 
 public class EffectHarvest extends AbstractEffect {
-
-    public static ITag.INamedTag<Block> FELLABLE =  BlockTags.createOptional(new ResourceLocation(ArsNouveau.MODID, "harvest/fellable"));
 
     public EffectHarvest() {
         super(ModConfig.EffectHarvestID, "Harvest");
@@ -46,24 +41,6 @@ public class EffectHarvest extends AbstractEffect {
                 return;
             for(BlockPos blockpos : SpellUtil.calcAOEBlocks(shooter, ray.getPos(), ray, getBuffCount(augments, AugmentAOE.class))){
                 BlockState state = world.getBlockState(blockpos);
-                if(isTree(state)){
-                    Set<BlockPos> list = getTree(world, ray.getPos(), 500);
-                    list.forEach(listPos -> {
-                        if(!BlockUtil.destroyRespectsClaim(shooter, world, listPos))
-                            return;
-                        if (hasBuff(augments, AugmentExtract.class)) {
-                            world.getBlockState(listPos).getDrops(LootUtil.getSilkContext((ServerWorld) world, listPos,  shooter)).forEach(i -> world.addEntity(new ItemEntity(world,listPos.getX(), listPos.getY(), listPos.getZ(), i )));
-                            BlockUtil.destroyBlockSafelyWithoutSound(world, listPos, false);
-                        } else if (hasBuff(augments, AugmentFortune.class)) {
-                            world.getBlockState(listPos).getDrops(LootUtil.getFortuneContext((ServerWorld) world, listPos, shooter, getBuffCount(augments, AugmentFortune.class))).forEach(i -> world.addEntity(new ItemEntity(world,listPos.getX(), listPos.getY(), listPos.getZ(),i )));
-                            BlockUtil.destroyBlockSafelyWithoutSound(world, listPos, false);
-                        } else {
-                            BlockUtil.destroyBlockSafelyWithoutSound(world, listPos, true);
-                        }
-                    });
-                    world.playEvent(2001, blockpos, Block.getStateId(state));
-                    return;
-                }
 
                 if(state.getBlock() instanceof FarmlandBlock || world.getBlockState(blockpos.up()).getBlock() instanceof CropsBlock){
                     blockpos = blockpos.up();
@@ -101,8 +78,7 @@ public class EffectHarvest extends AbstractEffect {
 
         BlockPos pos = ((BlockRayTraceResult) rayTraceResult).getPos();
         BlockState state = world.getBlockState(pos);
-        if(isTree(state))
-            return true;
+
         if(state.getBlock() instanceof FarmlandBlock || world.getBlockState(pos.up()).getBlock() instanceof CropsBlock){
             pos = pos.up();
             state = world.getBlockState(pos);
@@ -115,48 +91,20 @@ public class EffectHarvest extends AbstractEffect {
         return cropsBlock.isMaxAge(state) && world instanceof ServerWorld;
     }
 
-    public boolean isTree(BlockState blockstate){
-        return blockstate.getBlock().isIn(FELLABLE);
-    }
-
-    public Set<BlockPos> getTree(World world, BlockPos start, int maxBlocks) {
-        return getTree(world, Collections.singleton(start), maxBlocks);
-    }
-
-    public Set<BlockPos> getTree(World world, Collection<BlockPos> start, int maxBlocks) {
-        LinkedList<BlockPos> searchQueue = new LinkedList<>(start);
-        HashSet<BlockPos> searched = new HashSet<>(start);
-        HashSet<BlockPos> found = new HashSet<>();
-
-        while(!searchQueue.isEmpty() && found.size() < maxBlocks) {
-            BlockPos current = searchQueue.removeFirst();
-            BlockState state = world.getBlockState(current);
-            if (isTree(state)) {
-                found.add(current);
-                BlockPos.getAllInBox(current.add(1, 1, 1), current.add(-1, -1, -1)).forEach(neighborMutable -> {
-                    if (searched.contains(neighborMutable)) return;
-                    BlockPos neighbor = neighborMutable.toImmutable();
-                    searched.add(neighbor);
-                    searchQueue.add(neighbor);
-                });
-            }
-        }
-        return found;
-    }
 
     @Nullable
     @Override
     public Item getCraftingReagent() {
-        return Items.DIAMOND_AXE;
+        return Items.IRON_HOE;
     }
 
     @Override
     public int getManaCost() {
-        return 30;
+        return 10;
     }
 
     @Override
     protected String getBookDescription() {
-        return "Harvests grown crops and trees. When used on grown crops, this spell will obtain the fully grown product without destroying the plant.";
+        return "When used on grown crops, this spell will obtain the fully grown product without destroying the plant.";
     }
 }
