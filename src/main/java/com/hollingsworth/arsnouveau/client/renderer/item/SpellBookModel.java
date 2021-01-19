867066d7febd5a6a0023544934d445c290dd174a
Bailey Hollingsworth <Bailey.A.Hollingsworth-1@ou.edu>
2021-01-18 18:57:17 -0600

    Closed book models

--- src/main/java/com/hollingsworth/arsnouveau/client/renderer/item/SpellBookModel.java
+++ src/main/java/com/hollingsworth/arsnouveau/client/renderer/item/SpellBookModel.java
@@ -13,6 +13,8 @@ public class SpellBookModel extends TransformAnimatedModel<SpellBook> {
     ResourceLocation T2 =  new ResourceLocation(ArsNouveau.MODID , "geo/spellbook_tier2.geo.json");
     ResourceLocation T3 =  new ResourceLocation(ArsNouveau.MODID , "geo/spellbook_tier3.geo.json");
     ResourceLocation T3_CLOSED =  new ResourceLocation(ArsNouveau.MODID , "geo/spellbook_tier3closed.geo.json");
+    ResourceLocation T1_CLOSED =  new ResourceLocation(ArsNouveau.MODID , "geo/spellbook_tier1closed.geo.json");
+    ResourceLocation T2_CLOSED =  new ResourceLocation(ArsNouveau.MODID , "geo/spellbook_tier2closed.geo.json");
 
     public boolean isOpen;
 
@@ -22,6 +24,10 @@ public class SpellBookModel extends TransformAnimatedModel<SpellBook> {
     public ResourceLocation getModelLocation(SpellBook book, @Nullable ItemCameraTransforms.TransformType transformType) {
 
         if(transformType == ItemCameraTransforms.TransformType.GUI){
+            if(book.tier == ISpellTier.Tier.ONE)
+                return T1_CLOSED;
+            if(book.tier == ISpellTier.Tier.TWO)
+                return T2_CLOSED;
             return T3_CLOSED;
         }
 
