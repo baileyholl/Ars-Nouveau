ce0468e77cbc8aea4f817d94c50df107a5c417e6
Bailey Hollingsworth <Bailey.A.Hollingsworth-1@ou.edu>
2021-01-22 17:36:54 -0600

    Beginning shades test

--- src/main/java/com/hollingsworth/arsnouveau/client/renderer/entity/CarbuncleShadesModel.java
+++ src/main/java/com/hollingsworth/arsnouveau/client/renderer/entity/CarbuncleShadesModel.java
@@ -3,7 +3,10 @@ package com.hollingsworth.arsnouveau.client.renderer.entity;
 import com.hollingsworth.arsnouveau.ArsNouveau;
 import com.hollingsworth.arsnouveau.common.entity.EntityCarbuncle;
 import net.minecraft.util.ResourceLocation;
+import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
+import software.bernie.geckolib3.core.processor.IBone;
 import software.bernie.geckolib3.model.AnimatedGeoModel;
+import software.bernie.geckolib3.model.provider.data.EntityModelData;
 
 public class CarbuncleShadesModel extends AnimatedGeoModel<EntityCarbuncle> {
 
@@ -16,9 +19,24 @@ public class CarbuncleShadesModel extends AnimatedGeoModel<EntityCarbuncle> {
     public ResourceLocation getTextureLocation(EntityCarbuncle o) {
         return new ResourceLocation(ArsNouveau.MODID, "textures/entity/carbuncle_shades.png");
     }
-
+//carbuncle_animations
     @Override
     public ResourceLocation getAnimationFileLocation(EntityCarbuncle animatable) {
-        return null;
+        return new ResourceLocation(ArsNouveau.MODID , "animations/carbuncle_animations.json");
+    }
+
+    @Override
+    public void setLivingAnimations(EntityCarbuncle entity, Integer uniqueID, AnimationEvent customPredicate) {
+        super.setLivingAnimations(entity, uniqueID, customPredicate);
+        IBone head = this.getAnimationProcessor().getBone("specs");
+        try {
+
+            EntityModelData extraData = (EntityModelData) customPredicate.getExtraDataOfType(EntityModelData.class).get(0);
+            head.setRotationX(-extraData.headPitch * 0.017453292F);
+            head.setRotationY(-extraData.netHeadYaw * 0.017453292F);
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/com/hollingsworth/arsnouveau/client/renderer/entity/ModelLayerRenderer.java
+++ src/main/java/com/hollingsworth/arsnouveau/client/renderer/entity/ModelLayerRenderer.java
@@ -8,15 +8,21 @@ import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.entity.Entity;
 import net.minecraft.item.ArmorItem;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.vector.Quaternion;
+import net.minecraft.util.math.vector.Vector3f;
 import software.bernie.geckolib3.core.IAnimatable;
 import software.bernie.geckolib3.core.controller.AnimationController;
+import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
+import software.bernie.geckolib3.core.processor.IBone;
 import software.bernie.geckolib3.geo.render.built.GeoModel;
 import software.bernie.geckolib3.model.AnimatedGeoModel;
+import software.bernie.geckolib3.model.provider.data.EntityModelData;
 import software.bernie.geckolib3.renderers.geo.GeoArmorRenderer;
 import software.bernie.geckolib3.renderers.geo.GeoLayerRenderer;
 import software.bernie.geckolib3.renderers.geo.IGeoRenderer;
 
 import java.awt.*;
+import java.util.Collections;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -31,7 +37,7 @@ public class ModelLayerRenderer<T extends Entity & IAnimatable> extends GeoLayer
         {
             if (object instanceof Entity)
             {
-               
+
             }
             return null;
         });
@@ -51,17 +57,37 @@ public class ModelLayerRenderer<T extends Entity & IAnimatable> extends GeoLayer
         return geoModelProvider.getTextureLocation(instance);
     }
 
+
+
     @Override
-    public void render(MatrixStack stack, IRenderTypeBuffer bufferIn, int packedLightIn, Entity entity, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch) {
-        stack.push();
+    public void render(MatrixStack matrixStackIn, IRenderTypeBuffer bufferIn, int packedLightIn, Entity entity, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch) {
+        matrixStackIn.push();
+        EntityModelData entityModelData = new EntityModelData();
+        entityModelData.headPitch = headPitch;
+        entityModelData.netHeadYaw = netHeadYaw;
         GeoModel model = geoModelProvider.getModel(geoModelProvider.getModelLocation(entity));
-        geoModelProvider.setLivingAnimations(entity, this.getUniqueID((T) entity));
+        AnimationEvent predicate = new AnimationEvent((IAnimatable)entity, limbSwing, limbSwingAmount, partialTicks, !(limbSwingAmount > -0.15F && limbSwingAmount < 0.15F), Collections.singletonList(entityModelData));
+        IBone carbuncle = ((CarbuncleModel)getEntityModel()).getBone("carbuncle");
+        geoModelProvider.setLivingAnimations((IAnimatable) entity, this.getUniqueID((T) entity), predicate);
+        IBone head = ((CarbuncleModel)getEntityModel()).getBone("head");
+
+        matrixStackIn.translate((carbuncle.getPositionX())/32f, (carbuncle.getPositionY())/16f ,
+                (carbuncle.getPositionZ()));
+
+//        matrixStackIn.translate((double)0f, (double)0.5f, .2D);
+//        matrixStackIn.scale(0.75f, 0.75f, 0.75f);
+
+        Quaternion quaternion = Vector3f.ZP.rotationDegrees(carbuncle.getRotationZ());
+
+        matrixStackIn.rotate(quaternion);
+       // matrixStackIn.rotate(Vector3f.YP.rotationDegrees(180f));
+
 
         Minecraft.getInstance().textureManager.bindTexture(getTextureLocation((T) entity));
-        Color renderColor = getRenderColor((T) entity, partialTicks, stack, bufferIn, null, packedLightIn);
-        RenderType renderType = getRenderType((T) entity, partialTicks, stack, bufferIn, null, packedLightIn, getTextureLocation((T) entity));
-        render(model, (T) entity, partialTicks, renderType, stack, bufferIn, null, packedLightIn, OverlayTexture.NO_OVERLAY, (float) renderColor.getRed() / 255f, (float) renderColor.getGreen() / 255f, (float) renderColor.getBlue() / 255f, (float) renderColor.getAlpha() / 255);
-        stack.pop();
+        Color renderColor = getRenderColor((T) entity, partialTicks, matrixStackIn, bufferIn, null, packedLightIn);
+        RenderType renderType = getRenderType((T) entity, partialTicks, matrixStackIn, bufferIn, null, packedLightIn, getTextureLocation((T) entity));
+        render(model, (T) entity, partialTicks, renderType, matrixStackIn, bufferIn, null, packedLightIn, OverlayTexture.NO_OVERLAY, (float) renderColor.getRed() / 255f, (float) renderColor.getGreen() / 255f, (float) renderColor.getBlue() / 255f, (float) renderColor.getAlpha() / 255);
+        matrixStackIn.pop();
     }
 
 }
