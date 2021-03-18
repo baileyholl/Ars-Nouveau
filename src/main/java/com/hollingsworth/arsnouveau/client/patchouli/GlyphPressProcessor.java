ddbe2fa8b6093bae972e1ef2a2a54db77d78cecd
kappa_maintainer <pocketkirby1@gmail.com>
2021-03-18 14:25:09 +0800

    Overhaul Chinese translation, made some strings localizable

--- src/main/java/com/hollingsworth/arsnouveau/client/patchouli/GlyphPressProcessor.java
+++ src/main/java/com/hollingsworth/arsnouveau/client/patchouli/GlyphPressProcessor.java
@@ -3,6 +3,7 @@ package com.hollingsworth.arsnouveau.client.patchouli;
 import com.hollingsworth.arsnouveau.api.recipe.GlyphPressRecipe;
 import com.hollingsworth.arsnouveau.common.items.Glyph;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.resources.I18n;
 import net.minecraft.item.crafting.RecipeManager;
 import net.minecraft.util.ResourceLocation;
 import vazkii.patchouli.api.IComponentProcessor;
@@ -26,19 +27,19 @@ public class GlyphPressProcessor implements IComponentProcessor {
         if(s.equals("reagent"))
             return IVariable.from(recipe.reagent);
         if(s.equals("tier"))
-            return IVariable.wrap(recipe.tier.toString());
+            return IVariable.wrap(I18n.format("ars_nouveau.spell_tier." + recipe.tier.toString().toLowerCase()));
         if(s.equals("mana_cost") ){
             if(recipe.output.getItem() instanceof Glyph){
                 int cost =  ((Glyph) recipe.output.getItem()).spellPart.getManaCost();
                 String costLang = "";
                 if(cost == 0)
-                    costLang = "None";
+                    costLang = I18n.format("ars_nouveau.mana_cost.none");
                 if(cost < 20)
-                    costLang = "Low";
+                    costLang = I18n.format("ars_nouveau.mana_cost.low");
                 if(cost < 50)
-                    costLang = "Medium";
+                    costLang = I18n.format("ars_nouveau.mana_cost.medium");
                 if(cost >= 50)
-                    costLang = "High";
+                    costLang = I18n.format("ars_nouveau.mana_cost.high");
                 return IVariable.wrap(costLang);
             }
             return IVariable.wrap("");
