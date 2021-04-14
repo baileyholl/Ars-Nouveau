598e24848924ba3f04215569e5f8450174e66a99
Bailey Hollingsworth <baileyholl@gmail.com>
2021-04-14 18:15:54 -0500

    Dye recipe

---
+++ src/main/java/com/hollingsworth/arsnouveau/common/crafting/recipes/DyeRecipe.java
@@ -0,0 +1,93 @@
+package com.hollingsworth.arsnouveau.common.crafting.recipes;
+
+import com.google.gson.JsonObject;
+import com.hollingsworth.arsnouveau.common.crafting.ModCrafting;
+import com.hollingsworth.arsnouveau.common.items.SpellBook;
+import net.minecraft.inventory.CraftingInventory;
+import net.minecraft.item.DyeColor;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.IRecipeSerializer;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.item.crafting.ShapelessRecipe;
+import net.minecraft.network.PacketBuffer;
+import net.minecraft.util.JSONUtils;
+import net.minecraft.util.NonNullList;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.common.crafting.CraftingHelper;
+import net.minecraftforge.registries.ForgeRegistryEntry;
+
+public class DyeRecipe extends ShapelessRecipe {
+
+
+    public DyeRecipe(ResourceLocation idIn, String groupIn, ItemStack recipeOutputIn, NonNullList<Ingredient> recipeItemsIn) {
+        super(idIn, groupIn, recipeOutputIn, recipeItemsIn);
+    }
+
+    @Override
+    public ItemStack getCraftingResult(final CraftingInventory inv) {
+        final ItemStack output = super.getCraftingResult(inv); // Get the default output
+
+        if (!output.isEmpty()) {
+            for (int i = 0; i < inv.getSizeInventory(); i++) { // For each slot in the crafting inventory,
+                final ItemStack ingredient = inv.getStackInSlot(i); // Get the ingredient in the slot
+                if (!ingredient.isEmpty() && ingredient.getItem() instanceof SpellBook) {
+                    output.setTag(ingredient.getTag().copy());
+                }
+            }
+            for (int i = 0; i < inv.getSizeInventory(); i++) { // For each slot in the crafting inventory,
+                final ItemStack ingredient = inv.getStackInSlot(i); // Get the ingredient in the slot
+                DyeColor color = DyeColor.getColor(ingredient);
+                if (!ingredient.isEmpty() && color != null) {
+
+                    output.getTag().putInt("color",color.getId());
+
+                }
+            }
+        }
+
+        return output; // Return the modified output
+    }
+
+    @Override
+    public IRecipeSerializer<?> getSerializer() {
+        return ModCrafting.Recipes.DYE_RECIPE;
+    }
+
+    public static class Serializer extends ForgeRegistryEntry<IRecipeSerializer<?>> implements IRecipeSerializer<DyeRecipe> {
+        @Override
+        public DyeRecipe read(final ResourceLocation recipeID, final JsonObject json) {
+            final String group = JSONUtils.getString(json, "group", "");
+            final NonNullList<Ingredient> ingredients = RecipeUtil.parseShapeless(json);
+            final ItemStack result = CraftingHelper.getItemStack(JSONUtils.getJsonObject(json, "result"), true);
+
+            return new DyeRecipe(recipeID, group, result, ingredients);
+        }
+
+        @Override
+        public DyeRecipe read(final ResourceLocation recipeID, final PacketBuffer buffer) {
+            final String group = buffer.readString(Short.MAX_VALUE);
+            final int numIngredients = buffer.readVarInt();
+            final NonNullList<Ingredient> ingredients = NonNullList.withSize(numIngredients, Ingredient.EMPTY);
+
+            for (int j = 0; j < ingredients.size(); ++j) {
+                ingredients.set(j, Ingredient.read(buffer));
+            }
+
+            final ItemStack result = buffer.readItemStack();
+
+            return new DyeRecipe(recipeID, group, result, ingredients);
+        }
+
+        @Override
+        public void write(final PacketBuffer buffer, final DyeRecipe recipe) {
+            buffer.writeString(recipe.getGroup());
+            buffer.writeVarInt(recipe.getIngredients().size());
+
+            for (final Ingredient ingredient : recipe.getIngredients()) {
+                ingredient.write(buffer);
+            }
+
+            buffer.writeItemStack(recipe.getRecipeOutput());
+        }
+    }
+}
