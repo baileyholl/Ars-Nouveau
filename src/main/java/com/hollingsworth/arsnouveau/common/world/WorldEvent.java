68b85c956c6db010d15f503b7eb7a3562b7136ad
Bailey Hollingsworth <baileyholl@gmail.com>
2021-07-30 20:42:58 -0500

    Checks for 0 spawn weight on passive mobs #321

--- src/main/java/com/hollingsworth/arsnouveau/common/world/WorldEvent.java
+++ src/main/java/com/hollingsworth/arsnouveau/common/world/WorldEvent.java
@@ -205,34 +205,21 @@ public class WorldEvent {
                 .decorated(Placement.COUNT_EXTRA.configured(new AtSurfaceWithExtraConfig(0, 0, 0))))));
 
 
-        Registry.register(WorldGenRegistries.PROCESSOR_LIST, new ResourceLocation(ArsNouveau.MODID, "archwood_plains"), ARCHWOOD_PLAINS);
+       // Registry.register(WorldGenRegistries.PROCESSOR_LIST, new ResourceLocation(ArsNouveau.MODID, "archwood_plains"), ARCHWOOD_PLAINS);
 
     }
 
     @SubscribeEvent
     public static void biomeLoad(BiomeLoadingEvent e) {
+
         if (e.getCategory() == Biome.Category.NETHER || e.getCategory() == Biome.Category.THEEND)
             return;
+
+        addMobSpawns(e);
+
         if (Config.SPAWN_ORE.get()) {
             e.getGeneration().addFeature(GenerationStage.Decoration.UNDERGROUND_ORES,
                     Objects.requireNonNull(WorldGenRegistries.CONFIGURED_FEATURE.get(BlockRegistry.ARCANE_ORE.getRegistryName()))).build();
-        }
-        List<Biome.Category> categories = Arrays.asList(Biome.Category.FOREST, Biome.Category.EXTREME_HILLS, Biome.Category.JUNGLE,
-                Biome.Category.PLAINS, Biome.Category.SWAMP, Biome.Category.SAVANNA);
-        if (categories.contains(e.getCategory())) {
-            e.getSpawns().addSpawn(EntityClassification.CREATURE, new MobSpawnInfo.Spawners(ModEntities.ENTITY_CARBUNCLE_TYPE, Config.CARBUNCLE_WEIGHT.get(), 1, 1));
-            e.getSpawns().addSpawn(EntityClassification.CREATURE, new MobSpawnInfo.Spawners(ModEntities.ENTITY_SYLPH_TYPE, Config.SYLPH_WEIGHT.get(), 1, 1));
-        }
-        e.getSpawns().addSpawn(EntityClassification.CREATURE, new MobSpawnInfo.Spawners(ModEntities.ENTITY_DRYGMY, Config.DRYGMY_WEIGHT.get(), 1, 1));
-
-        if(!e.getCategory().equals(Biome.Category.MUSHROOM) && !e.getCategory().equals(Biome.Category.NONE)){
-            if(e.getClimate().temperature <= 0.35f &&  Config.WGUARDIAN_WEIGHT.get() > 0){
-                e.getSpawns().addSpawn(EntityClassification.MONSTER, new MobSpawnInfo.Spawners(ModEntities.WILDEN_GUARDIAN, Config.WGUARDIAN_WEIGHT.get(), 1, 1));
-            }
-            if( Config.WSTALKER_WEIGHT.get() > 0)
-                e.getSpawns().addSpawn(EntityClassification.MONSTER, new MobSpawnInfo.Spawners(ModEntities.WILDEN_STALKER, Config.WSTALKER_WEIGHT.get(), 3, 3));
-            if( Config.WHUNTER_WEIGHT.get() > 0)
-                e.getSpawns().addSpawn(EntityClassification.MONSTER, new MobSpawnInfo.Spawners(ModEntities.WILDEN_HUNTER, Config.WHUNTER_WEIGHT.get(), 1, 1));
         }
 
         if ((e.getCategory().equals(Biome.Category.TAIGA) ||e.getName().equals(new ResourceLocation(ArsNouveau.MODID, "archwood_forest")))  && Config.SPAWN_BERRIES.get()) {
@@ -253,6 +240,33 @@ public class WorldEvent {
 
         if(e.getName().equals(archwoodForest.getRegistryName())){
             addArchwoodForestFeatures(e);
+        }
+    }
+
+    public static void addMobSpawns(BiomeLoadingEvent e){
+        List<Biome.Category> categories = Arrays.asList(Biome.Category.FOREST, Biome.Category.EXTREME_HILLS, Biome.Category.JUNGLE,
+                Biome.Category.PLAINS, Biome.Category.SWAMP, Biome.Category.SAVANNA);
+
+        if (categories.contains(e.getCategory())) {
+            if (Config.CARBUNCLE_WEIGHT.get() > 0) {
+                e.getSpawns().addSpawn(EntityClassification.CREATURE, new MobSpawnInfo.Spawners(ModEntities.ENTITY_CARBUNCLE_TYPE, Config.CARBUNCLE_WEIGHT.get(), 1, 1));
+            }
+            if (Config.SYLPH_WEIGHT.get() > 0) {
+                e.getSpawns().addSpawn(EntityClassification.CREATURE, new MobSpawnInfo.Spawners(ModEntities.ENTITY_SYLPH_TYPE, Config.SYLPH_WEIGHT.get(), 1, 1));
+            }
+        }
+        if (Config.DRYGMY_WEIGHT.get() > 0) {
+            e.getSpawns().addSpawn(EntityClassification.CREATURE, new MobSpawnInfo.Spawners(ModEntities.ENTITY_DRYGMY, Config.DRYGMY_WEIGHT.get(), 1, 1));
+        }
+
+        if(!e.getCategory().equals(Biome.Category.MUSHROOM) && !e.getCategory().equals(Biome.Category.NONE)){
+            if(e.getClimate().temperature <= 0.35f &&  Config.WGUARDIAN_WEIGHT.get() > 0){
+                e.getSpawns().addSpawn(EntityClassification.MONSTER, new MobSpawnInfo.Spawners(ModEntities.WILDEN_GUARDIAN, Config.WGUARDIAN_WEIGHT.get(), 1, 1));
+            }
+            if( Config.WSTALKER_WEIGHT.get() > 0)
+                e.getSpawns().addSpawn(EntityClassification.MONSTER, new MobSpawnInfo.Spawners(ModEntities.WILDEN_STALKER, Config.WSTALKER_WEIGHT.get(), 3, 3));
+            if( Config.WHUNTER_WEIGHT.get() > 0)
+                e.getSpawns().addSpawn(EntityClassification.MONSTER, new MobSpawnInfo.Spawners(ModEntities.WILDEN_HUNTER, Config.WHUNTER_WEIGHT.get(), 1, 1));
         }
     }
 
