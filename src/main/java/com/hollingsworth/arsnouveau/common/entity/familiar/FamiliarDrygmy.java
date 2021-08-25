0c0c1beb305c3f047efabebb3614f93fdff3f20d
Bailey Hollingsworth <baileyholl@gmail.com>
2021-08-24 20:10:56 -0500

    Adds drygmy content

--- src/main/java/com/hollingsworth/arsnouveau/common/entity/familiar/FamiliarDrygmy.java
+++ src/main/java/com/hollingsworth/arsnouveau/common/entity/familiar/FamiliarDrygmy.java
@@ -1,9 +1,13 @@
 package com.hollingsworth.arsnouveau.common.entity.familiar;
 
+import com.hollingsworth.arsnouveau.api.event.SpellModifierEvent;
+import com.hollingsworth.arsnouveau.api.spell.SpellSchools;
 import com.hollingsworth.arsnouveau.common.entity.ModEntities;
 import net.minecraft.entity.CreatureEntity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.world.World;
+import net.minecraftforge.event.entity.living.LootingLevelEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import software.bernie.geckolib3.core.PlayState;
 import software.bernie.geckolib3.core.builder.AnimationBuilder;
 import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
@@ -12,6 +16,24 @@ public class FamiliarDrygmy extends FamiliarEntity {
 
     public FamiliarDrygmy(EntityType<? extends CreatureEntity> ent, World world) {
         super(ent, world);
+    }
+
+
+    @SubscribeEvent
+    public void spellResolveEvent(SpellModifierEvent event) {
+        if(isAlive() && getOwner() != null && getOwner().equals(event.caster) && SpellSchools.ELEMENTAL_EARTH.isPartOfSchool(event.spellPart)){
+            event.builder.addDamageModifier(2.0f);
+        }
+    }
+
+
+    @SubscribeEvent
+    public void fortuneEvent(LootingLevelEvent event) {
+        if(isAlive() && getOwner() != null && event.getDamageSource().getEntity() != null &&  getOwner().equals(event.getDamageSource().getEntity())){
+            if(level.random.nextFloat() > 0.4) {
+                event.setLootingLevel(event.getLootingLevel() + 1 + random.nextInt(3));
+            }
+        }
     }
 
     @Override
--- src/main/java/com/hollingsworth/arsnouveau/common/entity/familiar/FamiliarJabberwog.java
+++ src/main/java/com/hollingsworth/arsnouveau/common/entity/familiar/FamiliarJabberwog.java
@@ -1,9 +1,11 @@
 package com.hollingsworth.arsnouveau.common.entity.familiar;
 
+import com.hollingsworth.arsnouveau.api.event.SpellModifierEvent;
 import com.hollingsworth.arsnouveau.common.entity.ModEntities;
 import net.minecraft.entity.CreatureEntity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.world.World;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import software.bernie.geckolib3.core.PlayState;
 import software.bernie.geckolib3.core.builder.AnimationBuilder;
 import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
@@ -13,6 +15,13 @@ public class FamiliarJabberwog extends FlyingFamiliarEntity {
     public FamiliarJabberwog(EntityType<? extends CreatureEntity> ent, World world) {
         super(ent, world);
 
+    }
+
+    @SubscribeEvent
+    public void spellResolveEvent(SpellModifierEvent event) {
+        if(isAlive() && getOwner() != null && getOwner().equals(event.caster)){
+            event.builder.addDamageModifier(3.0f);
+        }
     }
 
     @Override
