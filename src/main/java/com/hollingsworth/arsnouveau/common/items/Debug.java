eab5fd5bf057496cb31bf1e5d78c7a195274a671
Bailey Hollingsworth <baileyholl@gmail.com>
2021-09-04 20:30:17 -0500

    Earthquake test

--- src/main/java/com/hollingsworth/arsnouveau/common/items/Debug.java
+++ src/main/java/com/hollingsworth/arsnouveau/common/items/Debug.java
@@ -1,11 +1,11 @@
 package com.hollingsworth.arsnouveau.common.items;
 
-import com.hollingsworth.arsnouveau.common.entity.familiar.FamiliarCarbuncle;
-import com.hollingsworth.arsnouveau.common.entity.ModEntities;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemUseContext;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Hand;
 import net.minecraft.world.World;
 
@@ -15,14 +15,20 @@ public class Debug extends ModItem{
         setRegistryName("debug");
     }
 
+
+    @Override
+    public ActionResultType useOn(ItemUseContext context) {
+        if(!context.getLevel().isClientSide){
+//            EventQueue.getServerInstance().addEvent(new EarthquakeEvent(context.getLevel(), context.getClickedPos(), context.getClickedPos().north(20).east(0)));
+//            EventQueue.getServerInstance().addEvent(new EarthquakeEvent(context.getLevel(), context.getClickedPos(), context.getClickedPos().north(20).east(20)));
+//            EventQueue.getServerInstance().addEvent(new EarthquakeEvent(context.getLevel(), context.getClickedPos(), context.getClickedPos().north(20).west(20)));
+        }
+        return super.useOn(context);
+    }
+
     @Override
     public ActionResult<ItemStack> use(World world, PlayerEntity playerIn, Hand handIn) {
-        if(!world.isClientSide){
-            FamiliarCarbuncle carbuncle = new FamiliarCarbuncle(ModEntities.ENTITY_FAMILIAR_CARBUNCLE, world);
-            carbuncle.setPos(playerIn.blockPosition().getX(), playerIn.position.y, playerIn.position.z);
-            carbuncle.setOwnerID(playerIn.getUUID());
-            world.addFreshEntity(carbuncle);
-        }
+
         return ActionResult.success(playerIn.getItemInHand(handIn));
     }
 }
