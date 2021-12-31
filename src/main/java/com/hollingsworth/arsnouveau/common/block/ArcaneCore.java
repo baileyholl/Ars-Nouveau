b7095b4f4c5e6a62650d365a259073c940a82375
Bailey Hollingsworth <baileyholl@gmail.com>
2021-12-30 19:14:39 -0600

    Update potion jar, brazier, core

--- src/main/java/com/hollingsworth/arsnouveau/common/block/ArcaneCore.java
+++ src/main/java/com/hollingsworth/arsnouveau/common/block/ArcaneCore.java
@@ -3,11 +3,12 @@ package com.hollingsworth.arsnouveau.common.block;
 import com.hollingsworth.arsnouveau.common.block.tile.ArcaneCoreTile;
 import com.hollingsworth.arsnouveau.common.lib.LibBlockNames;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.block.EntityBlock;
 import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 
-public class ArcaneCore extends TickableModBlock {
+public class ArcaneCore extends ModBlock implements EntityBlock {
     public ArcaneCore() {
         super(defaultProperties().noOcclusion().lightLevel((state) -> 15),LibBlockNames.ARCANE_CORE);
     }
@@ -16,6 +17,8 @@ public class ArcaneCore extends TickableModBlock {
     public BlockEntity newBlockEntity(BlockPos pos, BlockState state) {
         return new ArcaneCoreTile(pos, state);
     }
+
+
 
     @Override
     public RenderShape getRenderShape(BlockState p_149645_1_) {
