e4d2d3537ea1b32d5bc60a2b5de49ac26f2b8bf4
Bailey Hollingsworth <baileyholl@gmail.com>
2022-01-29 13:57:44 -0600

    Remove AWT for geckolib compat

--- src/main/java/com/hollingsworth/arsnouveau/client/renderer/entity/ModelLayerRenderer.java
+++ src/main/java/com/hollingsworth/arsnouveau/client/renderer/entity/ModelLayerRenderer.java
@@ -14,6 +14,7 @@ import software.bernie.geckolib3.core.IAnimatable;
 import software.bernie.geckolib3.core.controller.AnimationController;
 import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
 import software.bernie.geckolib3.core.processor.IBone;
+import software.bernie.geckolib3.core.util.Color;
 import software.bernie.geckolib3.geo.render.built.GeoModel;
 import software.bernie.geckolib3.model.AnimatedGeoModel;
 import software.bernie.geckolib3.model.provider.data.EntityModelData;
@@ -21,7 +22,6 @@ import software.bernie.geckolib3.renderers.geo.GeoArmorRenderer;
 import software.bernie.geckolib3.renderers.geo.GeoLayerRenderer;
 import software.bernie.geckolib3.renderers.geo.IGeoRenderer;
 
-import java.awt.*;
 import java.util.Collections;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
