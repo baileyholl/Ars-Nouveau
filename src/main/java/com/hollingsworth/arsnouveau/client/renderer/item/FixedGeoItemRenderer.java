a3b3a76fe639f51b2b333875b421bb2c8a435e7b
Bailey Hollingsworth <baileyholl@gmail.com>
2022-01-29 15:00:26 -0600

    Fix compat with latest geckolib

--- src/main/java/com/hollingsworth/arsnouveau/client/renderer/item/FixedGeoItemRenderer.java
+++ src/main/java/com/hollingsworth/arsnouveau/client/renderer/item/FixedGeoItemRenderer.java
@@ -12,11 +12,11 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import software.bernie.geckolib3.core.IAnimatable;
 import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
+import software.bernie.geckolib3.core.util.Color;
 import software.bernie.geckolib3.geo.render.built.GeoModel;
 import software.bernie.geckolib3.model.AnimatedGeoModel;
 import software.bernie.geckolib3.renderers.geo.GeoItemRenderer;
 
-import java.awt.*;
 import java.util.Collections;
 
 public class FixedGeoItemRenderer<T extends Item & IAnimatable> extends GeoItemRenderer {
