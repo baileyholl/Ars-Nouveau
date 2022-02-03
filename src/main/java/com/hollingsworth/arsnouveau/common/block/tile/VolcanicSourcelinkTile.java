4618fa32ab303f22e6d874a9f0c222bd2aec1498
Bailey Hollingsworth <baileyholl@gmail.com>
2022-02-03 15:05:40 -0600

    Fix volcanic sourcelink replacing itself

--- src/main/java/com/hollingsworth/arsnouveau/common/block/tile/VolcanicSourcelinkTile.java
+++ src/main/java/com/hollingsworth/arsnouveau/common/block/tile/VolcanicSourcelinkTile.java
@@ -90,7 +90,7 @@ public class VolcanicSourcelinkTile extends SourcelinkTile implements IAnimatabl
             return;
         AtomicBoolean set = new AtomicBoolean(false);
         BlockPos.withinManhattanStream(worldPosition, 1, 0,1).forEach(p ->{
-            if(!set.get() && level.getBlockState(p).isAir() && level.getFluidState(p.below()).getType() == Fluids.LAVA || level.getFluidState(p.below()).getType() == Fluids.FLOWING_LAVA){
+            if(!set.get() && level.getBlockState(p).isAir() && (level.getFluidState(p.below()).getType() == Fluids.LAVA || level.getFluidState(p.below()).getType() == Fluids.FLOWING_LAVA)){
                 level.setBlockAndUpdate(p, BlockRegistry.LAVA_LILY.getState(level, p));
                 set.set(true);
             }
