37806b7c6cd335208a44b72988535aeb3ae6cc70
Bailey Hollingsworth <baileyholl@gmail.com>
2022-02-27 15:44:25 -0600

    Updated turret, relay models

--- src/main/java/com/hollingsworth/arsnouveau/common/block/Relay.java
+++ src/main/java/com/hollingsworth/arsnouveau/common/block/Relay.java
@@ -3,14 +3,21 @@ package com.hollingsworth.arsnouveau.common.block;
 import com.hollingsworth.arsnouveau.common.block.tile.RelayTile;
 import com.hollingsworth.arsnouveau.common.lib.LibBlockNames;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.shapes.BooleanOp;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
 
 public class Relay extends TickableModBlock {
+
+    VoxelShape shape = makeShape();
 
     public Relay() {
         this(LibBlockNames.RELAY);
@@ -43,4 +50,40 @@ public class Relay extends TickableModBlock {
         }
     }
 
+    @Override
+    public VoxelShape getShape(BlockState pState, BlockGetter pLevel, BlockPos pPos, CollisionContext pContext) {
+        return shape;
+    }
+
+    public VoxelShape makeShape(){
+        VoxelShape shape = Shapes.empty();
+        shape = Shapes.join(shape, Shapes.box(0.3125, 0.6875, 0.1875, 0.625, 0.75, 0.25), BooleanOp.OR);
+        shape = Shapes.join(shape, Shapes.box(0.3125, 0.25, 0.3125, 0.6875, 0.75, 0.6875), BooleanOp.OR);
+        shape = Shapes.join(shape, Shapes.box(0.5625, 0.5, 0.1875, 0.625, 0.6875, 0.25), BooleanOp.OR);
+        shape = Shapes.join(shape, Shapes.box(0.4375, 0.5, 0.1875, 0.5625, 0.5625, 0.25), BooleanOp.OR);
+        shape = Shapes.join(shape, Shapes.box(0.4375, 0.5625, 0.1875, 0.5, 0.625, 0.25), BooleanOp.OR);
+        shape = Shapes.join(shape, Shapes.box(0.1875, 0.6875, 0.3125, 0.25, 0.75, 0.625), BooleanOp.OR);
+        shape = Shapes.join(shape, Shapes.box(0.1875, 0.5, 0.5625, 0.25, 0.6875, 0.625), BooleanOp.OR);
+        shape = Shapes.join(shape, Shapes.box(0.1875, 0.5, 0.4375, 0.25, 0.5625, 0.5625), BooleanOp.OR);
+        shape = Shapes.join(shape, Shapes.box(0.1875, 0.5625, 0.4375, 0.25, 0.625, 0.5), BooleanOp.OR);
+        shape = Shapes.join(shape, Shapes.box(0.3125, 0.3125, 0.1875, 0.375, 0.6875, 0.25), BooleanOp.OR);
+        shape = Shapes.join(shape, Shapes.box(0.1875, 0.3125, 0.3125, 0.25, 0.6875, 0.375), BooleanOp.OR);
+        shape = Shapes.join(shape, Shapes.box(0.1875, 0.3125, 0.1875, 0.3125, 0.375, 0.25), BooleanOp.OR);
+        shape = Shapes.join(shape, Shapes.box(0.1875, 0.3125, 0.25, 0.25, 0.375, 0.3125), BooleanOp.OR);
+        shape = Shapes.join(shape, Shapes.box(0.3125, 0.6875, 0.1875, 0.625, 0.75, 0.25), BooleanOp.OR);
+        shape = Shapes.join(shape, Shapes.box(0.3125, 0.3125, 0.1875, 0.375, 0.6875, 0.25), BooleanOp.OR);
+        shape = Shapes.join(shape, Shapes.box(0.1875, 0.3125, 0.1875, 0.3125, 0.375, 0.25), BooleanOp.OR);
+        shape = Shapes.join(shape, Shapes.box(0.1875, 0.3125, 0.25, 0.25, 0.375, 0.3125), BooleanOp.OR);
+        shape = Shapes.join(shape, Shapes.box(0.1875, 0.3125, 0.3125, 0.25, 0.6875, 0.375), BooleanOp.OR);
+        shape = Shapes.join(shape, Shapes.box(0.5625, 0.5, 0.1875, 0.625, 0.6875, 0.25), BooleanOp.OR);
+        shape = Shapes.join(shape, Shapes.box(0.4375, 0.5, 0.1875, 0.5625, 0.5625, 0.25), BooleanOp.OR);
+        shape = Shapes.join(shape, Shapes.box(0.4375, 0.5625, 0.1875, 0.5, 0.625, 0.25), BooleanOp.OR);
+        shape = Shapes.join(shape, Shapes.box(0.1875, 0.6875, 0.3125, 0.25, 0.75, 0.625), BooleanOp.OR);
+        shape = Shapes.join(shape, Shapes.box(0.1875, 0.5625, 0.4375, 0.25, 0.625, 0.5), BooleanOp.OR);
+        shape = Shapes.join(shape, Shapes.box(0.1875, 0.5, 0.4375, 0.25, 0.5625, 0.5625), BooleanOp.OR);
+        shape = Shapes.join(shape, Shapes.box(0.1875, 0.5, 0.5625, 0.25, 0.6875, 0.625), BooleanOp.OR);
+        shape = Shapes.join(shape, Shapes.box(0.3125, 0.25, 0.3125, 0.6875, 0.75, 0.6875), BooleanOp.OR);
+
+        return shape;
+    }
 }
