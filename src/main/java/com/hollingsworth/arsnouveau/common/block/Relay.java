9be469c8f8e105ea2d8bb04971b58ffecf666da7
Bailey Hollingsworth <baileyholl@gmail.com>
2022-02-27 15:57:56 -0600

    Shrink relay bbox

--- src/main/java/com/hollingsworth/arsnouveau/common/block/Relay.java
+++ src/main/java/com/hollingsworth/arsnouveau/common/block/Relay.java
@@ -10,14 +10,12 @@ import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.phys.shapes.BooleanOp;
 import net.minecraft.world.phys.shapes.CollisionContext;
-import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
 
 public class Relay extends TickableModBlock {
 
-    VoxelShape shape = makeShape();
+    public static final VoxelShape shape = Block.box(2D, 3.0D, 2.0D, 15, 14, 15);
 
     public Relay() {
         this(LibBlockNames.RELAY);
@@ -52,38 +50,6 @@ public class Relay extends TickableModBlock {
 
     @Override
     public VoxelShape getShape(BlockState pState, BlockGetter pLevel, BlockPos pPos, CollisionContext pContext) {
-        return shape;
-    }
-
-    public VoxelShape makeShape(){
-        VoxelShape shape = Shapes.empty();
-        shape = Shapes.join(shape, Shapes.box(0.3125, 0.6875, 0.1875, 0.625, 0.75, 0.25), BooleanOp.OR);
-        shape = Shapes.join(shape, Shapes.box(0.3125, 0.25, 0.3125, 0.6875, 0.75, 0.6875), BooleanOp.OR);
-        shape = Shapes.join(shape, Shapes.box(0.5625, 0.5, 0.1875, 0.625, 0.6875, 0.25), BooleanOp.OR);
-        shape = Shapes.join(shape, Shapes.box(0.4375, 0.5, 0.1875, 0.5625, 0.5625, 0.25), BooleanOp.OR);
-        shape = Shapes.join(shape, Shapes.box(0.4375, 0.5625, 0.1875, 0.5, 0.625, 0.25), BooleanOp.OR);
-        shape = Shapes.join(shape, Shapes.box(0.1875, 0.6875, 0.3125, 0.25, 0.75, 0.625), BooleanOp.OR);
-        shape = Shapes.join(shape, Shapes.box(0.1875, 0.5, 0.5625, 0.25, 0.6875, 0.625), BooleanOp.OR);
-        shape = Shapes.join(shape, Shapes.box(0.1875, 0.5, 0.4375, 0.25, 0.5625, 0.5625), BooleanOp.OR);
-        shape = Shapes.join(shape, Shapes.box(0.1875, 0.5625, 0.4375, 0.25, 0.625, 0.5), BooleanOp.OR);
-        shape = Shapes.join(shape, Shapes.box(0.3125, 0.3125, 0.1875, 0.375, 0.6875, 0.25), BooleanOp.OR);
-        shape = Shapes.join(shape, Shapes.box(0.1875, 0.3125, 0.3125, 0.25, 0.6875, 0.375), BooleanOp.OR);
-        shape = Shapes.join(shape, Shapes.box(0.1875, 0.3125, 0.1875, 0.3125, 0.375, 0.25), BooleanOp.OR);
-        shape = Shapes.join(shape, Shapes.box(0.1875, 0.3125, 0.25, 0.25, 0.375, 0.3125), BooleanOp.OR);
-        shape = Shapes.join(shape, Shapes.box(0.3125, 0.6875, 0.1875, 0.625, 0.75, 0.25), BooleanOp.OR);
-        shape = Shapes.join(shape, Shapes.box(0.3125, 0.3125, 0.1875, 0.375, 0.6875, 0.25), BooleanOp.OR);
-        shape = Shapes.join(shape, Shapes.box(0.1875, 0.3125, 0.1875, 0.3125, 0.375, 0.25), BooleanOp.OR);
-        shape = Shapes.join(shape, Shapes.box(0.1875, 0.3125, 0.25, 0.25, 0.375, 0.3125), BooleanOp.OR);
-        shape = Shapes.join(shape, Shapes.box(0.1875, 0.3125, 0.3125, 0.25, 0.6875, 0.375), BooleanOp.OR);
-        shape = Shapes.join(shape, Shapes.box(0.5625, 0.5, 0.1875, 0.625, 0.6875, 0.25), BooleanOp.OR);
-        shape = Shapes.join(shape, Shapes.box(0.4375, 0.5, 0.1875, 0.5625, 0.5625, 0.25), BooleanOp.OR);
-        shape = Shapes.join(shape, Shapes.box(0.4375, 0.5625, 0.1875, 0.5, 0.625, 0.25), BooleanOp.OR);
-        shape = Shapes.join(shape, Shapes.box(0.1875, 0.6875, 0.3125, 0.25, 0.75, 0.625), BooleanOp.OR);
-        shape = Shapes.join(shape, Shapes.box(0.1875, 0.5625, 0.4375, 0.25, 0.625, 0.5), BooleanOp.OR);
-        shape = Shapes.join(shape, Shapes.box(0.1875, 0.5, 0.4375, 0.25, 0.5625, 0.5625), BooleanOp.OR);
-        shape = Shapes.join(shape, Shapes.box(0.1875, 0.5, 0.5625, 0.25, 0.6875, 0.625), BooleanOp.OR);
-        shape = Shapes.join(shape, Shapes.box(0.3125, 0.25, 0.3125, 0.6875, 0.75, 0.6875), BooleanOp.OR);
-
         return shape;
     }
 }
--- src/main/java/com/hollingsworth/arsnouveau/common/block/RelaySplitter.java
+++ src/main/java/com/hollingsworth/arsnouveau/common/block/RelaySplitter.java
@@ -3,15 +3,10 @@ package com.hollingsworth.arsnouveau.common.block;
 import com.hollingsworth.arsnouveau.common.block.tile.RelaySplitterTile;
 import com.hollingsworth.arsnouveau.common.lib.LibBlockNames;
 import net.minecraft.core.BlockPos;
-import net.minecraft.world.level.BlockGetter;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.phys.shapes.CollisionContext;
-import net.minecraft.world.phys.shapes.VoxelShape;
 
-public class RelaySplitter extends TickableModBlock {
+public class RelaySplitter extends Relay {
 
     public RelaySplitter() {
         super(defaultProperties().lightLevel((state) -> 8).noOcclusion(), LibBlockNames.RELAY_SPLITTER);
@@ -20,15 +15,5 @@ public class RelaySplitter extends TickableModBlock {
     @Override
     public BlockEntity newBlockEntity(BlockPos pos, BlockState state) {
         return new RelaySplitterTile(pos, state);
-    }
-
-    @Override
-    public RenderShape getRenderShape(BlockState p_149645_1_) {
-        return RenderShape.ENTITYBLOCK_ANIMATED;
-    }
-
-    @Override
-    public VoxelShape getShape(BlockState state, BlockGetter worldIn, BlockPos pos, CollisionContext context) {
-        return Block.box(1D, 1.0D, 1.0D, 15, 15, 15);
     }
 }
