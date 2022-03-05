a73a3e142378b11220741028e3f1dc4e8b141c1c
Bailey Hollingsworth <baileyholl@gmail.com>
2022-03-05 13:10:11 -0600

    Fix augment enforcement setting #478

--- src/main/java/com/hollingsworth/arsnouveau/common/spell/validation/StandardSpellValidator.java
+++ src/main/java/com/hollingsworth/arsnouveau/common/spell/validation/StandardSpellValidator.java
@@ -39,19 +39,23 @@ public class StandardSpellValidator implements ISpellValidator {
         // Unconditional validators
         validators.add(MAX_ONE_CAST_METHOD);
         validators.add(GLYPH_OCCURRENCES_POLICY);
-        if(!enforceCastTimeValidations || Config.ENFORCE_AUGMENT_CAP_ON_CAST.get())
-            validators.add(EFFECT_AUGMENTATION_POLICY);
 
         // Validators only applicable at crafting time
         if (!enforceCastTimeValidations) {
             // Not enforcing this at cast time so we don't break existing spells with ineffective augments
             validators.add(AUGMENT_COMPATIBILITY);
+            validators.add(EFFECT_AUGMENTATION_POLICY);
         }
 
         // Validators only applicable at casting time
         if (enforceCastTimeValidations) {
             validators.add(NON_EMPTY_SPELL);
             validators.add(REQUIRE_CAST_METHOD_START);
+
+            if(Config.ENFORCE_AUGMENT_CAP_ON_CAST.get()){
+                validators.add(EFFECT_AUGMENTATION_POLICY);
+            }
+
         }
 
         // Combine them all together.
