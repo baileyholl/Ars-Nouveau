e8e8434f527e64112abe504139b1d279fbf452ec
Alexthw46 <77152778+Alexthw46@users.noreply.github.com>
2022-06-20 12:37:39 +0200

    Gradle versuion bumps. Biome tags test for Archwood forest
    Signed-off-by: Alexthw46 <77152778+Alexthw46@users.noreply.github.com>

--- src/main/java/com/hollingsworth/arsnouveau/common/datagen/BiomeTagProvider.java
+++ src/main/java/com/hollingsworth/arsnouveau/common/datagen/BiomeTagProvider.java
@@ -1,9 +1,11 @@
 package com.hollingsworth.arsnouveau.common.datagen;
 
 import com.hollingsworth.arsnouveau.ArsNouveau;
+import com.hollingsworth.arsnouveau.common.world.biome.ModBiomes;
 import net.minecraft.core.Registry;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.tags.BiomeTagsProvider;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.BiomeTags;
 import net.minecraft.tags.TagKey;
@@ -23,6 +25,18 @@ public class BiomeTagProvider extends BiomeTagsProvider {
 
     @Override
     protected void addTags() {
-        this.tag(SUMMON_SPAWN_TAG).addTags(BiomeTags.IS_FOREST, BiomeTags.IS_HILL, IS_TAIGA, IS_JUNGLE, IS_SAVANNA,IS_OVERWORLD ).add(Biomes.PLAINS);
+        addTagsToBiome(ModBiomes.ARCHWOOD_FOREST, IS_FOREST, IS_OVERWORLD, HAS_VILLAGE_PLAINS);
+        
+        this.tag(SUMMON_SPAWN_TAG).addTags(BiomeTags.IS_FOREST, BiomeTags.IS_HILL, IS_TAIGA, IS_JUNGLE, IS_SAVANNA, IS_OVERWORLD ).add(Biomes.PLAINS);
+        
     }
+    
+    void addTagsToBiome(ResourceKey<Biome> biome, TagKey<Biome>... tags){
+        
+        for (TagKey<Biome> tag : tags){
+            this.tag(tag).add(biome);
+        }
+        
+    }
+    
 }
