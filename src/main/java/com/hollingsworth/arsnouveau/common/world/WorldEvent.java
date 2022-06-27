e23ab9c5375131cd5793f397e62e382536a2877f
Bailey Hollingsworth <baileyholl@gmail.com>
2022-06-26 19:56:06 -0500

    Fix archwood density

--- src/main/java/com/hollingsworth/arsnouveau/common/world/WorldEvent.java
+++ src/main/java/com/hollingsworth/arsnouveau/common/world/WorldEvent.java
@@ -8,6 +8,7 @@ import com.hollingsworth.arsnouveau.common.world.tree.SupplierBlockStateProvider
 import com.hollingsworth.arsnouveau.setup.BlockRegistry;
 import com.hollingsworth.arsnouveau.setup.Config;
 import net.minecraft.core.Holder;
+import net.minecraft.core.HolderSet;
 import net.minecraft.data.worldgen.features.FeatureUtils;
 import net.minecraft.data.worldgen.placement.PlacementUtils;
 import net.minecraft.data.worldgen.placement.VegetationPlacements;
@@ -16,18 +17,14 @@ import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
 import net.minecraft.world.level.levelgen.feature.Feature;
-import net.minecraft.world.level.levelgen.feature.WeightedPlacedFeature;
-import net.minecraft.world.level.levelgen.feature.configurations.RandomFeatureConfiguration;
 import net.minecraft.world.level.levelgen.feature.configurations.RandomPatchConfiguration;
 import net.minecraft.world.level.levelgen.feature.configurations.SimpleBlockConfiguration;
+import net.minecraft.world.level.levelgen.feature.configurations.SimpleRandomFeatureConfiguration;
 import net.minecraft.world.level.levelgen.feature.configurations.TreeConfiguration;
 import net.minecraft.world.level.levelgen.feature.featuresize.TwoLayersFeatureSize;
 import net.minecraft.world.level.levelgen.feature.foliageplacers.BlobFoliagePlacer;
 import net.minecraft.world.level.levelgen.feature.stateproviders.BlockStateProvider;
-import net.minecraft.world.level.levelgen.placement.BiomeFilter;
-import net.minecraft.world.level.levelgen.placement.InSquarePlacement;
-import net.minecraft.world.level.levelgen.placement.PlacedFeature;
-import net.minecraft.world.level.levelgen.placement.RarityFilter;
+import net.minecraft.world.level.levelgen.placement.*;
 import net.minecraftforge.registries.ForgeRegistries;
 
 import java.util.List;
@@ -75,38 +72,34 @@ public class WorldEvent {
 
     public static Holder<PlacedFeature> COMMON_FLOURISHING = PlacementUtils.register("ars_nouveau:common_flourishing",
             FLOURISHING_TREE,
-            List.of(PlacementUtils.countExtra(12, 0.1f, 1), PlacementUtils.filteredByBlockSurvival(BlockRegistry.FLOURISHING_SAPLING)));
+            List.of(PlacementUtils.countExtra(12, 0.01f, 1), PlacementUtils.filteredByBlockSurvival(BlockRegistry.FLOURISHING_SAPLING)));
 
     public static Holder<PlacedFeature> COMMON_CASCADING = PlacementUtils.register("ars_nouveau:common_cascading",
             CASCADING_TREE,
-            List.of(PlacementUtils.countExtra(12, 0.1f, 1),  PlacementUtils.filteredByBlockSurvival(BlockRegistry.CASCADING_SAPLING)));
+            List.of(PlacementUtils.countExtra(12, 0.01f, 1),  PlacementUtils.filteredByBlockSurvival(BlockRegistry.CASCADING_SAPLING)));
 
     public static Holder<PlacedFeature> COMMON_BLAZING = PlacementUtils.register("ars_nouveau:common_blazing",
             BLAZING_TREE,
-            List.of(PlacementUtils.countExtra(12, 0.1f, 1),  PlacementUtils.filteredByBlockSurvival(BlockRegistry.BLAZING_SAPLING)));
+            List.of(PlacementUtils.countExtra(12, 0.01f, 1),  PlacementUtils.filteredByBlockSurvival(BlockRegistry.BLAZING_SAPLING)));
 
     public static Holder<PlacedFeature> COMMON_VEXING = PlacementUtils.register("ars_nouveau:common_vexing",
             VEXING_TREE,
-           List.of(PlacementUtils.countExtra(12, 0.1f, 1),  PlacementUtils.filteredByBlockSurvival(BlockRegistry.VEXING_SAPLING)));
+           List.of(PlacementUtils.countExtra(12, 0.01f, 1),  PlacementUtils.filteredByBlockSurvival(BlockRegistry.VEXING_SAPLING)));
 
-    public static Holder<ConfiguredFeature<RandomFeatureConfiguration, ?>> MIXED_TREES = FeatureUtils.register("ars_nouveau:random_mixed", Feature.RANDOM_SELECTOR, new RandomFeatureConfiguration(List.of(
-            new WeightedPlacedFeature(PLACED_CASCADE, 0.25f),
-            new WeightedPlacedFeature(PLACED_BLAZING, 0.25f),
-            new WeightedPlacedFeature(PLACED_VEXING, 0.25f),
-            new WeightedPlacedFeature(PLACED_FLOURISHING, 0.25f)), PLACED_CASCADE));
+    public static Holder<ConfiguredFeature<SimpleRandomFeatureConfiguration, ?>> MIXED_TREES = FeatureUtils.register("ars_nouveau:random_mixed", Feature.SIMPLE_RANDOM_SELECTOR, new SimpleRandomFeatureConfiguration(
+            HolderSet.direct(PLACED_CASCADE, PLACED_BLAZING, PLACED_VEXING, PLACED_FLOURISHING)));
 
 
 
-    public static Holder<ConfiguredFeature<RandomFeatureConfiguration, ?>> MIXED_COMMON_TREES = FeatureUtils.register("ars_nouveau:random_common_archwood", Feature.RANDOM_SELECTOR, new RandomFeatureConfiguration(List.of(
-            new WeightedPlacedFeature(COMMON_FLOURISHING, 0.25f),
-            new WeightedPlacedFeature(COMMON_CASCADING, 0.25f),
-            new WeightedPlacedFeature(COMMON_BLAZING, 0.25f),
-            new WeightedPlacedFeature(COMMON_VEXING, 0.25f)), COMMON_FLOURISHING));
+    public static Holder<ConfiguredFeature<SimpleRandomFeatureConfiguration, ?>> MIXED_COMMON_TREES = FeatureUtils.register("ars_nouveau:random_common_archwood", Feature.SIMPLE_RANDOM_SELECTOR, new SimpleRandomFeatureConfiguration(
+                   HolderSet.direct(COMMON_FLOURISHING, COMMON_BLAZING, COMMON_VEXING, COMMON_CASCADING)));
 
     public static ResourceLocation PLACED_MIXED_ID = new ResourceLocation(ArsNouveau.MODID, "archwood");
 
     public static Holder<PlacedFeature> PLACED_MIXED = PlacementUtils.register(PLACED_MIXED_ID.toString(), MIXED_TREES, VegetationPlacements.treePlacement(RarityFilter.onAverageOnceEvery(Config.TREE_SPAWN_RATE)));
 
-    public static Holder<PlacedFeature> COMMON_ARCHWOOD = PlacementUtils.register("ars_nouveau:common_archwood", MIXED_COMMON_TREES, VegetationPlacements.treePlacement(RarityFilter.onAverageOnceEvery(1)));
+    public static Holder<PlacedFeature> COMMON_ARCHWOOD = PlacementUtils.register("ars_nouveau:common_archwood",
+            MIXED_COMMON_TREES,
+            VegetationPlacements.treePlacement(CountPlacement.of(3), BlockRegistry.BLAZING_SAPLING));
 
 }
