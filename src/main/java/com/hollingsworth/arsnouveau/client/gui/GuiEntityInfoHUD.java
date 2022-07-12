1e79b624adc5a00d863e990105f2736cf58e2aef
Bailey Hollingsworth <baileyholl@gmail.com>
2022-07-11 20:56:35 -0500

    Adds Manabar config, new tooltips

--- src/main/java/com/hollingsworth/arsnouveau/client/gui/GuiEntityInfoHUD.java
+++ src/main/java/com/hollingsworth/arsnouveau/client/gui/GuiEntityInfoHUD.java
@@ -1,23 +1,296 @@
 package com.hollingsworth.arsnouveau.client.gui;
+import com.hollingsworth.arsnouveau.api.client.ITooltipProvider;
+import com.hollingsworth.arsnouveau.common.items.ItemScroll;
+import com.hollingsworth.arsnouveau.setup.Config;
+import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.Tesselator;
+import com.mojang.math.Matrix4f;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.GuiComponent;
+import net.minecraft.client.gui.Font;
+import net.minecraft.client.gui.screens.inventory.tooltip.ClientTooltipComponent;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.FormattedText;
+import net.minecraft.network.chat.Style;
+import net.minecraft.util.Mth;
+import net.minecraft.world.entity.decoration.ItemFrame;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.GameType;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.EntityHitResult;
+import net.minecraft.world.phys.HitResult;
+import net.minecraftforge.client.ForgeHooksClient;
+import net.minecraftforge.client.event.RenderTooltipEvent;
+import net.minecraftforge.client.gui.ForgeIngameGui;
+import net.minecraftforge.client.gui.GuiUtils;
+import net.minecraftforge.client.gui.IIngameOverlay;
+import net.minecraftforge.common.MinecraftForge;
 
+import javax.annotation.Nonnull;
+import java.util.ArrayList;
 import java.util.List;
-public class GuiEntityInfoHUD extends GuiComponent {
 
-    private static final Minecraft minecraft = Minecraft.getInstance();
+import static com.hollingsworth.arsnouveau.common.items.ItemScroll.ITEM_PREFIX;
 
-    public void drawHUD(PoseStack ms, List<Component> tooltips){
-        int offsetLeft = 5;
-        fill(ms, offsetLeft, 50, 100+ offsetLeft, 0, 300);
-        int counter = 0;
-        if(tooltips == null)
+
+public class GuiEntityInfoHUD {
+    public static final IIngameOverlay OVERLAY = GuiEntityInfoHUD::renderOverlay;
+
+
+    public static int hoverTicks = 0;
+    public static Object lastHovered = null;
+
+
+    public static void renderOverlay(ForgeIngameGui gui, PoseStack poseStack, float partialTicks, int width,
+                                     int height) {
+        Minecraft mc = Minecraft.getInstance();
+        if (mc.options.hideGui || mc.gameMode.getPlayerMode() == GameType.SPECTATOR)
             return;
-        for(Component s: tooltips){
-            minecraft.font.drawShadow(ms, s.getString(), offsetLeft, 5f + 10 * counter, 0xFFFFFF);
-            counter++;
+
+        HitResult objectMouseOver = mc.hitResult;
+        List<Component> tooltip = new ArrayList<>();
+        Object hovering = null;
+        if(objectMouseOver instanceof BlockHitResult hitResult){
+            hovering = hitResult.getBlockPos();
+            if(mc.level.getBlockEntity(hitResult.getBlockPos()) instanceof ITooltipProvider iTooltipProvider){
+                iTooltipProvider.getTooltip(tooltip);
+            }
+        }else if(objectMouseOver instanceof EntityHitResult result){
+            if (result.getEntity() instanceof ITooltipProvider iTooltipProvider) {
+                iTooltipProvider.getTooltip(tooltip);
+            }
+            if (result.getEntity() instanceof ItemFrame frame) {
+                List<ItemStack> stacks = new ArrayList<>();
+                ItemStack stack = frame.getItem();
+                CompoundTag tag = stack.getOrCreateTag();
+                for(String s : tag.getAllKeys()){
+                    if(s.contains(ITEM_PREFIX)){
+                       tooltip.add(ItemStack.of(tag.getCompound(s)).getHoverName());
+                    }
+                }
+            }
+            hovering = result.getEntity();
         }
+
+        if (hovering == null || (lastHovered != null && !lastHovered.equals(hovering))) {
+            lastHovered = null;
+            hoverTicks = 0;
+        }
+
+        if (lastHovered == null || lastHovered.equals(hovering))
+            hoverTicks++;
+        else
+            hoverTicks = 0;
+        lastHovered = hovering;
+
+
+        if (tooltip.isEmpty()) {
+            return;
+        }
+        poseStack.pushPose();
+
+        int tooltipTextWidth = 0;
+        for (FormattedText textLine : tooltip) {
+            int textLineWidth = mc.font.width(textLine);
+            if (textLineWidth > tooltipTextWidth)
+                tooltipTextWidth = textLineWidth;
+        }
+
+        int tooltipHeight = 8;
+        if (tooltip.size() > 1) {
+            tooltipHeight += 2; // gap between title lines and next lines
+            tooltipHeight += (tooltip.size() - 1) * 10;
+        }
+        int xOffset = Config.TOOLTIP_X_OFFSET.get();
+        int posX = width / 2 +  xOffset;
+        int posY = height / 2 + + Config.TOOLTIP_Y_OFFSET.get();
+
+        posX = Math.min(posX, width - tooltipTextWidth - 20);
+        posY = Math.min(posY, height - tooltipHeight - 20);
+
+        float fade = Mth.clamp((hoverTicks + partialTicks) / 12f, 0, 1);
+        Color colorBackground = VANILLA_TOOLTIP_BACKGROUND.scaleAlpha(.75f);
+        Color colorBorderTop = VANILLA_TOOLTIP_BORDER_1;
+        Color colorBorderBot = VANILLA_TOOLTIP_BORDER_2;
+
+        if (fade < 1) {
+            poseStack.translate((1 - fade) * Math.signum(xOffset + .5f) * 4, 0, 0);
+            colorBackground.scaleAlpha(fade);
+            colorBorderTop.scaleAlpha(fade);
+            colorBorderBot.scaleAlpha(fade);
+        }
+
+        drawHoveringText(ItemStack.EMPTY, poseStack, tooltip,  posX, posY, width, height, -1, colorBackground.getRGB(),
+                colorBorderTop.getRGB(), colorBorderBot.getRGB(), mc.font);
+    }
+    public static final Color VANILLA_TOOLTIP_BORDER_1 = new Color(0x50_5000ff, true);
+    public static final Color VANILLA_TOOLTIP_BORDER_2 = new Color(0x50_28007f, true);
+    public static final Color VANILLA_TOOLTIP_BACKGROUND =  new Color(0xf0_100010, true);
+
+
+    public static void drawHoveringText(PoseStack mStack, List<? extends FormattedText> textLines, int mouseX,
+                                        int mouseY, int screenWidth, int screenHeight, int maxTextWidth, Font font) {
+        drawHoveringText(mStack, textLines, mouseX, mouseY, screenWidth, screenHeight, maxTextWidth,
+                GuiUtils.DEFAULT_BACKGROUND_COLOR, GuiUtils.DEFAULT_BORDER_COLOR_START, GuiUtils.DEFAULT_BORDER_COLOR_END,
+                font);
+    }
+
+    public static void drawHoveringText(PoseStack mStack, List<? extends FormattedText> textLines, int mouseX,
+                                        int mouseY, int screenWidth, int screenHeight, int maxTextWidth, int backgroundColor, int borderColorStart,
+                                        int borderColorEnd, Font font) {
+        drawHoveringText(ItemStack.EMPTY, mStack, textLines, mouseX, mouseY, screenWidth, screenHeight, maxTextWidth,
+                backgroundColor, borderColorStart, borderColorEnd, font);
+    }
+
+    public static void drawHoveringText(@Nonnull final ItemStack stack, PoseStack mStack,
+                                        List<? extends FormattedText> textLines, int mouseX, int mouseY, int screenWidth, int screenHeight,
+                                        int maxTextWidth, Font font) {
+        drawHoveringText(stack, mStack, textLines, mouseX, mouseY, screenWidth, screenHeight, maxTextWidth,
+                GuiUtils.DEFAULT_BACKGROUND_COLOR, GuiUtils.DEFAULT_BORDER_COLOR_START, GuiUtils.DEFAULT_BORDER_COLOR_END,
+                font);
+    }
+
+    public static void drawHoveringText(@Nonnull final ItemStack stack, PoseStack pStack,
+                                        List<? extends FormattedText> textLines, int mouseX, int mouseY, int screenWidth, int screenHeight,
+                                        int maxTextWidth, int backgroundColor, int borderColorStart, int borderColorEnd, Font font) {
+        if (textLines.isEmpty())
+            return;
+
+        List<ClientTooltipComponent> list = ForgeHooksClient.gatherTooltipComponents(stack, textLines,
+                stack.getTooltipImage(), mouseX, screenWidth, screenHeight, font, font);
+        RenderTooltipEvent.Pre event =
+                new RenderTooltipEvent.Pre(stack, pStack, mouseX, mouseY, screenWidth, screenHeight, font, list);
+        if (MinecraftForge.EVENT_BUS.post(event))
+            return;
+
+        mouseX = event.getX();
+        mouseY = event.getY();
+        screenWidth = event.getScreenWidth();
+        screenHeight = event.getScreenHeight();
+        font = event.getFont();
+
+        // RenderSystem.disableRescaleNormal();
+        RenderSystem.disableDepthTest();
+        int tooltipTextWidth = 0;
+
+        for (FormattedText textLine : textLines) {
+            int textLineWidth = font.width(textLine);
+            if (textLineWidth > tooltipTextWidth)
+                tooltipTextWidth = textLineWidth;
+        }
+
+        boolean needsWrap = false;
+
+        int titleLinesCount = 1;
+        int tooltipX = mouseX + 12;
+        if (tooltipX + tooltipTextWidth + 4 > screenWidth) {
+            tooltipX = mouseX - 16 - tooltipTextWidth;
+            if (tooltipX < 4) // if the tooltip doesn't fit on the screen
+            {
+                if (mouseX > screenWidth / 2)
+                    tooltipTextWidth = mouseX - 12 - 8;
+                else
+                    tooltipTextWidth = screenWidth - 16 - mouseX;
+                needsWrap = true;
+            }
+        }
+
+        if (maxTextWidth > 0 && tooltipTextWidth > maxTextWidth) {
+            tooltipTextWidth = maxTextWidth;
+            needsWrap = true;
+        }
+
+        if (needsWrap) {
+            int wrappedTooltipWidth = 0;
+            List<FormattedText> wrappedTextLines = new ArrayList<>();
+            for (int i = 0; i < textLines.size(); i++) {
+                FormattedText textLine = textLines.get(i);
+                List<FormattedText> wrappedLine = font.getSplitter()
+                        .splitLines(textLine, tooltipTextWidth, Style.EMPTY);
+                if (i == 0)
+                    titleLinesCount = wrappedLine.size();
+
+                for (FormattedText line : wrappedLine) {
+                    int lineWidth = font.width(line);
+                    if (lineWidth > wrappedTooltipWidth)
+                        wrappedTooltipWidth = lineWidth;
+                    wrappedTextLines.add(line);
+                }
+            }
+            tooltipTextWidth = wrappedTooltipWidth;
+            textLines = wrappedTextLines;
+
+            if (mouseX > screenWidth / 2)
+                tooltipX = mouseX - 16 - tooltipTextWidth;
+            else
+                tooltipX = mouseX + 12;
+        }
+
+        int tooltipY = mouseY - 12;
+        int tooltipHeight = 8;
+
+        if (textLines.size() > 1) {
+            tooltipHeight += (textLines.size() - 1) * 10;
+            if (textLines.size() > titleLinesCount)
+                tooltipHeight += 2; // gap between title lines and next lines
+        }
+
+        if (tooltipY < 4)
+            tooltipY = 4;
+        else if (tooltipY + tooltipHeight + 4 > screenHeight)
+            tooltipY = screenHeight - tooltipHeight - 4;
+
+        final int zLevel = 400;
+        RenderTooltipEvent.Color colorEvent = new RenderTooltipEvent.Color(stack, pStack, tooltipX, tooltipY,
+                font, backgroundColor, borderColorStart, borderColorEnd, list);
+        MinecraftForge.EVENT_BUS.post(colorEvent);
+        backgroundColor = colorEvent.getBackgroundStart();
+        borderColorStart = colorEvent.getBorderStart();
+        borderColorEnd = colorEvent.getBorderEnd();
+
+        pStack.pushPose();
+        Matrix4f mat = pStack.last()
+                .pose();
+        GuiUtils.drawGradientRect(mat, zLevel, tooltipX - 3, tooltipY - 4, tooltipX + tooltipTextWidth + 3,
+                tooltipY - 3, backgroundColor, backgroundColor);
+        GuiUtils.drawGradientRect(mat, zLevel, tooltipX - 3, tooltipY + tooltipHeight + 3,
+                tooltipX + tooltipTextWidth + 3, tooltipY + tooltipHeight + 4, backgroundColor, backgroundColor);
+        GuiUtils.drawGradientRect(mat, zLevel, tooltipX - 3, tooltipY - 3, tooltipX + tooltipTextWidth + 3,
+                tooltipY + tooltipHeight + 3, backgroundColor, backgroundColor);
+        GuiUtils.drawGradientRect(mat, zLevel, tooltipX - 4, tooltipY - 3, tooltipX - 3, tooltipY + tooltipHeight + 3,
+                backgroundColor, backgroundColor);
+        GuiUtils.drawGradientRect(mat, zLevel, tooltipX + tooltipTextWidth + 3, tooltipY - 3,
+                tooltipX + tooltipTextWidth + 4, tooltipY + tooltipHeight + 3, backgroundColor, backgroundColor);
+        GuiUtils.drawGradientRect(mat, zLevel, tooltipX - 3, tooltipY - 3 + 1, tooltipX - 3 + 1,
+                tooltipY + tooltipHeight + 3 - 1, borderColorStart, borderColorEnd);
+        GuiUtils.drawGradientRect(mat, zLevel, tooltipX + tooltipTextWidth + 2, tooltipY - 3 + 1,
+                tooltipX + tooltipTextWidth + 3, tooltipY + tooltipHeight + 3 - 1, borderColorStart, borderColorEnd);
+        GuiUtils.drawGradientRect(mat, zLevel, tooltipX - 3, tooltipY - 3, tooltipX + tooltipTextWidth + 3,
+                tooltipY - 3 + 1, borderColorStart, borderColorStart);
+        GuiUtils.drawGradientRect(mat, zLevel, tooltipX - 3, tooltipY + tooltipHeight + 2,
+                tooltipX + tooltipTextWidth + 3, tooltipY + tooltipHeight + 3, borderColorEnd, borderColorEnd);
+
+        MultiBufferSource.BufferSource renderType = MultiBufferSource.immediate(Tesselator.getInstance()
+                .getBuilder());
+        pStack.translate(0.0D, 0.0D, zLevel);
+
+        for (int lineNumber = 0; lineNumber < list.size(); ++lineNumber) {
+            ClientTooltipComponent line = list.get(lineNumber);
+
+            if (line != null)
+                line.renderText(font, tooltipX, tooltipY, mat, renderType);
+
+            if (lineNumber + 1 == titleLinesCount)
+                tooltipY += 2;
+
+            tooltipY += 10;
+        }
+
+        renderType.endBatch();
+        pStack.popPose();
+
+        RenderSystem.enableDepthTest();
     }
 }
--- src/main/java/com/hollingsworth/arsnouveau/client/gui/HUDEventHandler.java
+++ src/main/java/com/hollingsworth/arsnouveau/client/gui/HUDEventHandler.java
@@ -1,13 +1,7 @@
 package com.hollingsworth.arsnouveau.client.gui;
 
 import com.hollingsworth.arsnouveau.ArsNouveau;
-import com.hollingsworth.arsnouveau.api.client.ITooltipProvider;
-import net.minecraft.client.Minecraft;
-import net.minecraft.core.BlockPos;
-import net.minecraft.network.chat.Component;
-import net.minecraft.world.entity.decoration.ItemFrame;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.EntityHitResult;
 import net.minecraft.world.phys.HitResult;
 import net.minecraftforge.api.distmarker.Dist;
@@ -15,22 +9,11 @@ import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
-import java.util.ArrayList;
-import java.util.List;
-
-
-/**
- * Renders this mod's HUDs.
- *
- * @author Choonster
- */
 @Mod.EventBusSubscriber(value = Dist.CLIENT, modid = ArsNouveau.MODID)
 public class HUDEventHandler {
-    private static final Minecraft minecraft = Minecraft.getInstance();
     private static final GuiSpellHUD spellHUD = new GuiSpellHUD();
     private static final GuiManaHUD manaHUD = new GuiManaHUD();
-    private static final GuiEntityInfoHUD entityHUD = new GuiEntityInfoHUD();
-    private static final GuiScrollHUD scrollHUD = new GuiScrollHUD();
+
     /**
      * Render the current spell when the SpellBook is held in the players hand
      *
@@ -39,39 +22,7 @@ public class HUDEventHandler {
     @SubscribeEvent
     public static void renderSpellHUD(final RenderGameOverlayEvent.Post event) {
         if (event.getType() != RenderGameOverlayEvent.ElementType.ALL) return;
-
-        final Player player = minecraft.player;
-
         spellHUD.drawHUD(event.getMatrixStack());
         manaHUD.drawHUD(event.getMatrixStack(), event.getPartialTicks());
-
-    }
-
-    @SubscribeEvent
-    public static void renderEntityHUD(final RenderGameOverlayEvent.Post event) {
-        if (event.getType() != RenderGameOverlayEvent.ElementType.ALL) return;
-        HitResult mouseOver = Minecraft.getInstance().hitResult;
-        if (mouseOver != null && mouseOver.getType() == HitResult.Type.ENTITY) {
-
-          EntityHitResult result = (EntityHitResult) mouseOver;
-          if(result.getEntity() instanceof ITooltipProvider) {
-              List<Component> components = new ArrayList<>();
-              ((ITooltipProvider) result.getEntity()).getTooltip(components);
-              entityHUD.drawHUD(event.getMatrixStack(), components);
-          }
-          if(result.getEntity() instanceof ItemFrame){
-              scrollHUD.drawHUD(event.getMatrixStack(), (ItemFrame) result.getEntity());
-          }
-
-        }
-        if (mouseOver != null && mouseOver.getType() == HitResult.Type.BLOCK) {
-            BlockHitResult result = (BlockHitResult) mouseOver;
-            BlockPos pos = result.getBlockPos();
-            if(Minecraft.getInstance().level != null && Minecraft.getInstance().level.getBlockEntity(pos) instanceof ITooltipProvider tooltipProvider){
-                List<Component> components = new ArrayList<>();
-                tooltipProvider.getTooltip(components);
-                entityHUD.drawHUD(event.getMatrixStack(), components);
-            }
-        }
     }
 }
