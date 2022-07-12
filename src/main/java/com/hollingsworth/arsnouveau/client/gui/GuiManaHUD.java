78df8bc247b7f3be026c245b823c452531bcc84a
Bailey Hollingsworth <baileyholl@gmail.com>
2022-07-11 20:56:35 -0500

    Add tooltip overlay (#605)
    * Add tooltip overlay
    
    * Add configs to offset tooltips and mana bar

--- src/main/java/com/hollingsworth/arsnouveau/client/gui/GuiManaHUD.java
+++ src/main/java/com/hollingsworth/arsnouveau/client/gui/GuiManaHUD.java
@@ -6,6 +6,7 @@ import com.hollingsworth.arsnouveau.api.mana.IManaCap;
 import com.hollingsworth.arsnouveau.api.util.ManaUtil;
 import com.hollingsworth.arsnouveau.client.ClientInfo;
 import com.hollingsworth.arsnouveau.common.capability.CapabilityRegistry;
+import com.hollingsworth.arsnouveau.setup.Config;
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.Minecraft;
@@ -32,21 +33,21 @@ public class GuiManaHUD extends GuiComponent {
         if (mana == null)
             return;
 
-        int offsetLeft = 10;
+        int offsetLeft = 10 + Config.MANABAR_X_OFFSET.get();
         int manaLength = 96;
         manaLength = (int) ((manaLength) * ((mana.getCurrentMana()) / ((double) mana.getMaxMana() - 0.0)));
 
-        int height = minecraft.getWindow().getGuiScaledHeight() - 5;
+        int yOffset = minecraft.getWindow().getGuiScaledHeight() - 5 + Config.MANABAR_Y_OFFSET.get();
 
         RenderSystem.setShaderTexture(0, new ResourceLocation(ArsNouveau.MODID, "textures/gui/manabar_gui_border.png"));
-        blit(ms, offsetLeft, height - 18, 0, 0, 108, 18, 256, 256);
+        blit(ms, offsetLeft, yOffset - 18, 0, 0, 108, 18, 256, 256);
         int manaOffset = (int) (((ClientInfo.ticksInGame + pt) / 3 % (33))) * 6;
 
         // 96
         RenderSystem.setShaderTexture(0, new ResourceLocation(ArsNouveau.MODID, "textures/gui/manabar_gui_mana.png"));
-        blit(ms, offsetLeft + 9, height - 9, 0, manaOffset, manaLength, 6, 256, 256);
+        blit(ms, offsetLeft + 9, yOffset - 9, 0, manaOffset, manaLength, 6, 256, 256);
 
         RenderSystem.setShaderTexture(0, new ResourceLocation(ArsNouveau.MODID, "textures/gui/manabar_gui_border.png"));
-        blit(ms, offsetLeft, height - 17, 0, 18, 108, 20, 256, 256);
+        blit(ms, offsetLeft, yOffset - 17, 0, 18, 108, 20, 256, 256);
     }
 }
