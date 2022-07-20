722dac2b15b7644cde6c8be07b9c306fe14e2969
Bailey Hollingsworth <baileyholl@gmail.com>
2022-07-20 18:26:27 -0500

    Vanilla lights (#622)
    * Add configs for lights
    
    * Add base mob and item support

--- src/main/java/com/hollingsworth/arsnouveau/ArsNouveau.java
+++ src/main/java/com/hollingsworth/arsnouveau/ArsNouveau.java
@@ -43,12 +43,12 @@ public class ArsNouveau {
     };
 
     public ArsNouveau(){
+        Mod.EventBusSubscriber.Bus.FORGE.bus().get().register(FMLEventHandler.class);
         caelusLoaded = ModList.get().isLoaded("caelus");
         APIRegistry.setup();
         ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SERVER_CONFIG);
         ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, Config.CLIENT_CONFIG);
         DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> () -> Mod.EventBusSubscriber.Bus.FORGE.bus().get().register(ClientEventHandler.class));
-        Mod.EventBusSubscriber.Bus.FORGE.bus().get().register(FMLEventHandler.class);
 
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::postModLoadEvent);
--- src/main/java/com/hollingsworth/arsnouveau/common/light/DynamLightUtil.java
+++ src/main/java/com/hollingsworth/arsnouveau/common/light/DynamLightUtil.java
@@ -1,7 +1,10 @@
 package com.hollingsworth.arsnouveau.common.light;
 
+import com.hollingsworth.arsnouveau.setup.Config;
 import net.minecraft.util.Mth;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.ItemLike;
 
 import java.util.function.Function;
 
@@ -16,8 +19,12 @@ public class DynamLightUtil {
         return coord >> 4;
     }
 
-    public static int getLuminance(Entity entity){
+    private static int getLuminance(Entity entity){
         int level = 0;
+        if(entity.isOnFire())
+            return 15;
+        if(Config.ENTITY_LIGHT_MAP.containsKey(entity.getType().getRegistryName()))
+            return Config.ENTITY_LIGHT_MAP.get(entity.getType().getRegistryName());
         if(LightManager.getLightRegistry().containsKey(entity.getType())){
             for(Function<Entity, Integer> function : LightManager.getLightRegistry().get(entity.getType())){
                 int val = function.apply(entity);
@@ -25,6 +32,33 @@ public class DynamLightUtil {
             }
         }
         return Math.min(15, level);
+    }
+
+    public static boolean couldGiveLight(Entity entity){
+        return LightManager.getLightRegistry().containsKey(entity.getType()) || Config.ENTITY_LIGHT_MAP.containsKey(entity.getType().getRegistryName()) || (entity instanceof Player player && getPlayerLight(player) > 0);
+    }
+
+    public static int getPlayerLight(Player player){
+        int mainLight = Config.ITEM_LIGHTMAP.getOrDefault(player.getMainHandItem().getItem().getRegistryName(), 0);
+        int offHandLight = Config.ITEM_LIGHTMAP.getOrDefault(player.getOffhandItem().getItem().getRegistryName(), 0);
+        return Math.max(mainLight, offHandLight);
+    }
+
+    public static int lightForEntity(Entity entity){
+        int light = 0;
+        if(entity instanceof Player player){
+            light = getPlayerLight(player);
+        }
+        if(light < 15 && LightManager.containsEntity(entity.getType())) {
+            int entityLuminance = getLuminance(entity);
+            return Math.max(entityLuminance, light);
+        }
+
+        return Math.min(15, light);
+    }
+
+    public static int fromItemLike(ItemLike itemLike){
+        return Config.ITEM_LIGHTMAP.getOrDefault(itemLike.asItem().getRegistryName(), 0);
     }
 
 }
