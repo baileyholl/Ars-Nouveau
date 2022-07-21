6b394819b3ed3da114b7a62985daf230859f346f
Bailey Hollingsworth <baileyholl@gmail.com>
2022-07-21 18:26:35 -0500

    Add wixie hat assets

--- src/main/java/com/hollingsworth/arsnouveau/common/items/WixieHat.java
+++ src/main/java/com/hollingsworth/arsnouveau/common/items/WixieHat.java
@@ -1,8 +1,13 @@
 package com.hollingsworth.arsnouveau.common.items;
 
+import com.hollingsworth.arsnouveau.api.item.ICosmeticItem;
+import com.hollingsworth.arsnouveau.client.renderer.item.GenericItemRenderer;
+import com.hollingsworth.arsnouveau.client.renderer.tile.GenericModel;
 import com.hollingsworth.arsnouveau.common.entity.Starbuncle;
 import com.hollingsworth.arsnouveau.common.entity.goal.carbuncle.StarbyPotionBehavior;
 import com.hollingsworth.arsnouveau.common.util.PortUtil;
+import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer;
+import net.minecraft.client.renderer.block.model.ItemTransforms;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionHand;
@@ -11,14 +16,20 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.phys.Vec3;
+import net.minecraftforge.client.extensions.common.IClientItemExtensions;
 
-public class WixieHat extends ModItem{
+import java.util.function.Consumer;
+
+public class WixieHat extends AnimModItem implements ICosmeticItem {
 
     @Override
     public InteractionResult interactLivingEntity(ItemStack pStack, Player pPlayer, LivingEntity pInteractionTarget, InteractionHand pUsedHand) {
         if(pInteractionTarget instanceof Starbuncle starbuncle){
             starbuncle.setBehavior(new StarbyPotionBehavior(starbuncle, new CompoundTag()));
             PortUtil.sendMessage(pPlayer, Component.translatable("ars_nouveau.starbuncle.potion_behavior_set"));
+            starbuncle.setCosmeticItem(pStack.split(1));
+            return InteractionResult.SUCCESS;
         }
         return super.interactLivingEntity(pStack, pPlayer, pInteractionTarget, pUsedHand);
     }
@@ -26,5 +37,39 @@ public class WixieHat extends ModItem{
     @Override
     public InteractionResult useOn(UseOnContext pContext) {
         return super.useOn(pContext);
+    }
+
+    @Override
+    public void initializeClient(Consumer<IClientItemExtensions> consumer) {
+        super.initializeClient(consumer);
+        consumer.accept(new IClientItemExtensions() {
+            private final BlockEntityWithoutLevelRenderer renderer = new GenericItemRenderer(new GenericModel<>("witch_hat", "items")).withTranslucency();
+
+            @Override
+            public BlockEntityWithoutLevelRenderer getCustomRenderer() {
+                return renderer;
+            }
+        });
+    }
+
+    @Override
+    public Vec3 getTranslations() {
+        return new Vec3(0, -.24, -.1);
+    }
+
+
+    @Override
+    public Vec3 getScaling() {
+        return new Vec3(1.0, 1.0, 1.0);
+    }
+
+    @Override
+    public boolean canWear(LivingEntity entity) {
+        return entity instanceof Starbuncle;
+    }
+
+    @Override
+    public ItemTransforms.TransformType getTransformType() {
+        return ItemTransforms.TransformType.NONE;
     }
 }
