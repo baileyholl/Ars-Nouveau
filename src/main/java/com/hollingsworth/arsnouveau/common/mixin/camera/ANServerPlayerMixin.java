0326d84f0e6e3c90813ffd073514fbe22950258a
Bailey Hollingsworth <baileyholl@gmail.com>
2022-08-18 11:12:28 -0500

    Attempt to add mixin extras AND FAIL

--- src/main/java/com/hollingsworth/arsnouveau/common/mixin/camera/ANServerPlayerMixin.java
+++ src/main/java/com/hollingsworth/arsnouveau/common/mixin/camera/ANServerPlayerMixin.java
@@ -1,10 +1,10 @@
 package com.hollingsworth.arsnouveau.common.mixin.camera;
 
 import com.hollingsworth.arsnouveau.common.util.CameraUtil;
+import com.llamalad7.mixinextras.injector.WrapWithCondition;
 import net.minecraft.server.level.ServerPlayer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 /**
  * https://github.com/Geforce132/SecurityCraft/blob/1.18.2/src/main/java/net/geforcemods/securitycraft/mixin/camera/ServerPlayerMixin.java
@@ -12,10 +12,8 @@ import org.spongepowered.asm.mixin.injection.Redirect;
  */
 @Mixin(value = ServerPlayer.class, priority = 1000)
 public class ANServerPlayerMixin {
-    @Redirect(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/level/ServerPlayer;absMoveTo(DDDFF)V"))
-    private void an_tick(ServerPlayer player, double x, double y, double z, float yaw, float pitch) {
-        if (!CameraUtil.isPlayerMountedOnCamera(player)) {
-            player.absMoveTo(x, y, z, yaw, pitch);
-        }
+    @WrapWithCondition(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/level/ServerPlayer;absMoveTo(DDDFF)V"))
+    private boolean shouldMove(ServerPlayer player, double x, double y, double z, float yaw, float pitch) {
+        return !CameraUtil.isPlayerMountedOnCamera(player);
     }
 }
