f7109e54266f54d4467f1f3b252bbdbec86ec6c3
Bailey Hollingsworth <baileyholl@gmail.com>
2022-09-05 11:20:30 -0500

    Remove deprecations, needed for geckolib

--- src/main/java/com/hollingsworth/arsnouveau/common/armor/HeavyArmor.java
+++ src/main/java/com/hollingsworth/arsnouveau/common/armor/HeavyArmor.java
@@ -3,7 +3,6 @@ package com.hollingsworth.arsnouveau.common.armor;
 import com.hollingsworth.arsnouveau.setup.ItemsRegistry;
 import net.minecraft.world.entity.EquipmentSlot;
 
-@Deprecated(forRemoval = true)
 public class HeavyArmor extends AnimatedMagicArmor {
 
     public HeavyArmor(EquipmentSlot slot) {
--- src/main/java/com/hollingsworth/arsnouveau/common/armor/MediumArmor.java
+++ src/main/java/com/hollingsworth/arsnouveau/common/armor/MediumArmor.java
@@ -3,7 +3,6 @@ package com.hollingsworth.arsnouveau.common.armor;
 import com.hollingsworth.arsnouveau.setup.ItemsRegistry;
 import net.minecraft.world.entity.EquipmentSlot;
 
-@Deprecated(forRemoval = true)
 public class MediumArmor extends AnimatedMagicArmor {
 
     public MediumArmor(EquipmentSlot slot) {
