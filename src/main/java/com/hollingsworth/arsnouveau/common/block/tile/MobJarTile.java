a55d66fae325ed6d97ac39c5ffbbb7fe6928c614
Bailey Hollingsworth <baileyholl@gmail.com>
2022-09-18 10:52:00 -0500

    Dispel jar

--- src/main/java/com/hollingsworth/arsnouveau/common/block/tile/MobJarTile.java
+++ src/main/java/com/hollingsworth/arsnouveau/common/block/tile/MobJarTile.java
@@ -1,18 +1,24 @@
 package com.hollingsworth.arsnouveau.common.block.tile;
 
+import com.hollingsworth.arsnouveau.api.entity.IDispellable;
+import com.hollingsworth.arsnouveau.api.util.BlockUtil;
 import com.hollingsworth.arsnouveau.common.block.ITickable;
 import com.hollingsworth.arsnouveau.setup.BlockRegistry;
 import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.Connection;
+import net.minecraft.network.protocol.game.ClientboundBlockEntityDataPacket;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.level.block.state.BlockState;
+import org.jetbrains.annotations.NotNull;
 
 import javax.annotation.Nullable;
 import java.util.function.Function;
 
-public class MobJarTile extends ModdedTile implements ITickable {
+public class MobJarTile extends ModdedTile implements ITickable, IDispellable {
     @Nullable
     public Entity displayEntity;
 
@@ -53,6 +59,20 @@ public class MobJarTile extends ModdedTile implements ITickable {
     }
 
     @Override
+    public boolean onDispel(@NotNull LivingEntity caster) {
+        if(entityTag == null)
+            return false;
+        Entity entity = getEntity();
+        if(entity == null)
+            return false;
+        entity.setPos(getBlockPos().getX() + 0.5, getBlockPos().getY() + 1, getBlockPos().getZ() + 0.5);
+        level.addFreshEntity(entity);
+        this.entityTag = null;
+        updateBlock();
+        return true;
+    }
+
+    @Override
     public void saveAdditional(CompoundTag tag) {
         super.saveAdditional(tag);
         if(entityTag != null){
@@ -66,5 +86,12 @@ public class MobJarTile extends ModdedTile implements ITickable {
         if(pTag.contains("entityTag")){
             this.entityTag = pTag.getCompound("entityTag");
         }
+    }
+
+    @Override
+    public void onDataPacket(Connection net, ClientboundBlockEntityDataPacket pkt) {
+        this.displayEntity = null;
+        this.entityTag = null;
+        super.onDataPacket(net, pkt);
     }
 }
