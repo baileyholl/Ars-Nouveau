ea895be0020c64f9f05861d567308d90f0d7b748
Bailey Hollingsworth <baileyholl@gmail.com>
2022-10-26 18:03:04 -0500

    Fix tickable

--- src/main/java/com/hollingsworth/arsnouveau/common/block/RepositoryBlock.java
+++ src/main/java/com/hollingsworth/arsnouveau/common/block/RepositoryBlock.java
@@ -2,11 +2,12 @@ package com.hollingsworth.arsnouveau.common.block;
 
 import com.hollingsworth.arsnouveau.common.block.tile.RepositoryTile;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.block.EntityBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import org.jetbrains.annotations.Nullable;
 
-public class RepositoryBlock extends TickableModBlock {
+public class RepositoryBlock extends ModBlock implements EntityBlock {
     public RepositoryBlock() {
         super(ModBlock.defaultProperties());
     }
