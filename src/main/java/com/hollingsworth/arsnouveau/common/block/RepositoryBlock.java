02bd1d774ec9c10772c32b51885249c4965bbad4
Bailey Hollingsworth <baileyholl@gmail.com>
2022-10-26 23:17:12 -0500

    add repository storage

--- src/main/java/com/hollingsworth/arsnouveau/common/block/RepositoryBlock.java
+++ src/main/java/com/hollingsworth/arsnouveau/common/block/RepositoryBlock.java
@@ -2,14 +2,65 @@ package com.hollingsworth.arsnouveau.common.block;
 
 import com.hollingsworth.arsnouveau.common.block.tile.RepositoryTile;
 import net.minecraft.core.BlockPos;
+import net.minecraft.stats.Stats;
+import net.minecraft.world.Container;
+import net.minecraft.world.Containers;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.monster.piglin.PiglinAi;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.EntityBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.BlockHitResult;
 import org.jetbrains.annotations.Nullable;
 
 public class RepositoryBlock extends ModBlock implements EntityBlock {
     public RepositoryBlock() {
         super(ModBlock.defaultProperties());
+    }
+
+    /**
+     * Called by BlockItem after this block has been placed.
+     */
+    public void setPlacedBy(Level pLevel, BlockPos pPos, BlockState pState, @javax.annotation.Nullable LivingEntity pPlacer, ItemStack pStack) {
+        if (pStack.hasCustomHoverName()) {
+            BlockEntity blockentity = pLevel.getBlockEntity(pPos);
+            if (blockentity instanceof RepositoryTile tile) {
+                tile.setCustomName(pStack.getHoverName());
+            }
+        }
+
+    }
+
+    public InteractionResult use(BlockState pState, Level pLevel, BlockPos pPos, Player pPlayer, InteractionHand pHand, BlockHitResult pHit) {
+        if (pLevel.isClientSide) {
+            return InteractionResult.SUCCESS;
+        } else {
+            BlockEntity blockentity = pLevel.getBlockEntity(pPos);
+            if (blockentity instanceof RepositoryTile tile) {
+                pPlayer.openMenu(tile);
+                pPlayer.awardStat(Stats.OPEN_BARREL);
+                PiglinAi.angerNearbyPiglins(pPlayer, true);
+            }
+
+            return InteractionResult.CONSUME;
+        }
+    }
+
+    public void onRemove(BlockState pState, Level pLevel, BlockPos pPos, BlockState pNewState, boolean pIsMoving) {
+        if (!pState.is(pNewState.getBlock())) {
+            BlockEntity blockentity = pLevel.getBlockEntity(pPos);
+            if (blockentity instanceof Container container) {
+                Containers.dropContents(pLevel, pPos, container);
+                pLevel.updateNeighbourForOutputSignal(pPos, this);
+            }
+
+            super.onRemove(pState, pLevel, pPos, pNewState, pIsMoving);
+        }
     }
 
     @Nullable
--- src/main/java/com/hollingsworth/arsnouveau/common/block/tile/RepositoryTile.java
+++ src/main/java/com/hollingsworth/arsnouveau/common/block/tile/RepositoryTile.java
@@ -2,10 +2,99 @@ package com.hollingsworth.arsnouveau.common.block.tile;
 
 import com.hollingsworth.arsnouveau.setup.BlockRegistry;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.NonNullList;
+import net.minecraft.core.Vec3i;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.Component;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.Container;
+import net.minecraft.world.ContainerHelper;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.inventory.ChestMenu;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.BarrelBlock;
+import net.minecraft.world.level.block.entity.ContainerOpenersCounter;
+import net.minecraft.world.level.block.entity.RandomizableContainerBlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 
-public class RepositoryTile extends ModdedTile{
+public class RepositoryTile extends RandomizableContainerBlockEntity {
+    private NonNullList<ItemStack> items = NonNullList.withSize(27, ItemStack.EMPTY);
+    private ContainerOpenersCounter openersCounter = new ContainerOpenersCounter() {
+        protected void onOpen(Level p_155062_, BlockPos p_155063_, BlockState p_155064_) {
+            RepositoryTile.this.playSound(p_155064_, SoundEvents.BARREL_OPEN);
+        }
+
+        protected void onClose(Level p_155072_, BlockPos p_155073_, BlockState p_155074_) {
+            RepositoryTile.this.playSound(p_155074_, SoundEvents.BARREL_CLOSE);
+        }
+
+        protected void openerCountChanged(Level p_155066_, BlockPos p_155067_, BlockState p_155068_, int p_155069_, int p_155070_) {
+        }
+
+        protected boolean isOwnContainer(Player p_155060_) {
+            if (p_155060_.containerMenu instanceof ChestMenu) {
+                Container container = ((ChestMenu)p_155060_.containerMenu).getContainer();
+                return container == RepositoryTile.this;
+            } else {
+                return false;
+            }
+        }
+    };
+
     public RepositoryTile(BlockPos pos, BlockState state) {
         super(BlockRegistry.REPOSITORY_TILE, pos, state);
+    }
+
+    @Override
+    protected NonNullList<ItemStack> getItems() {
+        return items;
+    }
+
+    @Override
+    protected void setItems(NonNullList<ItemStack> pItemStacks) {
+        items = pItemStacks;
+    }
+
+    protected Component getDefaultName() {
+        return Component.translatable("container.barrel");
+    }
+
+    protected AbstractContainerMenu createMenu(int pId, Inventory pPlayer) {
+        return ChestMenu.threeRows(pId, pPlayer, this);
+    }
+
+    @Override
+    public int getContainerSize() {
+        return 27;
+    }
+
+    protected void saveAdditional(CompoundTag pTag) {
+        super.saveAdditional(pTag);
+        if (!this.trySaveLootTable(pTag)) {
+            ContainerHelper.saveAllItems(pTag, this.items);
+        }
+
+    }
+
+    public void load(CompoundTag pTag) {
+        super.load(pTag);
+        this.items = NonNullList.withSize(this.getContainerSize(), ItemStack.EMPTY);
+        if (!this.tryLoadLootTable(pTag)) {
+            ContainerHelper.loadAllItems(pTag, this.items);
+        }
+    }
+
+
+    void playSound(BlockState pState, SoundEvent pSound) {
+        Vec3i vec3i = pState.getValue(BarrelBlock.FACING).getNormal();
+        double d0 = (double)this.worldPosition.getX() + 0.5D + (double)vec3i.getX() / 2.0D;
+        double d1 = (double)this.worldPosition.getY() + 0.5D + (double)vec3i.getY() / 2.0D;
+        double d2 = (double)this.worldPosition.getZ() + 0.5D + (double)vec3i.getZ() / 2.0D;
+        this.level.playSound((Player)null, d0, d1, d2, pSound, SoundSource.BLOCKS, 0.5F, this.level.random.nextFloat() * 0.1F + 0.9F);
     }
 }
