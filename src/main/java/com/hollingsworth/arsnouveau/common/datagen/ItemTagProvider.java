5d65f39437d258988cbba320f211492140f65e62
Alexthw46 <77152778+Alexthw46@users.noreply.github.com>
2022-11-15 11:27:55 +0100

    Tags for archw leaf items

--- src/main/java/com/hollingsworth/arsnouveau/common/datagen/ItemTagProvider.java
+++ src/main/java/com/hollingsworth/arsnouveau/common/datagen/ItemTagProvider.java
@@ -73,7 +73,8 @@ public class ItemTagProvider extends ItemTagsProvider {
                 BlockRegistry.BLAZING_WOOD.asItem(),
                 BlockRegistry.CASCADING_WOOD.asItem(),
                 BlockRegistry.FLOURISHING_WOOD.asItem(),
-                BlockRegistry.VEXING_WOOD.asItem(), BlockRegistry.STRIPPED_AWLOG_BLUE.asItem(),
+                BlockRegistry.VEXING_WOOD.asItem(),
+                BlockRegistry.STRIPPED_AWLOG_BLUE.asItem(),
                 BlockRegistry.STRIPPED_AWWOOD_BLUE.asItem(),
                 BlockRegistry.STRIPPED_AWLOG_GREEN.asItem(),
                 BlockRegistry.STRIPPED_AWWOOD_GREEN.asItem(),
@@ -82,6 +83,10 @@ public class ItemTagProvider extends ItemTagsProvider {
                 BlockRegistry.STRIPPED_AWLOG_PURPLE.asItem(),
                 BlockRegistry.STRIPPED_AWWOOD_PURPLE.asItem()
         );
+        this.tag(ItemTags.LEAVES).add(BlockRegistry.VEXING_LEAVES.asItem(),
+                BlockRegistry.CASCADING_LEAVE.asItem(),
+                BlockRegistry.BLAZING_LEAVES.asItem(),
+                BlockRegistry.FLOURISHING_LEAVES.asItem());
         this.tag(ItemTags.LOGS_THAT_BURN).add(
                 BlockRegistry.BLAZING_LOG.asItem(),
                 BlockRegistry.CASCADING_LOG.asItem(),
