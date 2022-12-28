1d9777ac644da066b9260ff7de68c339f26a4269
Bailey Hollingsworth <baileyholl@gmail.com>
2022-12-28 13:02:54 -0600

    extra parenthesis

--- src/main/java/com/hollingsworth/arsnouveau/common/block/BasicSpellTurret.java
+++ src/main/java/com/hollingsworth/arsnouveau/common/block/BasicSpellTurret.java
@@ -100,7 +100,7 @@ public class BasicSpellTurret extends TickableModBlock implements SimpleWaterlog
                     LivingEntity entity = entityList.get(serverLevel.random.nextInt(entityList.size()));
                     resolver.onCastOnEntity(ItemStack.EMPTY, entity, InteractionHand.MAIN_HAND);
                 } else {
-                    Vec3 hitVec = new Vec3(touchPos.getX() + facingDir.getStepX() * 0.5, touchPos.getY() + facingDir.getStepY() * 0.5, touchPos.getZ() + facingDir.getStepZ() * 0.5));
+                    Vec3 hitVec = new Vec3(touchPos.getX() + facingDir.getStepX() * 0.5, touchPos.getY() + facingDir.getStepY() * 0.5, touchPos.getZ() + facingDir.getStepZ() * 0.5);
                     resolver.onCastOnBlock(new BlockHitResult(hitVec, facingDir, new BlockPos(touchPos.getX(), touchPos.getY(), touchPos.getZ()), true));
                 }
             }
