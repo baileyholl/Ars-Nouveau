7b718496002f82c7c28040a2c57b74b0a9bdebe5
Bailey Hollingsworth <baileyholl@gmail.com>
2023-01-09 21:23:18 -0600

    Add in-game support to swap portal texture

--- src/main/java/com/hollingsworth/arsnouveau/common/block/PortalBlock.java
+++ src/main/java/com/hollingsworth/arsnouveau/common/block/PortalBlock.java
@@ -3,12 +3,15 @@ package com.hollingsworth.arsnouveau.common.block;
 import com.hollingsworth.arsnouveau.api.util.FlatPortalAreaHelper;
 import com.hollingsworth.arsnouveau.common.block.tile.PortalTile;
 import com.hollingsworth.arsnouveau.common.datagen.BlockTagProvider;
+import com.hollingsworth.arsnouveau.common.items.DominionWand;
 import com.hollingsworth.arsnouveau.common.items.WarpScroll;
 import com.hollingsworth.arsnouveau.setup.BlockRegistry;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.util.RandomSource;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.Projectile;
@@ -68,7 +71,20 @@ public class PortalBlock extends TickableModBlock {
 
             worldIn.addParticle(ParticleTypes.PORTAL, d0, d1, d2, d3, d4, d5);
         }
+    }
 
+    @Override
+    public InteractionResult use(BlockState pState, Level pLevel, BlockPos pPos, Player pPlayer, InteractionHand pHand, BlockHitResult pHit) {
+        if(pLevel.isClientSide || pHand != InteractionHand.MAIN_HAND)
+            return InteractionResult.SUCCESS;
+        if(pPlayer.getItemInHand(pHand).getItem() instanceof DominionWand){
+            if(pLevel.getBlockEntity(pPos) instanceof PortalTile tile){
+                tile.setUseAlternateRender(!tile.isUseAlternateRender());
+                tile.updateBlock();
+                return InteractionResult.CONSUME;
+            }
+        }
+        return super.use(pState, pLevel, pPos, pPlayer, pHand, pHit);
     }
 
     @Override
