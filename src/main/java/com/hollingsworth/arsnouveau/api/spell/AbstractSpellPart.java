c9687a1e716b4f167eaaddc31fbbd68041cf6cdc
Bailey Hollingsworth <baileyholl@gmail.com>
2023-01-22 12:12:47 -0600

    swap to default tier on fallback

--- src/main/java/com/hollingsworth/arsnouveau/api/spell/AbstractSpellPart.java
+++ src/main/java/com/hollingsworth/arsnouveau/api/spell/AbstractSpellPart.java
@@ -67,7 +67,7 @@ public abstract class AbstractSpellPart implements Comparable<AbstractSpellPart>
     }
 
     public SpellTier getConfigTier(){
-        return GLYPH_TIER == null ? SpellTier.ONE : SpellTier.SPELL_TIER_MAP.get(GLYPH_TIER.get());
+        return GLYPH_TIER == null ? getTier() : SpellTier.SPELL_TIER_MAP.get(GLYPH_TIER.get());
     }
 
     @Deprecated() // TODO: 1.20 Rename to default tier
