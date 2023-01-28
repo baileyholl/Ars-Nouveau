bf05758e4f577e84c778d7dbb4c3a3d6a2e99211
Alexthw46 <77152778+Alexthw46@users.noreply.github.com>
2023-01-28 15:34:14 +0100

    Remove redundant check

--- src/main/java/com/hollingsworth/arsnouveau/common/spell/effect/EffectSnare.java
+++ src/main/java/com/hollingsworth/arsnouveau/common/spell/effect/EffectSnare.java
@@ -35,7 +35,7 @@ public class EffectSnare extends AbstractEffect {
             living.hurtMarked = true;
         }
 
-        if (rayTraceResult.getEntity() instanceof EnchantedFallingBlock fallingBlock && !fallingBlock.isRemoved()) {
+        if (rayTraceResult.getEntity() instanceof EnchantedFallingBlock fallingBlock) {
             BlockPos resultPos = fallingBlock.groundBlock(true);
             if(resultPos != null) {
                 ShapersFocus.tryPropagateBlockSpell(new BlockHitResult(new Vec3(resultPos.getX(), resultPos.getY(), resultPos.getZ()), rayTraceResult.getEntity().getMotionDirection(), resultPos, false), world, shooter, spellContext, resolver);
