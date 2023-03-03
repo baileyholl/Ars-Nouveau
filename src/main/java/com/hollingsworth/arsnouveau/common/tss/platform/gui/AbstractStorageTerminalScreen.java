f6ad6a3a29a0679f0dbe7107acd235c18dfa364b
Bailey Hollingsworth <baileyholl@gmail.com>
2023-03-02 21:59:17 -0600

    Fix settings icons

--- src/main/java/com/hollingsworth/arsnouveau/common/tss/platform/gui/AbstractStorageTerminalScreen.java
+++ src/main/java/com/hollingsworth/arsnouveau/common/tss/platform/gui/AbstractStorageTerminalScreen.java
@@ -137,20 +137,20 @@ public abstract class AbstractStorageTerminalScreen<T extends StorageTerminalMen
 		searchLast = "";
 		addRenderableWidget(searchField);
 
-		buttonSortingType = addRenderableWidget(new StorageSettingsButton(leftPos - 17, topPos + 14, 21, 12, 256, 13, 0, new ResourceLocation(ArsNouveau.MODID, "textures/gui/sort_tabs.png"), b -> {
+		buttonSortingType = addRenderableWidget(new StorageSettingsButton(leftPos - 18, topPos + 14, 22, 12, 44, 13, 0, new ResourceLocation(ArsNouveau.MODID, "textures/gui/sort_type.png"), b -> {
 			comparator = StoredItemStack.SortingTypes.VALUES[(comparator.type() + 1) % StoredItemStack.SortingTypes.VALUES.length].create(comparator.isReversed());
 			buttonSortingType.state = comparator.type();
 			sendUpdate();
 			refreshItemList = true;
 		}));
 
-		buttonDirection = addRenderableWidget(new StorageSettingsButton(leftPos - 17, topPos + 29, 21, 12, 256, 13, 0, new ResourceLocation(ArsNouveau.MODID, "textures/gui/sort_tabs.png"), b -> {
+		buttonDirection = addRenderableWidget(new StorageSettingsButton(leftPos - 18, topPos + 29, 22, 12, 44, 13, 0, new ResourceLocation(ArsNouveau.MODID, "textures/gui/sort_order.png"), b -> {
 			comparator.setReversed(!comparator.isReversed());
 			buttonDirection.state = comparator.isReversed() ? 1 : 0;
 			sendUpdate();
 			refreshItemList = true;
 		}));
-		buttonSearchType = addRenderableWidget(new StorageSettingsButton(leftPos - 17, topPos + 44, 21, 12, 256, 13, 0, new ResourceLocation(ArsNouveau.MODID, "textures/gui/sort_tabs.png"), b -> {
+		buttonSearchType = addRenderableWidget(new StorageSettingsButton(leftPos - 18, topPos + 44, 22, 12, 44, 13, 0, new ResourceLocation(ArsNouveau.MODID, "textures/gui/search_sync.png"), b -> {
 			searchType = (searchType + 1) & ((IAutoFillTerminal.hasSync() || this instanceof CraftingTerminalScreen) ? 0b111 : 0b011);
 			buttonSearchType.state = searchType;
 			sendUpdate();
