d98e1c6ab11e3fc92a3d087f2706ce477e4f0909
Bailey Hollingsworth <baileyholl@gmail.com>
2023-03-23 19:29:33 -0500

    name effect on blocks, store name on lectern

--- src/main/java/com/hollingsworth/arsnouveau/common/block/tile/RepositoryTile.java
+++ src/main/java/com/hollingsworth/arsnouveau/common/block/tile/RepositoryTile.java
@@ -1,10 +1,13 @@
 package com.hollingsworth.arsnouveau.common.block.tile;
 
+import com.hollingsworth.arsnouveau.api.client.ITooltipProvider;
 import com.hollingsworth.arsnouveau.setup.BlockRegistry;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.NonNullList;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.Connection;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.protocol.game.ClientboundBlockEntityDataPacket;
 import net.minecraft.world.ContainerHelper;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.inventory.AbstractContainerMenu;
@@ -17,9 +20,12 @@ import software.bernie.geckolib3.core.manager.AnimationData;
 import software.bernie.geckolib3.core.manager.AnimationFactory;
 import software.bernie.geckolib3.util.GeckoLibUtil;
 
-public class RepositoryTile extends RandomizableContainerBlockEntity implements IAnimatable {
-    private NonNullList<ItemStack> items = NonNullList.withSize(54, ItemStack.EMPTY);
+import javax.annotation.Nullable;
+import java.util.List;
 
+public class RepositoryTile extends RandomizableContainerBlockEntity implements IAnimatable, ITooltipProvider {
+    private NonNullList<ItemStack> items = NonNullList.withSize(54, ItemStack.EMPTY);
+    public int fillLevel;
 
     public RepositoryTile(BlockPos pos, BlockState state) {
         super(BlockRegistry.REPOSITORY_TILE, pos, state);
@@ -48,12 +54,13 @@ public class RepositoryTile extends RandomizableContainerBlockEntity implements 
         return 54;
     }
 
+
     protected void saveAdditional(CompoundTag pTag) {
         super.saveAdditional(pTag);
         if (!this.trySaveLootTable(pTag)) {
             ContainerHelper.saveAllItems(pTag, this.items);
         }
-
+        pTag.putInt("fillLevel", fillLevel);
     }
 
     public void load(CompoundTag pTag) {
@@ -62,15 +69,52 @@ public class RepositoryTile extends RandomizableContainerBlockEntity implements 
         if (!this.tryLoadLootTable(pTag)) {
             ContainerHelper.loadAllItems(pTag, this.items);
         }
+        fillLevel = pTag.getInt("fillLevel");
     }
 
     @Override
-    public void registerControllers(AnimationData data) {
-
+    public void onDataPacket(Connection net, ClientboundBlockEntityDataPacket pkt) {
+        super.onDataPacket(net, pkt);
+        handleUpdateTag(pkt.getTag() == null ? new CompoundTag() : pkt.getTag());
     }
+
+    public boolean updateBlock() {
+        if(level != null) {
+            BlockState state = level.getBlockState(worldPosition);
+            level.sendBlockUpdated(worldPosition, state, state, 3);
+            setChanged();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public CompoundTag getUpdateTag() {
+        CompoundTag tag = new CompoundTag();
+        this.saveAdditional(tag);
+        return tag;
+    }
+
+    @Override
+    @Nullable
+    public ClientboundBlockEntityDataPacket getUpdatePacket() {
+        return ClientboundBlockEntityDataPacket.create(this);
+    }
+
+    @Override
+    public void registerControllers(AnimationData data) {}
+
     AnimationFactory factory = GeckoLibUtil.createFactory(this);
+
     @Override
     public AnimationFactory getFactory() {
         return factory;
+    }
+
+    @Override
+    public void getTooltip(List<Component> tooltip) {
+        if(hasCustomName()){
+            tooltip.add(getCustomName());
+        }
     }
 }
