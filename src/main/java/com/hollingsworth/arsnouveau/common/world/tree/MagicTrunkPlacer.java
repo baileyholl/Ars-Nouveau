af091ae17750d14c38d89955f1fb0e44e9f92567
Bailey Hollingsworth <baileyholl@gmail.com>
2023-03-29 17:03:39 -0500

    Fix import

--- src/main/java/com/hollingsworth/arsnouveau/common/world/tree/MagicTrunkPlacer.java
+++ src/main/java/com/hollingsworth/arsnouveau/common/world/tree/MagicTrunkPlacer.java
@@ -9,6 +9,7 @@ import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BlockTags;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.LevelSimulatedReader;
 import net.minecraft.world.level.block.Blocks;
