413b3407179ddf7d400ecd1b08bf8de4d650a563
Jarva <4622609+Jarva@users.noreply.github.com>
2023-04-08 21:25:37 +0100

    fix: make archfruit inherit from CocoaBlock

--- src/main/java/com/hollingsworth/arsnouveau/common/block/ArchfruitPod.java
+++ src/main/java/com/hollingsworth/arsnouveau/common/block/ArchfruitPod.java
@@ -32,9 +32,8 @@ import java.util.function.Supplier;
 
 import static com.hollingsworth.arsnouveau.common.block.SconceBlock.LIGHT_LEVEL;
 
-public class ArchfruitPod extends HorizontalDirectionalBlock implements BonemealableBlock, ILightable {
+public class ArchfruitPod extends CocoaBlock implements ILightable {
     public Supplier<Block> surviveBlock;
-    public static final IntegerProperty AGE = BlockStateProperties.AGE_2;
     protected static final VoxelShape[] EAST_AABB = new VoxelShape[]{Block.box(11.0D, 7.0D, 6.0D, 15.0D, 11.0D, 10.0D),
             Block.box(9.0D, 5.0D, 5.0D, 15.0D, 11.0D, 11.0D),
             Block.box(7.0D, 3.0D, 4.0D, 15.0D, 11.0D, 12.0D)};
@@ -55,29 +54,12 @@ public class ArchfruitPod extends HorizontalDirectionalBlock implements Bonemeal
        this.surviveBlock = surviveBlock;
     }
 
-    public ArchfruitPod(BlockBehaviour.Properties p_51743_) {
-        super(p_51743_);
+    public ArchfruitPod(BlockBehaviour.Properties properties) {
+        super(properties);
         this.registerDefaultState(this.stateDefinition.any()
                 .setValue(FACING, Direction.NORTH)
                 .setValue(AGE, 0)
                 .setValue(LIGHT_LEVEL, 0));
-    }
-
-    /**
-     * @return whether this block needs random ticking.
-     */
-    @Override
-    public boolean isRandomlyTicking(BlockState pState) {
-        return pState.getValue(AGE) < 2;
-    }
-
-    @Override
-    public void randomTick(BlockState p_221000_, ServerLevel p_221001_, BlockPos p_221002_, RandomSource p_221003_) {
-        int i = p_221000_.getValue(AGE);
-        if (i < 2 && net.minecraftforge.common.ForgeHooks.onCropsGrowPre(p_221001_, p_221002_, p_221000_, p_221001_.random.nextInt(5) == 0)) {
-            p_221001_.setBlock(p_221002_, p_221000_.setValue(AGE, i + 1), 2);
-            net.minecraftforge.common.ForgeHooks.onCropsGrowPost(p_221001_, p_221002_, p_221000_);
-        }
     }
 
     @Override
@@ -101,59 +83,9 @@ public class ArchfruitPod extends HorizontalDirectionalBlock implements Bonemeal
         }
     }
 
-    @Nullable
-    public BlockState getStateForPlacement(BlockPlaceContext pContext) {
-        BlockState blockstate = this.defaultBlockState();
-        LevelReader levelreader = pContext.getLevel();
-        BlockPos blockpos = pContext.getClickedPos();
-
-        for (Direction direction : pContext.getNearestLookingDirections()) {
-            if (direction.getAxis().isHorizontal()) {
-                blockstate = blockstate.setValue(FACING, direction);
-                if (blockstate.canSurvive(levelreader, blockpos)) {
-                    return blockstate;
-                }
-            }
-        }
-
-        return null;
-    }
-
-    @Override
-    public BlockState updateShape(BlockState pState, Direction pFacing, BlockState pFacingState, LevelAccessor pLevel, BlockPos pCurrentPos, BlockPos pFacingPos) {
-        return pFacing == pState.getValue(FACING) && !pState.canSurvive(pLevel, pCurrentPos) ?
-                Blocks.AIR.defaultBlockState() :
-                super.updateShape(pState, pFacing, pFacingState, pLevel, pCurrentPos, pFacingPos);
-    }
-
-    @Override
-    public boolean isValidBonemealTarget(BlockGetter pLevel, BlockPos pPos, BlockState pState, boolean pIsClient) {
-        return pState.getValue(AGE) < 2;
-    }
-
-    @Override
-    public boolean isBonemealSuccess(Level p_220995_, RandomSource p_220996_, BlockPos p_220997_, BlockState p_220998_) {
-        return true;
-    }
-
-    @Override
-    public void performBonemeal(ServerLevel p_220990_, RandomSource p_220991_, BlockPos p_220992_, BlockState p_220993_) {
-        p_220990_.setBlock(p_220992_, p_220993_.setValue(AGE, p_220993_.getValue(AGE) + 1), 2);
-    }
-
     @Override
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> pBuilder) {
         pBuilder.add(FACING, AGE, LIGHT_LEVEL);
-    }
-
-    @Override
-    public boolean isPathfindable(BlockState pState, BlockGetter pLevel, BlockPos pPos, PathComputationType pType) {
-        return false;
-    }
-
-    @Override
-    public RenderShape getRenderShape(BlockState pState) {
-        return RenderShape.MODEL;
     }
 
     @Override
