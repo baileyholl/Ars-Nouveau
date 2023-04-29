5621aaf4093f5f05dc445a0a3572369afcaa7b3e
Jarva <4622609+Jarva@users.noreply.github.com>
2023-04-29 21:26:10 +0100

    feat: extract method to PotionEffectTags#getEffects

--- src/main/java/com/hollingsworth/arsnouveau/common/items/curios/BeltOfUnstableGifts.java
+++ src/main/java/com/hollingsworth/arsnouveau/common/items/curios/BeltOfUnstableGifts.java
@@ -2,9 +2,6 @@ package com.hollingsworth.arsnouveau.common.items.curios;
 
 import com.hollingsworth.arsnouveau.api.item.ArsNouveauCurio;
 import com.hollingsworth.arsnouveau.common.lib.PotionEffectTags;
-import net.minecraft.core.Holder;
-import net.minecraft.core.HolderSet;
-import net.minecraft.core.Registry;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectInstance;
@@ -13,36 +10,18 @@ import net.minecraft.world.item.ItemStack;
 import top.theillusivec4.curios.api.SlotContext;
 
 import java.util.ArrayList;
-import java.util.Optional;
 import java.util.Random;
 
 public class BeltOfUnstableGifts extends ArsNouveauCurio {
-
-    public static Optional<HolderSet.Named<MobEffect>> effects = Registry.MOB_EFFECT.getTag(PotionEffectTags.UNSTABLE_GIFTS);
-    public static ArrayList<MobEffect> effectTable;
 
     public BeltOfUnstableGifts() {
         super();
     }
 
-    public static void fetchEffects() {
-        effects.ifPresentOrElse(mobEffects -> {
-            effectTable = new ArrayList<>();
-            for (Holder<MobEffect> mobEffect : mobEffects) {
-                effectTable.add(mobEffect.get());
-            }
-        }, () -> {
-            effects = Registry.MOB_EFFECT.getTag(PotionEffectTags.UNSTABLE_GIFTS);
-        });
-    }
-
     @Override
     public void curioTick(SlotContext slotContext, ItemStack stack) {
-        if (effects.isEmpty() || effectTable == null) {
-            fetchEffects();
-            return;
-        }
-        if (effectTable.size() == 0) return;
+        ArrayList<MobEffect> effectTable = PotionEffectTags.getEffects(PotionEffectTags.UNSTABLE_GIFTS);
+        if (effectTable == null || effectTable.size() == 0) return;
         LivingEntity wearer = slotContext.entity();
         if (wearer == null) return;
         if (slotContext.entity().getLevel() instanceof ServerLevel world) {
--- src/main/java/com/hollingsworth/arsnouveau/common/lib/PotionEffectTags.java
+++ src/main/java/com/hollingsworth/arsnouveau/common/lib/PotionEffectTags.java
@@ -1,11 +1,31 @@
 package com.hollingsworth.arsnouveau.common.lib;
 
 import com.hollingsworth.arsnouveau.ArsNouveau;
+import net.minecraft.core.Holder;
+import net.minecraft.core.HolderSet;
 import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.effect.MobEffect;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Optional;
+
 public class PotionEffectTags {
+    private static final HashMap<TagKey<MobEffect>, ArrayList<MobEffect>> potionEffects = new HashMap<>();
     public static final TagKey<MobEffect> UNSTABLE_GIFTS = TagKey.create(Registry.MOB_EFFECT_REGISTRY, new ResourceLocation(ArsNouveau.MODID, "unstable_gifts"));
+
+    public static ArrayList<MobEffect> getEffects(TagKey<MobEffect> tag) {
+        return potionEffects.computeIfAbsent(tag, (_key) -> {
+            Optional<HolderSet.Named<MobEffect>> effects = Registry.MOB_EFFECT.getTag(tag);
+            if (effects.isEmpty()) return null;
+
+            ArrayList<MobEffect> effectList = new ArrayList<>();
+            for (Holder<MobEffect> mobEffectHolder : effects.get()) {
+                effectList.add(mobEffectHolder.get());
+            }
+            return effectList;
+        });
+    }
 }
