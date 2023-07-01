a3ca9e052d7d9ef10f5bae3811ad3790ecc82245
Alexthw <77152778+Alexthw46@users.noreply.github.com>
2023-07-01 16:08:43 +0200

    catch all glyphs and order them in tab (#1019)

--- src/main/java/com/hollingsworth/arsnouveau/setup/CreativeTabRegistry.java
+++ src/main/java/com/hollingsworth/arsnouveau/setup/CreativeTabRegistry.java
@@ -2,6 +2,7 @@ package com.hollingsworth.arsnouveau.setup;
 
 import com.hollingsworth.arsnouveau.ArsNouveau;
 import com.hollingsworth.arsnouveau.api.ArsNouveauAPI;
+import com.hollingsworth.arsnouveau.client.gui.book.BaseBook;
 import com.hollingsworth.arsnouveau.common.items.Glyph;
 import com.hollingsworth.arsnouveau.common.spell.method.MethodProjectile;
 import net.minecraft.core.registries.Registries;
@@ -21,10 +22,10 @@ public class CreativeTabRegistry {
     public static final RegistryObject<CreativeModeTab> BLOCKS = TABS.register("general", () -> CreativeModeTab.builder()
             .title(Component.translatable("itemGroup.ars_nouveau"))
             .icon(() -> ItemsRegistry.CREATIVE_SPELLBOOK.get().getDefaultInstance())
-            .displayItems((params, output) ->{
-                for(Map.Entry<ResourceKey<Item>, Item> entry : ForgeRegistries.ITEMS.getEntries()){
-                    if(entry.getKey().location().getNamespace().equals(ArsNouveau.MODID)
-                     && !(entry.getValue() instanceof Glyph)){
+            .displayItems((params, output) -> {
+                for (Map.Entry<ResourceKey<Item>, Item> entry : ForgeRegistries.ITEMS.getEntries()) {
+                    if (entry.getKey().location().getNamespace().equals(ArsNouveau.MODID)
+                        && !(entry.getValue() instanceof Glyph)) {
                         output.accept(entry.getValue().getDefaultInstance());
                     }
                 }
@@ -34,12 +35,13 @@ public class CreativeTabRegistry {
     public static final RegistryObject<CreativeModeTab> GLYPHS = TABS.register("glyphs", () -> CreativeModeTab.builder()
             .title(Component.translatable("itemGroup.ars_glyphs"))
             .icon(() -> ArsNouveauAPI.getInstance().getGlyphItem(MethodProjectile.INSTANCE).getDefaultInstance())
-            .displayItems((params, output) ->{
-                for(Map.Entry<ResourceKey<Item>, Item> entry : ForgeRegistries.ITEMS.getEntries()){
-                    if(entry.getKey().location().getNamespace().equals(ArsNouveau.MODID) && entry.getValue() instanceof Glyph){
-                        output.accept(entry.getValue().getDefaultInstance());
-                    }
+            .displayItems((params, output) -> {
+
+                for (var glyph : ArsNouveauAPI.getInstance().getSpellpartMap().values().stream()
+                        .sorted(BaseBook.COMPARE_TYPE_THEN_NAME).toList()) {
+                    output.accept(ArsNouveauAPI.getInstance().getGlyphItem(glyph).getDefaultInstance());
                 }
+
             }).withTabsBefore(BLOCKS.getKey())
             .build());
 }
