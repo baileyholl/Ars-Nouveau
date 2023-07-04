5f8a456690ec460e09e0a6129c089d86a2a5182e
Bailey Hollingsworth <baileyholl@gmail.com>
2023-07-04 08:27:22 -0500

    add validation on supporters file for dev

--- src/main/java/com/hollingsworth/arsnouveau/setup/reward/Rewards.java
+++ src/main/java/com/hollingsworth/arsnouveau/setup/reward/Rewards.java
@@ -4,12 +4,15 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import net.minecraft.world.item.DyeColor;
+import net.minecraftforge.fml.loading.FMLEnvironment;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
@@ -35,6 +38,9 @@ public class Rewards {
             }
         } catch (IOException var2) {
             var2.printStackTrace();
+            if(!FMLEnvironment.production){
+                throw new RuntimeException("Failed to load supporters.json");
+            }
         }
     }
 
@@ -72,6 +78,25 @@ public class Rewards {
             this.adopter = adopter;
             this.color = color;
             this.bio = bio;
+
+            if(!FMLEnvironment.production){
+                if(name == null){
+                    throw new RuntimeException("Name is null");
+                }
+                if(adopter == null){
+                    throw new RuntimeException("Adopter is null");
+                }
+                if(color == null){
+                    throw new RuntimeException("Color is null");
+                }
+                if(bio == null){
+                    throw new RuntimeException("Bio is null");
+                }
+                boolean foundColor = Arrays.stream(DyeColor.values()).anyMatch(dye -> dye.getName().equals(color));
+                if(!foundColor){
+                    throw new RuntimeException("Color is not a valid dye color");
+                }
+            }
         }
     }
 }
