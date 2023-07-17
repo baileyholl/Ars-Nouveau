d440dcd0bd05c08c8be98f578595af7f30d20a8f
Bailey Hollingsworth <baileyholl@gmail.com>
2023-07-16 19:34:39 -0500

    Fix beds not having bed properties

--- src/main/java/com/hollingsworth/arsnouveau/common/block/MageBlock.java
+++ src/main/java/com/hollingsworth/arsnouveau/common/block/MageBlock.java
@@ -2,8 +2,6 @@ package com.hollingsworth.arsnouveau.common.block;
 
 import com.hollingsworth.arsnouveau.common.block.tile.MageBlockTile;
 import net.minecraft.core.BlockPos;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Explosion;
 import net.minecraft.world.level.Level;
@@ -13,7 +11,6 @@ import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
-import net.minecraft.world.phys.Vec3;
 import org.jetbrains.annotations.Nullable;
 
 public class MageBlock extends TickableModBlock {
@@ -43,33 +40,6 @@ public class MageBlock extends TickableModBlock {
     @Override
     public <T extends BlockEntity> BlockEntityTicker<T> getTicker(Level level, BlockState state, BlockEntityType<T> type) {
         return (state.hasProperty(TEMPORARY) && state.getValue(TEMPORARY)) ? super.getTicker(level, state, type) : null;
-    }
-
-    public void fallOn(Level pLevel, BlockState pState, BlockPos pPos, Entity pEntity, float pFallDistance) {
-        super.fallOn(pLevel, pState, pPos, pEntity, pFallDistance * 0.5F);
-    }
-
-    /**
-     * Called when an Entity lands on this Block.
-     * This method is responsible for doing any modification on the motion of the entity that should result from the
-     * landing.
-     */
-    public void updateEntityAfterFallOn(BlockGetter pLevel, Entity pEntity) {
-        if (pEntity.isSuppressingBounce()) {
-            super.updateEntityAfterFallOn(pLevel, pEntity);
-        } else {
-            this.bounceUp(pEntity);
-        }
-
-    }
-
-    private void bounceUp(Entity pEntity) {
-        Vec3 vec3 = pEntity.getDeltaMovement();
-        if (vec3.y < 0.0D) {
-            double d0 = pEntity instanceof LivingEntity ? 1.0D : 0.8D;
-            pEntity.setDeltaMovement(vec3.x, -vec3.y * (double)0.66F * d0, vec3.z);
-        }
-
     }
 }
 
