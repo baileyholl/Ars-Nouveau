46e188cc12e1aa9a8337a1bed96d30c2b619c45a
Bailey Hollingsworth <baileyholl@gmail.com>
2023-08-14 18:47:25 -0500

    Change elytra mixins to mixin extras

--- src/main/java/com/hollingsworth/arsnouveau/common/mixin/elytra/ElytraPlayerMixin.java
+++ src/main/java/com/hollingsworth/arsnouveau/common/mixin/elytra/ElytraPlayerMixin.java
@@ -1,16 +1,15 @@
 package com.hollingsworth.arsnouveau.common.mixin.elytra;
 
 import com.hollingsworth.arsnouveau.common.spell.effect.EffectGlide;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(Player.class)
 public class ElytraPlayerMixin {
-    @Redirect(
+    @ModifyExpressionValue(
             method = "tryToStartFallFlying",
             at = @At(
                     value = "INVOKE",
@@ -18,7 +17,7 @@ public class ElytraPlayerMixin {
                     remap = false
             )
     )
-    public boolean elytraOverride(ItemStack stack, LivingEntity entity) {
-        return EffectGlide.canGlide(entity) || stack.canElytraFly(entity);
+    public boolean elytraOverride(boolean original) {
+        return original || EffectGlide.canGlide(((LivingEntity) ((Object)this)));
     }
 }
