295b0e5f27324ff3e81915d598a2c5e35f755b2c
Alexthw46 <77152778+Alexthw46@users.noreply.github.com>
2023-08-14 23:53:14 +0200

    Fix fake texture errors

--- src/main/java/com/hollingsworth/arsnouveau/common/armor/AnimatedMagicArmor.java
+++ src/main/java/com/hollingsworth/arsnouveau/common/armor/AnimatedMagicArmor.java
@@ -2,6 +2,7 @@ package com.hollingsworth.arsnouveau.common.armor;
 
 import com.google.common.collect.ImmutableMultimap;
 import com.google.common.collect.Multimap;
+import com.hollingsworth.arsnouveau.ArsNouveau;
 import com.hollingsworth.arsnouveau.api.client.IVariantColorProvider;
 import com.hollingsworth.arsnouveau.api.mana.IManaEquipment;
 import com.hollingsworth.arsnouveau.api.perk.*;
@@ -15,6 +16,8 @@ import com.hollingsworth.arsnouveau.setup.registry.ItemsRegistry;
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.model.HumanoidModel;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.attributes.Attribute;
@@ -26,6 +29,7 @@ import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.extensions.common.IClientItemExtensions;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import software.bernie.geckolib.animatable.GeoItem;
 import software.bernie.geckolib.core.animatable.instance.AnimatableInstanceCache;
 import software.bernie.geckolib.core.animation.AnimatableManager;
@@ -153,6 +157,15 @@ public class AnimatedMagicArmor extends ArmorItem implements IManaEquipment, IDy
 
     public GeoModel<AnimatedMagicArmor> getArmorModel() {
         return model;
+    }
+
+    /*
+     * Needed to avoid file not found errors since Geckolib doesn't redirect to the correct texture
+     */
+    @Override
+    public @Nullable String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlot slot, String type) {
+        GenericModel<AnimatedMagicArmor> genericModel = (GenericModel<AnimatedMagicArmor>) model;
+        return new ResourceLocation(ArsNouveau.MODID, "textures/" + genericModel.textPathRoot + "/" + genericModel.name + "_" + this.getColor(stack) + ".png").toString();
     }
 
     @Override
