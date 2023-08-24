2c713ddfe0e4ea8236d49bd3d7fa5dd8312e7492
dkmk100 <33072952+dkmk100@users.noreply.github.com>
2023-08-24 11:42:56 -0700

    add world to IFilter
    so that block filters can actually check the block they're supposed to filter

--- src/main/java/com/hollingsworth/arsnouveau/api/spell/IFilter.java
+++ src/main/java/com/hollingsworth/arsnouveau/api/spell/IFilter.java
@@ -3,6 +3,7 @@ package com.hollingsworth.arsnouveau.api.spell;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.EntityHitResult;
 import net.minecraft.world.phys.HitResult;
@@ -16,36 +17,36 @@ public interface IFilter{
     /**
      * Whether the filter should allow the block hit
      */
-    boolean shouldResolveOnBlock(BlockHitResult target);
+    boolean shouldResolveOnBlock(BlockHitResult target, Level level);
 
     /**
      * Default method that wraps the position/direction to call the actual method
      */
-    default boolean shouldResolveOnBlock(BlockPos pos, Direction direction){
+    default boolean shouldResolveOnBlock(BlockPos pos, Direction direction, Level level){
         return shouldResolveOnBlock(
                 new BlockHitResult(new Vec3(pos.getX(), pos.getY(), pos.getZ()), direction, pos, false)
-        );
+        ,level);
     }
 
     /**
      * Whether the filter should allow the entity hit
      */
-    boolean shouldResolveOnEntity(EntityHitResult target);
+    boolean shouldResolveOnEntity(EntityHitResult target, Level level);
 
     /**
      * Default method that wraps an entity to call the actual method
      */
-    default boolean shouldResolveOnEntity(Entity entity){
-        return shouldResolveOnEntity(new EntityHitResult(entity));
+    default boolean shouldResolveOnEntity(Entity entity, Level level){
+        return shouldResolveOnEntity(new EntityHitResult(entity),level);
     }
 
     /**
      * @param rayTraceResult block or entity about to be affected by the spell
      * @return true if the glyph filter pass, false if the filter blocks
      */
-    default boolean shouldAffect(HitResult rayTraceResult){
-        if (rayTraceResult instanceof BlockHitResult block) return shouldResolveOnBlock(block);
-        else if (rayTraceResult instanceof EntityHitResult entity) return shouldResolveOnEntity(entity);
+    default boolean shouldAffect(HitResult rayTraceResult, Level level){
+        if (rayTraceResult instanceof BlockHitResult block) return shouldResolveOnBlock(block,level);
+        else if (rayTraceResult instanceof EntityHitResult entity) return shouldResolveOnEntity(entity,level);
         else return false;
     }
 
