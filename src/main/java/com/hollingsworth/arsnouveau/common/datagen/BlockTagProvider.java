70435bf0101dd9a070f302c21fd8c6955f41d58d
Bailey Hollingsworth <baileyholl@gmail.com>
2023-11-24 13:37:06 -0600

    Add cactus, sugarcane, chorus #1121

--- src/main/java/com/hollingsworth/arsnouveau/common/datagen/BlockTagProvider.java
+++ src/main/java/com/hollingsworth/arsnouveau/common/datagen/BlockTagProvider.java
@@ -166,7 +166,11 @@ public class BlockTagProvider extends BlockTagsProvider {
                 Blocks.BROWN_MUSHROOM_BLOCK,
                 Blocks.RED_MUSHROOM_BLOCK,
                 Blocks.SHROOMLIGHT,
-                Blocks.WARPED_WART_BLOCK, Blocks.NETHER_WART_BLOCK);
+                Blocks.WARPED_WART_BLOCK, Blocks.NETHER_WART_BLOCK,
+                Blocks.CACTUS,
+                Blocks.SUGAR_CANE,
+                Blocks.CHORUS_FLOWER,
+                Blocks.CHORUS_PLANT);
         this.tag(WHIRLISPRIG_KINDA_LIKES);
 
         this.tag(MAGIC_SAPLINGS).add(
