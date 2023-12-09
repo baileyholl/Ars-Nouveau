290d8b84fb7b630bed7cf1075f33164a7f165f03
Bailey Hollingsworth <baileyholl@gmail.com>
2023-12-09 14:56:51 -0600

    Revert "temp disable validator"
    This reverts commit f5d11e3d18c84f45fcc9ae635dd43511985ebe60.

--- src/main/java/com/hollingsworth/arsnouveau/common/spell/validation/ContextSpellValidator.java
+++ src/main/java/com/hollingsworth/arsnouveau/common/spell/validation/ContextSpellValidator.java
@@ -1,11 +1,14 @@
 package com.hollingsworth.arsnouveau.common.spell.validation;
 
-import com.hollingsworth.arsnouveau.api.spell.AbstractEffect;
-import com.hollingsworth.arsnouveau.api.spell.AbstractSpellPart;
-import com.hollingsworth.arsnouveau.api.spell.SpellValidationError;
+import com.hollingsworth.arsnouveau.api.ArsNouveauAPI;
+import com.hollingsworth.arsnouveau.api.registry.GlyphRegistry;
+import com.hollingsworth.arsnouveau.api.spell.*;
+import net.minecraft.resources.ResourceLocation;
+import org.apache.logging.log4j.core.LoggerContext;
 
 import java.util.List;
 import java.util.Stack;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class ContextSpellValidator extends ScanningSpellValidator<Stack<AbstractEffect>> {
     @Override
@@ -15,49 +18,49 @@ public class ContextSpellValidator extends ScanningSpellValidator<Stack<Abstract
 
     @Override
     protected void digestSpellPart(Stack<AbstractEffect> context, int position, AbstractSpellPart spellPart, List<SpellValidationError> validationErrors) {
-//        if(spellPart instanceof AbstractEffect effect) {
-//
-//            if(effect instanceof IContextManipulator manip){
-//                if(context.isEmpty()){
-//                    validationErrors.add(new EmptyManipulationSpellValidationError(position, effect));
-//                }
-//                else {
-//                    if (manip.CanEscape(context.peek())) {
-//                        context.pop();
-//                    } else{
-//                        validationErrors.add(new InvalidManipulationSpellValidationError(position, effect, context.peek()));
-//                    }
-//                }
-//            }
-//
-//            for(ResourceLocation invalidPart : spellPart.invalidNestings.parseComboLimits()){
-//                AbstractSpellPart offendingPart = GlyphRegistry.getSpellPart(invalidPart);
-//                if(offendingPart == null)
-//                    continue;
-//                if(context.contains(offendingPart)){
-//                    LoggerContext.getContext().getLogger(ContextSpellValidator.class).info("context has invalid part");
-//                    validationErrors.add(new InvalidNestingValidationError(position, spellPart, offendingPart));
-//                }else if(offendingPart.invalidNestings.contains(spellPart.getRegistryName()) && offendingPart != spellPart){
-//                    LoggerContext.getContext().getLogger(ContextSpellValidator.class).info("offending part has spell part");
-//                    validationErrors.add(new InvalidNestingValidationError(position, offendingPart, spellPart));
-//                }
-//            }
-//
-//            for(AbstractEffect other : context){
-//                AbstractSpellPart part = GlyphRegistry.getSpellPart(other.getRegistryName());
-//                if(part == null)
-//                    return;
-//                if(part.invalidNestings.contains(spellPart.getRegistryName())){
-//                    LoggerContext.getContext().getLogger(ContextSpellValidator.class).info("context has part that has us");
-//                    validationErrors.add(new InvalidNestingValidationError(position, part, spellPart));
-//                }
-//            }
-//
-//            //push after validating so a glyph can be an invalid nesting with itself
-//            if (ArsNouveauAPI.IsContextCreator(effect)){
-//                context.push(effect);//push context onto the stack
-//            }
-//        }
+        if(spellPart instanceof AbstractEffect effect) {
+
+            if(effect instanceof IContextManipulator manip){
+                if(context.isEmpty()){
+                    validationErrors.add(new EmptyManipulationSpellValidationError(position, effect));
+                }
+                else {
+                    if (manip.CanEscape(context.peek())) {
+                        context.pop();
+                    } else{
+                        validationErrors.add(new InvalidManipulationSpellValidationError(position, effect, context.peek()));
+                    }
+                }
+            }
+
+            for(ResourceLocation invalidPart : spellPart.invalidNestings.parseComboLimits()){
+                AbstractSpellPart offendingPart = GlyphRegistry.getSpellPart(invalidPart);
+                if(offendingPart == null)
+                    continue;
+                if(context.contains(offendingPart)){
+                    LoggerContext.getContext().getLogger(ContextSpellValidator.class).info("context has invalid part");
+                    validationErrors.add(new InvalidNestingValidationError(position, spellPart, offendingPart));
+                }else if(offendingPart.invalidNestings.contains(spellPart.getRegistryName()) && offendingPart != spellPart){
+                    LoggerContext.getContext().getLogger(ContextSpellValidator.class).info("offending part has spell part");
+                    validationErrors.add(new InvalidNestingValidationError(position, offendingPart, spellPart));
+                }
+            }
+
+            for(AbstractEffect other : context){
+                AbstractSpellPart part = GlyphRegistry.getSpellPart(other.getRegistryName());
+                if(part == null)
+                    return;
+                if(part.invalidNestings.contains(spellPart.getRegistryName())){
+                    LoggerContext.getContext().getLogger(ContextSpellValidator.class).info("context has part that has us");
+                    validationErrors.add(new InvalidNestingValidationError(position, part, spellPart));
+                }
+            }
+
+            //push after validating so a glyph can be an invalid nesting with itself
+            if (ArsNouveauAPI.IsContextCreator(effect)){
+                context.push(effect);//push context onto the stack
+            }
+        }
     }
 
     @Override
