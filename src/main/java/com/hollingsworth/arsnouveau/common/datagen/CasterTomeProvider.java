3003ebf2c4b759e97600b62c8735d5490533d3e6
Bailey Hollingsworth <baileyholl@gmail.com>
2024-03-09 13:05:22 -0600

    update supporters

--- src/main/java/com/hollingsworth/arsnouveau/common/datagen/CasterTomeProvider.java
+++ src/main/java/com/hollingsworth/arsnouveau/common/datagen/CasterTomeProvider.java
@@ -228,6 +228,23 @@ public class CasterTomeProvider extends SimpleDataProvider {
                         .add(AugmentAmplify.INSTANCE)
                         .withSound(new ConfiguredSpellSound(SoundRegistry.TEMPESTRY_SPELL_SOUND, 1.69f, 1.9f)), "Encases your enemies or friends in a tomb of ice. Guaranteed to leave them shocked and confused.",
                 new ParticleColor(25, 255, 255)));
+        
+        tomes.add(buildTome("spinoftw", "You were hurt, but you're fine", new Spell(MethodSelf.INSTANCE)
+                .add(EffectConjureWater.INSTANCE)
+                .add(EffectHeal.INSTANCE)
+                .add(AugmentAmplify.INSTANCE, 2)
+                .add(EffectPhantomBlock.INSTANCE)
+                .add(AugmentAOE.INSTANCE, 3), " A utility spell to aid you in your adventures", new ParticleColor(225, 90 ,1)));
+
+        tomes.add(buildTome("lyrellion", "Lyrellion’s Wall of Force", new Spell(MethodProjectile.INSTANCE)
+                .add(AugmentAccelerate.INSTANCE)
+                .add(EffectWall.INSTANCE)
+                .add(AugmentSensitive.INSTANCE)
+                .add(EffectPhantomBlock.INSTANCE)
+                .add(AugmentPierce.INSTANCE)
+                .add(AugmentAmplify.INSTANCE), "You. Shall. Not. Pass."));
+
+        Path output = this.generator.getOutputFolder();
 
         for (CasterTomeData g : tomes) {
             Path path = getRecipePath(output, g.getId().getPath());
