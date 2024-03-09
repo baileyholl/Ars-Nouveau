f8ca3613bf981fb7805a66d304806d91b40ac2a5
Alexthw <77152778+Alexthw46@users.noreply.github.com>
2024-03-09 21:15:34 +0100

    Potion effect sync for custom particle effects (#1231)
    * Potion effect sync for custom particle effects
    
    * PotionSync on expire

--- src/main/java/com/hollingsworth/arsnouveau/common/lib/PotionEffectTags.java
+++ src/main/java/com/hollingsworth/arsnouveau/common/lib/PotionEffectTags.java
@@ -3,7 +3,6 @@ package com.hollingsworth.arsnouveau.common.lib;
 import com.hollingsworth.arsnouveau.ArsNouveau;
 import net.minecraft.core.Holder;
 import net.minecraft.core.HolderSet;
-import net.minecraft.core.Registry;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagKey;
@@ -20,7 +19,7 @@ public class PotionEffectTags {
     public static final TagKey<MobEffect> UNSTABLE_GIFTS = TagKey.create(Registries.MOB_EFFECT, new ResourceLocation(ArsNouveau.MODID, "unstable_gifts"));
     public static final TagKey<MobEffect> DISPEL_DENY = TagKey.create(Registries.MOB_EFFECT, new ResourceLocation(ArsNouveau.MODID, "deny_dispel"));
     public static final TagKey<MobEffect> DISPEL_ALLOW = TagKey.create(Registries.MOB_EFFECT, new ResourceLocation(ArsNouveau.MODID, "allow_dispel"));
-
+    public static final TagKey<MobEffect> TO_SYNC = TagKey.create(Registries.MOB_EFFECT, new ResourceLocation(ArsNouveau.MODID, "to_sync"));
     public static ArrayList<MobEffect> getEffects(Level level, TagKey<MobEffect> tag) {
         return potionEffects.computeIfAbsent(tag, (_key) -> {
             Optional<HolderSet.Named<MobEffect>> effects = level.registryAccess().registryOrThrow(Registries.MOB_EFFECT).getTag(tag);
