429ef04fc1239f22371a1dd88f2facf6add1080b
Bailey Hollingsworth <baileyholl@gmail.com>
2024-03-09 13:05:22 -0600

    update supporters

--- src/main/java/com/hollingsworth/arsnouveau/common/datagen/CasterTomeProvider.java
+++ src/main/java/com/hollingsworth/arsnouveau/common/datagen/CasterTomeProvider.java
@@ -232,6 +232,15 @@ public class CasterTomeProvider implements DataProvider {
                 .add(AugmentAmplify.INSTANCE, 2)
                 .add(EffectPhantomBlock.INSTANCE)
                 .add(AugmentAOE.INSTANCE, 3), " A utility spell to aid you in your adventures", new ParticleColor(225, 90 ,1)));
+
+        tomes.add(buildTome("lyrellion", "Lyrellion’s Wall of Force", new Spell(MethodProjectile.INSTANCE)
+                .add(AugmentAccelerate.INSTANCE)
+                .add(EffectWall.INSTANCE)
+                .add(AugmentSensitive.INSTANCE)
+                .add(EffectPhantomBlock.INSTANCE)
+                .add(AugmentPierce.INSTANCE)
+                .add(AugmentAmplify.INSTANCE), "You. Shall. Not. Pass."));
+
         Path output = this.generator.getOutputFolder();
         for (CasterTomeData g : tomes) {
             Path path = getRecipePath(output, g.getId().getPath());
