111b6986c2d6213c00e76fd0032867c6b8e28117
Alexthw <77152778+Alexthw46@users.noreply.github.com>
2024-04-10 00:21:00 +0200

    Wixie crafting callbacks on start and end (#1258)
    * Wixie crafting callbacks on start and end
    
    * remove null level checks
    
    ---------
    
    Co-authored-by: Bailey Hollingsworth <baileyholl@gmail.com>

--- src/main/java/com/hollingsworth/arsnouveau/api/recipe/CraftingManager.java
+++ src/main/java/com/hollingsworth/arsnouveau/api/recipe/CraftingManager.java
@@ -1,12 +1,9 @@
 package com.hollingsworth.arsnouveau.api.recipe;
 
 import com.hollingsworth.arsnouveau.api.util.NBTUtil;
-import com.hollingsworth.arsnouveau.common.block.WixieCauldron;
 import com.hollingsworth.arsnouveau.common.block.tile.WixieCauldronTile;
 import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
-import net.minecraft.sounds.SoundEvents;
-import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
@@ -85,8 +82,7 @@ public class CraftingManager {
             }
         }
         tile.hasSource = false;
-        level.setBlockAndUpdate(worldPosition, level.getBlockState(worldPosition).setValue(WixieCauldron.FILLED, false));
-        level.playSound(null, worldPosition, SoundEvents.ILLUSIONER_CAST_SPELL, SoundSource.BLOCKS, 0.15f, 0.6f);
+        tile.onCraftingComplete();
         this.craftCompleted = true;
     }
 
