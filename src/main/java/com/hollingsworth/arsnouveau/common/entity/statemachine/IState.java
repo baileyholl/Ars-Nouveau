badad8da2500505b287c6e6426e793b16f3b885c
Bailey Hollingsworth <baileyholl@gmail.com>
2024-05-25 14:38:49 -0500

    Starby rewrite and stacking support  (#1270)
    * state machinify starbies
    
    * more goals
    
    * berry harvesting
    
    * add backoffs
    
    * stacked starby support
    
    * remove debug logs
    
    * delegate wand and tooltip to base starby
    
    * docs
    
    * support picking up multiple items at once

---
+++ src/main/java/com/hollingsworth/arsnouveau/common/entity/statemachine/IState.java
@@ -0,0 +1,26 @@
+package com.hollingsworth.arsnouveau.common.entity.statemachine;
+
+import javax.annotation.Nullable;
+
+public interface IState<T extends IState<?>> {
+
+    /**
+     * When the state is first entered.
+     */
+    void onStart();
+
+    /**
+     * When the state is exited.
+     */
+    void onEnd();
+
+    /**
+     * Returns a new state if the state is finished, otherwise returns null.
+     */
+    @Nullable T tick();
+
+    /**
+     * Returns a new state if applicable, otherwise returns null.
+     */
+    @Nullable T onEvent(IStateEvent event);
+}
