2ab26278c03c08b95054115ccc2ef85d8807ca9a
Bailey Hollingsworth <baileyholl@gmail.com>
2024-07-14 18:51:01 -0500

    migrate book upgrades to datagen

--- src/main/java/com/hollingsworth/arsnouveau/common/datagen/OneOffRecipesProvider.java
+++ src/main/java/com/hollingsworth/arsnouveau/common/datagen/OneOffRecipesProvider.java
@@ -1,14 +1,21 @@
 package com.hollingsworth.arsnouveau.common.datagen;
 
 
+import com.hollingsworth.arsnouveau.common.crafting.recipes.BookUpgradeRecipe;
 import com.hollingsworth.arsnouveau.common.crafting.recipes.PotionFlaskRecipe;
 import com.hollingsworth.arsnouveau.setup.registry.ItemsRegistry;
 import com.mojang.serialization.JsonOps;
+import net.minecraft.core.NonNullList;
 import net.minecraft.data.CachedOutput;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.crafting.CraftingBookCategory;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.neoforged.neoforge.common.Tags;
 
 import java.nio.file.Path;
+import java.util.List;
 
 public class OneOffRecipesProvider extends SimpleDataProvider{
 
@@ -28,6 +35,35 @@ public class OneOffRecipesProvider extends SimpleDataProvider{
             var flaskRecipe = new PotionFlaskRecipe("", flasks[i].getDefaultInstance(), flasks[i].getDefaultInstance());
             saveStable(pOutput, PotionFlaskRecipe.CODEC.encodeStart(JsonOps.INSTANCE, flaskRecipe).getOrThrow(), getRecipePath(output, "fill_potion_flask_" + i));
         }
+
+        BookUpgradeRecipe apprentice = new BookUpgradeRecipe("", CraftingBookCategory.MISC, ItemsRegistry.APPRENTICE_SPELLBOOK.get().getDefaultInstance(),
+                NonNullList.copyOf(List.of(
+                        Ingredient.of(ItemsRegistry.NOVICE_SPELLBOOK.get().getDefaultInstance()),
+                        Ingredient.of(Tags.Items.OBSIDIANS),
+                        Ingredient.of(Tags.Items.GEMS_DIAMOND),
+                        Ingredient.of(Tags.Items.GEMS_DIAMOND),
+                        Ingredient.of(Tags.Items.GEMS_DIAMOND),
+                        Ingredient.of(Items.QUARTZ_BLOCK),
+                        Ingredient.of(Items.QUARTZ_BLOCK),
+                        Ingredient.of(Tags.Items.RODS_BLAZE),
+                        Ingredient.of(Tags.Items.RODS_BLAZE)
+                        )));
+
+        BookUpgradeRecipe archmage = new BookUpgradeRecipe("", CraftingBookCategory.MISC, ItemsRegistry.ARCHMAGE_SPELLBOOK.get().getDefaultInstance(),
+                NonNullList.copyOf(List.of(
+                        Ingredient.of(ItemsRegistry.APPRENTICE_SPELLBOOK.get().getDefaultInstance()),
+                        Ingredient.of(Tags.Items.ENDER_PEARLS),
+                        Ingredient.of(Tags.Items.ENDER_PEARLS),
+                        Ingredient.of(Tags.Items.ENDER_PEARLS),
+                        Ingredient.of(Tags.Items.GEMS_EMERALD),
+                        Ingredient.of(Tags.Items.GEMS_EMERALD),
+                        Ingredient.of(Items.TOTEM_OF_UNDYING),
+                        Ingredient.of(Items.NETHER_STAR),
+                        Ingredient.of(ItemsRegistry.WILDEN_TRIBUTE)
+                )));
+        saveStable(pOutput, BookUpgradeRecipe.CODEC.encodeStart(JsonOps.INSTANCE, apprentice).getOrThrow(), getRecipePath(output, "apprentice_book_upgrade"));
+
+        saveStable(pOutput, BookUpgradeRecipe.CODEC.encodeStart(JsonOps.INSTANCE, archmage).getOrThrow(), getRecipePath(output, "archmage_book_upgrade"));
     }
 
 
