c35af5cd7669161fc3b95fa4c74046040e3421b9
Bailey Hollingsworth <baileyholl@gmail.com>
2024-07-23 20:23:04 -0500

    fix saplings

--- src/main/java/com/hollingsworth/arsnouveau/common/world/tree/MagicTree.java
+++ src/main/java/com/hollingsworth/arsnouveau/common/world/tree/MagicTree.java
@@ -1,15 +1,13 @@
 package com.hollingsworth.arsnouveau.common.world.tree;
 
 import net.minecraft.resources.ResourceKey;
-import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.block.grower.TreeGrower;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
-import org.jetbrains.annotations.Nullable;
 
 import java.util.Optional;
 
 public class MagicTree {
     public static TreeGrower getGrower(String name, ResourceKey<ConfiguredFeature<?, ?>> configConfiguredFeature) {
-        return new TreeGrower(name, Optional.of(configConfiguredFeature), Optional.empty(), Optional.empty());
+        return new TreeGrower(name, Optional.empty(), Optional.of(configConfiguredFeature), Optional.empty());
     }
 }
