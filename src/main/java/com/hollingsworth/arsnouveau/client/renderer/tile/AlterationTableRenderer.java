1742c15e0970a6a32f3e007961b70077d01d5ee9
Bailey Hollingsworth <baileyholl@gmail.com>
2024-08-17 11:19:37 -0500

    Render plain stacks for armor

--- src/main/java/com/hollingsworth/arsnouveau/client/renderer/tile/AlterationTableRenderer.java
+++ src/main/java/com/hollingsworth/arsnouveau/client/renderer/tile/AlterationTableRenderer.java
@@ -3,7 +3,6 @@ package com.hollingsworth.arsnouveau.client.renderer.tile;
 import com.google.common.collect.Maps;
 import com.hollingsworth.arsnouveau.api.perk.PerkSlot;
 import com.hollingsworth.arsnouveau.api.util.PerkUtil;
-import com.hollingsworth.arsnouveau.client.ClientInfo;
 import com.hollingsworth.arsnouveau.client.renderer.item.GenericItemBlockRenderer;
 import com.hollingsworth.arsnouveau.common.block.AlterationTable;
 import com.hollingsworth.arsnouveau.common.block.ThreePartBlock;
@@ -30,7 +29,6 @@ import net.minecraft.core.Holder;
 import net.minecraft.core.component.DataComponents;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.FastColor;
-import net.minecraft.util.Mth;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.ArmorMaterial;
@@ -69,27 +67,27 @@ public class AlterationTableRenderer extends GeoBlockRenderer<AlterationTile> {
         if (!(state.getBlock() instanceof AlterationTable))
             return;
         ItemStack stack = tile.armorStack;
-        if (stack.getItem() instanceof ArmorItem armorItem) {
-            // to rotate around a point: scale, point translate, rotate, object translate
-            matrixStack.scale(0.5f, 0.5f, 0.5f);
-            matrixStack.translate(-2.1, 3.3, 0);
-            double yOffset = Mth.smoothstepDerivative((Math.sin((ClientInfo.ticksInGame + ticks) / 20f) + 1f) / 2f) * 0.0625;
-            if (tile.newPerkTimer >= 0) {
-                // need zero it out or else it fights the translation we're doing below
-                yOffset = 0;
-                float percentage = Mth.abs(tile.newPerkTimer - 20) / 20f;
-                double smooooooooth = Mth.smoothstep(percentage);
-                double perkYOffset = 0.625 - (smooooooooth * 0.625);
-                matrixStack.mulPose(Axis.YP.rotationDegrees((float) (Mth.smoothstep(tile.newPerkTimer / 40f) * 360)));
-                matrixStack.translate(0, perkYOffset, 0);
-            }
-            matrixStack.mulPose(Axis.ZP.rotationDegrees(180F));
-            matrixStack.translate(0, yOffset + rotForSlot(armorItem.getEquipmentSlot()), 0);
-
-            this.renderArmorPiece(tile,stack, matrixStack, iRenderTypeBuffer, packedLightIn, getArmorModel(armorItem.getEquipmentSlot()));
-        } else {
+//        if (stack.getItem() instanceof ArmorItem armorItem) {
+//            // to rotate around a point: scale, point translate, rotate, object translate
+//            matrixStack.scale(0.5f, 0.5f, 0.5f);
+//            matrixStack.translate(-2.1, 3.3, 0);
+//            double yOffset = Mth.smoothstepDerivative((Math.sin((ClientInfo.ticksInGame + ticks) / 20f) + 1f) / 2f) * 0.0625;
+//            if (tile.newPerkTimer >= 0) {
+//                // need zero it out or else it fights the translation we're doing below
+//                yOffset = 0;
+//                float percentage = Mth.abs(tile.newPerkTimer - 20) / 20f;
+//                double smooooooooth = Mth.smoothstep(percentage);
+//                double perkYOffset = 0.625 - (smooooooooth * 0.625);
+//                matrixStack.mulPose(Axis.YP.rotationDegrees((float) (Mth.smoothstep(tile.newPerkTimer / 40f) * 360)));
+//                matrixStack.translate(0, perkYOffset, 0);
+//            }
+//            matrixStack.mulPose(Axis.ZP.rotationDegrees(180F));
+//            matrixStack.translate(0, yOffset + rotForSlot(armorItem.getEquipmentSlot()), 0);
+//
+//            this.renderArmorPiece(tile,stack, matrixStack, iRenderTypeBuffer, packedLightIn, getArmorModel(armorItem.getEquipmentSlot()));
+//        } else {
             Minecraft.getInstance().getItemRenderer().renderStatic(stack, ItemDisplayContext.FIXED, packedLightIn, packedOverlayIn, matrixStack, iRenderTypeBuffer, tile.getLevel(), (int) tile.getBlockPos().asLong());
-        }
+//        }
         matrixStack.popPose();
     }
 
