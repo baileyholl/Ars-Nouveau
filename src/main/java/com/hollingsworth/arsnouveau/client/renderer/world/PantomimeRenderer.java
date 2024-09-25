24c614bd8df428c79723987268c636ceded9a5db
Bailey Hollingsworth <baileyholl@gmail.com>
2024-09-24 20:07:14 -0500

    Update PantomimeRenderer.java

--- src/main/java/com/hollingsworth/arsnouveau/client/renderer/world/PantomimeRenderer.java
+++ src/main/java/com/hollingsworth/arsnouveau/client/renderer/world/PantomimeRenderer.java
@@ -1,9 +1,9 @@
 package com.hollingsworth.arsnouveau.client.renderer.world;
 
 import com.hollingsworth.arsnouveau.api.item.ICasterTool;
-import com.hollingsworth.arsnouveau.api.spell.ISpellCaster;
+import com.hollingsworth.arsnouveau.api.registry.SpellCasterRegistry;
+import com.hollingsworth.arsnouveau.api.spell.AbstractCaster;
 import com.hollingsworth.arsnouveau.api.spell.Spell;
-import com.hollingsworth.arsnouveau.api.util.CasterUtil;
 import com.hollingsworth.arsnouveau.common.spell.method.MethodPantomime;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
@@ -26,7 +26,7 @@ public class PantomimeRenderer {
 
         if (!(player.getItemInHand(InteractionHand.MAIN_HAND).getItem() instanceof ICasterTool)) return;
 
-        ISpellCaster caster = CasterUtil.getCaster(heldItem);
+        AbstractCaster caster = SpellCasterRegistry.from(heldItem);
         Spell selectedSpell = caster.getSpell();
 
         if (!(selectedSpell.getCastMethod() instanceof MethodPantomime pantomime)) return;
@@ -40,7 +40,7 @@ public class PantomimeRenderer {
         RenderType lineType = RenderType.lines();
         OutlineBufferSource buffer = Minecraft.getInstance().renderBuffers().outlineBufferSource();
         VertexConsumer lines = buffer.getBuffer(lineType);
-        int color = selectedSpell.color.getColor();
+        int color = selectedSpell.color().getColor();
         LevelRenderer.renderLineBox(poseStack, lines, 0, 0, 0, 1, 1, 1, FastColor.ARGB32.red(color) / 255F, FastColor.ARGB32.green(color) / 255F, FastColor.ARGB32.blue(color) / 255F, 1);
 
         poseStack.popPose();
