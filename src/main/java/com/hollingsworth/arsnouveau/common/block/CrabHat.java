8c6ba4469f44ff12120c967f6638762a21d21ccd
Bailey Hollingsworth <baileyholl@gmail.com>
2024-09-27 20:36:17 -0500

    crab hat assets

--- src/main/java/com/hollingsworth/arsnouveau/common/block/CrabHat.java
+++ src/main/java/com/hollingsworth/arsnouveau/common/block/CrabHat.java
@@ -1,9 +1,29 @@
 package com.hollingsworth.arsnouveau.common.block;
 
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.shapes.BooleanOp;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
+
+import java.util.stream.Stream;
 
 public class CrabHat extends ModBlock {
     public CrabHat() {
-        super(Properties.of().strength(0.8F).sound(SoundType.WOOL));
+        super(Properties.of().strength(0.8F).sound(SoundType.WOOL).noOcclusion().noLootTable());
+    }
+
+    public static VoxelShape shape = Stream.of(
+            Block.box(0, 13, 0, 16, 17, 16),
+            Block.box(2, 0, 2, 14, 13, 14)
+            ).reduce((v1, v2) -> Shapes.join(v1, v2, BooleanOp.OR)).get();
+
+    @Override
+    protected VoxelShape getShape(BlockState pState, BlockGetter pLevel, BlockPos pPos, CollisionContext pContext) {
+        return shape;
     }
 }
