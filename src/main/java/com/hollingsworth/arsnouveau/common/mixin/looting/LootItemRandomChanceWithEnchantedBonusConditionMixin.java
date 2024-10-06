a0ee3723c957b46f0928dd4f069f0522426603ab
Alexthw <77152778+Alexthw46@users.noreply.github.com>
2024-10-06 18:57:44 +0200

    Fix Luck not applying Looting correctly, restore Drygmy familiar buff (#1427)
    * Fix Loot rolls with Luck
    
    * Convert hardcoded DygmyPerk and Familiar to attribute-based Perk, add related attribute and mob effect, now attribute for LootBoost Mixin
    
    * better docs, move set luck to before dealing damage

--- src/main/java/com/hollingsworth/arsnouveau/common/mixin/looting/LootItemRandomChanceWithEnchantedBonusConditionMixin.java
+++ src/main/java/com/hollingsworth/arsnouveau/common/mixin/looting/LootItemRandomChanceWithEnchantedBonusConditionMixin.java
@@ -1,7 +1,7 @@
 package com.hollingsworth.arsnouveau.common.mixin.looting;
 
-import com.hollingsworth.arsnouveau.api.util.PerkUtil;
-import com.hollingsworth.arsnouveau.common.perk.LootingPerk;
+import com.hollingsworth.arsnouveau.api.perk.PerkAttributes;
+import com.hollingsworth.arsnouveau.api.util.DamageUtil;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
@@ -9,6 +9,7 @@ import net.minecraft.core.Holder;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.level.storage.loot.LootContext;
+import net.minecraft.world.level.storage.loot.parameters.LootContextParams;
 import net.minecraft.world.level.storage.loot.predicates.LootItemRandomChanceWithEnchantedBonusCondition;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -23,10 +24,10 @@ public class LootItemRandomChanceWithEnchantedBonusConditionMixin {
                     target = "Lnet/minecraft/world/item/enchantment/EnchantmentHelper;getEnchantmentLevel(Lnet/minecraft/core/Holder;Lnet/minecraft/world/entity/LivingEntity;)I"
             )
     )
-    private int ars_nouveau$lootingOnGoldSnowball(Holder<Enchantment> enchantment, LivingEntity attacker, Operation<Integer> original, @Local(argsOnly = true) LootContext context) {
-        int add = PerkUtil.countForPerk(LootingPerk.INSTANCE, attacker);
-        // TODO: add FamilarEntity check
-        return original.call(enchantment, attacker) + add;
+    private int ars_nouveau$adjustLooting(Holder<Enchantment> enchantment, LivingEntity attacker, Operation<Integer> original, @Local(argsOnly = true) LootContext context) {
+        int perkLooting = (int) attacker.getAttributeValue(PerkAttributes.DRYGMY);
+        int spellLuck = context.getParam(LootContextParams.DAMAGE_SOURCE) instanceof DamageUtil.SpellDamageSource spellDamageSource ? spellDamageSource.getLuckLevel() : 0;
+        return Math.max(spellLuck, original.call(enchantment, attacker)) + perkLooting;
     }
 
 }
