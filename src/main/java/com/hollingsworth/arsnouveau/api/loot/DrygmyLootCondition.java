8f0faf35e95dcad4578f33181779e5387b29dd3d
Jarva <4622609+Jarva@users.noreply.github.com>
2024-10-16 00:10:32 +0100

    feat: add drygmy loot condition (#1430)
    * feat: add drygmy loot condition
    
    * fix: remove custom name

---
+++ src/main/java/com/hollingsworth/arsnouveau/api/loot/DrygmyLootCondition.java
@@ -0,0 +1,46 @@
+package com.hollingsworth.arsnouveau.api.loot;
+
+import com.google.common.collect.ImmutableSet;
+import com.hollingsworth.arsnouveau.api.ANFakePlayer;
+import com.hollingsworth.arsnouveau.setup.registry.LootRegistry;
+import com.mojang.serialization.MapCodec;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.storage.loot.LootContext;
+import net.minecraft.world.level.storage.loot.parameters.LootContextParam;
+import net.minecraft.world.level.storage.loot.parameters.LootContextParams;
+import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
+import net.minecraft.world.level.storage.loot.predicates.LootItemConditionType;
+
+import java.util.Set;
+
+public class DrygmyLootCondition implements LootItemCondition {
+    public static final MapCodec<DrygmyLootCondition> CODEC = MapCodec.unit(DrygmyLootCondition::new);
+
+    @Override
+    public boolean test(LootContext context) {
+        Entity entity = context.getParamOrNull(LootContextParams.ATTACKING_ENTITY);
+
+        return entity instanceof ANFakePlayer;
+    }
+
+    @Override
+    public Set<LootContextParam<?>> getReferencedContextParams() {
+        return ImmutableSet.of(LootContextParams.ATTACKING_ENTITY);
+    }
+
+    @Override
+    public LootItemConditionType getType() {
+        return LootRegistry.IS_DRYGMY.get();
+    }
+
+    public static DrygmyLootCondition.Builder drygmy() {
+        return new Builder();
+    }
+
+    public static class Builder implements LootItemCondition.Builder {
+        public DrygmyLootCondition build() {
+            return new DrygmyLootCondition();
+        }
+    }
+}
