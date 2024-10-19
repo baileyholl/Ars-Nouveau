a7bd55c964da8fef3fec7ccbb1f229bfa802dcf8
Bailey Hollingsworth <baileyholl@gmail.com>
2024-10-19 13:58:08 -0500

    add wet support for bubble vehicle

--- src/main/java/com/hollingsworth/arsnouveau/common/mixin/EntityMixin.java
+++ src/main/java/com/hollingsworth/arsnouveau/common/mixin/EntityMixin.java
@@ -1,5 +1,6 @@
 package com.hollingsworth.arsnouveau.common.mixin;
 
+import com.hollingsworth.arsnouveau.common.entity.BubbleEntity;
 import com.hollingsworth.arsnouveau.setup.registry.ModPotions;
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import net.minecraft.world.entity.Entity;
@@ -14,7 +15,7 @@ public class EntityMixin {
 
     @ModifyReturnValue(method = "isInWaterOrRain", at = @At("RETURN"))
     private boolean ars_nouveau$isInWaterOrRain(boolean original) {
-        if (((Entity) (Object) this) instanceof LivingEntity livingEntity && livingEntity.hasEffect(ModPotions.SOAKED_EFFECT)) {
+        if (((Entity) (Object) this) instanceof LivingEntity livingEntity && isWet(livingEntity)) {
             return true;
         }
         return original;
@@ -22,10 +23,14 @@ public class EntityMixin {
 
     @Inject(method = "setRemainingFireTicks", at = @At("HEAD"), cancellable = true)
     private void ars_nouveau$setSecondsOnFire(int pRemainingFireTicks, CallbackInfo ci) {
-        if (((Entity) (Object) this) instanceof LivingEntity livingEntity && livingEntity.hasEffect(ModPotions.SOAKED_EFFECT) && pRemainingFireTicks > 0) {
+        if (((Entity) (Object) this) instanceof LivingEntity livingEntity && isWet(livingEntity) && pRemainingFireTicks > 0) {
             livingEntity.clearFire();
             ci.cancel();
         }
     }
 
+
+    private static boolean isWet(LivingEntity livingEntity){
+        return livingEntity.hasEffect(ModPotions.SOAKED_EFFECT) || livingEntity.getVehicle() instanceof BubbleEntity;
+    }
 }
