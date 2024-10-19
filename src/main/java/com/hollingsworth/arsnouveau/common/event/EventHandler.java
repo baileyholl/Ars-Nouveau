26bb3e73f0516ca9939bf5af580419e9a39f89da
Jarva <4622609+Jarva@users.noreply.github.com>
2024-10-19 19:33:21 +0100

    feat: add command to summon adopted starbuncles (#1438)
    * Update supporters.json
    
    * feat: add command to summon adopted starbuncles
    
    ---------
    
    Co-authored-by: Bailey Hollingsworth <baileyholl@gmail.com>

--- src/main/java/com/hollingsworth/arsnouveau/common/event/EventHandler.java
+++ src/main/java/com/hollingsworth/arsnouveau/common/event/EventHandler.java
@@ -343,6 +343,7 @@ public class EventHandler {
         AddTomeCommand.register(event.getDispatcher());
         SummonAnimHeadCommand.register(event.getDispatcher());
         LearnGlyphCommand.register(event.getDispatcher());
+        AdoptCommand.register(event.getDispatcher());
     }
 
     @SubscribeEvent
