7a53e229230e2459154ec3ed921e5e8773dddbbf
Alexthw <77152778+Alexthw46@users.noreply.github.com>
2024-10-29 01:04:39 +0100

    swap IER with direct item entity in imbuement chamber. example localRuntime swap (#1467)

--- src/main/java/com/hollingsworth/arsnouveau/client/renderer/tile/ImbuementRenderer.java
+++ src/main/java/com/hollingsworth/arsnouveau/client/renderer/tile/ImbuementRenderer.java
@@ -1,14 +1,15 @@
 package com.hollingsworth.arsnouveau.client.renderer.tile;
 
+import com.hollingsworth.arsnouveau.client.ClientInfo;
 import com.hollingsworth.arsnouveau.common.block.tile.ImbuementTile;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
+import com.mojang.math.Axis;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
-import net.minecraft.world.entity.item.ItemEntity;
-import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.ItemDisplayContext;
 import software.bernie.geckolib.cache.object.BakedGeoModel;
 
 public class ImbuementRenderer extends ArsGeoBlockRenderer<ImbuementTile> {
@@ -20,25 +21,22 @@ public class ImbuementRenderer extends ArsGeoBlockRenderer<ImbuementTile> {
     @Override
     public void actuallyRender(PoseStack matrixStack, ImbuementTile tileEntityIn, BakedGeoModel model, RenderType renderType, MultiBufferSource bufferSource, VertexConsumer buffer, boolean isReRender, float partialTick, int packedLight, int packedOverlay, int color) {
         super.actuallyRender(matrixStack, tileEntityIn, model, renderType, bufferSource, buffer, isReRender, partialTick, packedLight, packedOverlay, color);
-        double x = tileEntityIn.getBlockPos().getX();
-        double y = tileEntityIn.getBlockPos().getY();
-        double z = tileEntityIn.getBlockPos().getZ();
-        if (tileEntityIn.entity == null || !ItemStack.matches(tileEntityIn.entity.getItem(), tileEntityIn.stack)) {
-            tileEntityIn.entity = new ItemEntity(tileEntityIn.getLevel(), x, y, z, tileEntityIn.stack);
+        if (tileEntityIn.getStack() == null || tileEntityIn.getStack().isEmpty()) return;
 
-        }
-        if (tileEntityIn.entity != null) {
-            ItemEntity entityItem = tileEntityIn.entity;
-            tileEntityIn.frames += 1.5f * partialTick;
-            entityItem.setYHeadRot(tileEntityIn.frames);
-            entityItem.age = (int) tileEntityIn.frames;
-            matrixStack.pushPose();
-            matrixStack.translate(-0.5, 0, -0.5);
-            matrixStack.scale(0.75f, 0.75f, 0.75f);
-            float offset = 0.5f * 0.75f + 0.31f;
-            Minecraft.getInstance().getEntityRenderDispatcher().render(entityItem, offset, 0.3, offset, entityItem.yRot, partialTick, matrixStack, bufferSource, packedLight);
-            matrixStack.popPose();
-        }
+        matrixStack.pushPose();
+        matrixStack.translate(0.f, 0.5f, 0.f);
+        matrixStack.scale(0.5f, 0.5f, 0.5f);
+        matrixStack.mulPose(Axis.YP.rotationDegrees((partialTick + (float) ClientInfo.ticksInGame) * 3f));
+        Minecraft.getInstance().getItemRenderer().renderStatic(tileEntityIn.getStack(),
+                ItemDisplayContext.FIXED,
+                packedLight,
+                packedOverlay,
+                matrixStack,
+                bufferSource,
+                tileEntityIn.getLevel(),
+                (int) tileEntityIn.getBlockPos().asLong());
+        matrixStack.popPose();
+
     }
 
 }
