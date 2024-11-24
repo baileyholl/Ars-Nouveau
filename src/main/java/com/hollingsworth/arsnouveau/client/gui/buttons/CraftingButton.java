9252e7705d62fb3d18bc427a9339d98f57e3082b
Bailey Hollingsworth <baileyholl@gmail.com>
2024-11-24 15:18:24 -0600

    Show extended tooltip on crafting cell

--- src/main/java/com/hollingsworth/arsnouveau/client/gui/buttons/CraftingButton.java
+++ src/main/java/com/hollingsworth/arsnouveau/client/gui/buttons/CraftingButton.java
@@ -2,13 +2,20 @@ package com.hollingsworth.arsnouveau.client.gui.buttons;
 
 import com.hollingsworth.arsnouveau.api.spell.AbstractAugment;
 import com.hollingsworth.arsnouveau.api.spell.AbstractSpellPart;
+import com.hollingsworth.arsnouveau.api.spell.SpellSchool;
 import com.hollingsworth.arsnouveau.api.spell.SpellValidationError;
 import com.hollingsworth.arsnouveau.client.gui.utils.RenderUtils;
 import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.ChatFormatting;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.Button;
+import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.Style;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.ModList;
+import net.neoforged.neoforgespi.language.IModInfo;
 
 import javax.annotation.Nullable;
 import java.util.LinkedList;
@@ -60,22 +67,36 @@ public class CraftingButton extends GuiImageButton {
     }
 
     @Override
-    public void getTooltip(List<Component> tooltip) {
+    public void getTooltip(List<Component> tip) {
         if (abstractSpellPart != null) {
-            tooltip.add(Component.translatable(abstractSpellPart.getLocalizationKey()));
+            tip.add(Component.translatable(abstractSpellPart.getLocalizationKey()));
             for (SpellValidationError ve : validationErrors) {
-                tooltip.add(ve.makeTextComponentExisting().withStyle(ChatFormatting.RED));
+                tip.add(ve.makeTextComponentExisting().withStyle(ChatFormatting.RED));
             }
-
             if(abstractSpellPart instanceof AbstractAugment augment && augmentedParent != null) {
                 if(validationErrors != null && !validationErrors.isEmpty()){
                     return;
                 }
                 Component augmentDescription = augmentedParent.augmentDescriptions.get(augment);
                 if (augmentDescription != null) {
-                    tooltip.add(Component.translatable("ars_nouveau.augmenting", augmentedParent.getLocaleName()));
-                    tooltip.add(augmentDescription.copy().withStyle(ChatFormatting.GOLD));
+                    tip.add(Component.translatable("ars_nouveau.augmenting", augmentedParent.getLocaleName()));
+                    tip.add(augmentDescription.copy().withStyle(ChatFormatting.GOLD));
                 }
+            }
+            if (Screen.hasShiftDown()) {
+                tip.add(Component.translatable("tooltip.ars_nouveau.glyph_level", abstractSpellPart.getConfigTier().value).setStyle(Style.EMPTY.withColor(ChatFormatting.BLUE)));
+                tip.add(Component.translatable("ars_nouveau.schools"));
+                for (SpellSchool s : abstractSpellPart.spellSchools) {
+                    tip.add(s.getTextComponent());
+                }
+                tip.add(abstractSpellPart.getBookDescLang());
+            } else {
+                tip.add(Component.translatable("tooltip.ars_nouveau.hold_shift", Minecraft.getInstance().options.keyShift.getKey().getDisplayName()));
+                var modName = ModList.get()
+                        .getModContainerById(abstractSpellPart.getRegistryName().getNamespace())
+                        .map(ModContainer::getModInfo)
+                        .map(IModInfo::getDisplayName).orElse(abstractSpellPart.getRegistryName().getNamespace());
+                tip.add(Component.literal(modName).withStyle(ChatFormatting.BLUE));
             }
         }
     }
