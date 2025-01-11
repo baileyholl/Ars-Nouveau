6f6166bcdca498d381498cd5bc864af5927d281c
Jarva <4622609+Jarva@users.noreply.github.com>
2025-01-11 20:43:06 +0000

    feat: add function for perk added and removed

--- src/main/java/com/hollingsworth/arsnouveau/api/perk/IPerk.java
+++ src/main/java/com/hollingsworth/arsnouveau/api/perk/IPerk.java
@@ -8,6 +8,7 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.EquipmentSlotGroup;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.attributes.Attribute;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.entity.player.Player;
@@ -90,4 +91,7 @@ public interface IPerk {
     default String getDescriptionKey() {
         return getRegistryName().getNamespace() + ".perk_desc." + getRegistryName().getPath();
     }
+
+    default void onRemoved(LivingEntity entity) {}
+    default void onAdded(LivingEntity entity) {}
 }
