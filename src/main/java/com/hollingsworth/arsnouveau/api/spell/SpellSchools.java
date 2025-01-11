fa44a3a5d1d279eb1fcc7e06c409a425c2f0f474
Alexthw <77152778+Alexthw46@users.noreply.github.com>
2025-01-11 20:05:40 +0100

    make SpellSchools final so they can be used in switch blocks (#1580)

--- src/main/java/com/hollingsworth/arsnouveau/api/spell/SpellSchools.java
+++ src/main/java/com/hollingsworth/arsnouveau/api/spell/SpellSchools.java
@@ -2,13 +2,13 @@ package com.hollingsworth.arsnouveau.api.spell;
 
 public class SpellSchools {
 
-    public static SpellSchool ABJURATION = new SpellSchool("abjuration");
-    public static SpellSchool CONJURATION = new SpellSchool("conjuration");
-    public static SpellSchool NECROMANCY = new SpellSchool("necromancy");
-    public static SpellSchool MANIPULATION = new SpellSchool("manipulation");
-    public static SpellSchool ELEMENTAL_AIR = new SpellSchool("air");
-    public static SpellSchool ELEMENTAL_EARTH = new SpellSchool("earth");
-    public static SpellSchool ELEMENTAL_FIRE = new SpellSchool("fire");
-    public static SpellSchool ELEMENTAL_WATER = new SpellSchool("water");
-    public static SpellSchool ELEMENTAL = new SpellSchool("elemental").withSubSchool(ELEMENTAL_AIR).withSubSchool(ELEMENTAL_EARTH).withSubSchool(ELEMENTAL_FIRE).withSubSchool(ELEMENTAL_WATER);
+    public static final SpellSchool ABJURATION = new SpellSchool("abjuration");
+    public static final SpellSchool CONJURATION = new SpellSchool("conjuration");
+    public static final SpellSchool NECROMANCY = new SpellSchool("necromancy");
+    public static final SpellSchool MANIPULATION = new SpellSchool("manipulation");
+    public static final SpellSchool ELEMENTAL_AIR = new SpellSchool("air");
+    public static final SpellSchool ELEMENTAL_EARTH = new SpellSchool("earth");
+    public static final SpellSchool ELEMENTAL_FIRE = new SpellSchool("fire");
+    public static final SpellSchool ELEMENTAL_WATER = new SpellSchool("water");
+    public static final SpellSchool ELEMENTAL = new SpellSchool("elemental").withSubSchool(ELEMENTAL_AIR).withSubSchool(ELEMENTAL_EARTH).withSubSchool(ELEMENTAL_FIRE).withSubSchool(ELEMENTAL_WATER);
 }
