363a6256fcd4e896aeb7f86514ee3dcd80c441e4
Qther <qther@tuta.io>
2025-02-02 13:42:24 +0800

    clean up imports

--- src/main/java/com/hollingsworth/arsnouveau/common/block/tile/MobJarTile.java
+++ src/main/java/com/hollingsworth/arsnouveau/common/block/tile/MobJarTile.java
@@ -24,7 +24,6 @@ import net.minecraft.world.level.ClipContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.AABB;
-import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.HitResult;
 import net.minecraft.world.phys.Vec3;
 import net.minecraft.world.phys.shapes.CollisionContext;
