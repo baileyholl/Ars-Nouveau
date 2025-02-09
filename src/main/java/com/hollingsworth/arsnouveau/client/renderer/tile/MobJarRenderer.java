8d26ba848bf99e1e1ad4c1abcd70c49e5f0dfd64
Qther <qther@tuta.io>
2025-02-09 08:31:08 +0800

    remove EntityRendererDispatcher#shouldRender check
    slower than not doing it

--- src/main/java/com/hollingsworth/arsnouveau/client/renderer/tile/MobJarRenderer.java
+++ src/main/java/com/hollingsworth/arsnouveau/client/renderer/tile/MobJarRenderer.java
@@ -6,7 +6,6 @@ import com.hollingsworth.arsnouveau.common.block.MobJar;
 import com.hollingsworth.arsnouveau.common.block.tile.MobJarTile;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.math.Axis;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
@@ -21,7 +20,8 @@ import java.util.concurrent.atomic.AtomicReference;
 
 public class MobJarRenderer implements BlockEntityRenderer<MobJarTile> {
     private final EntityRenderDispatcher entityRenderer;
-    public MobJarRenderer(BlockEntityRendererProvider.Context pContext){
+
+    public MobJarRenderer(BlockEntityRendererProvider.Context pContext) {
         entityRenderer = pContext.getEntityRenderer();
     }
 
@@ -33,48 +33,48 @@ public class MobJarRenderer implements BlockEntityRenderer<MobJarTile> {
     @Override
     public void render(MobJarTile pBlockEntity, float pPartialTick, PoseStack pPoseStack, MultiBufferSource pBufferSource, int pPackedLight, int pPackedOverlay) {
         Entity entity = pBlockEntity.getEntity();
-        if(entity == null)
+        if (entity == null)
             return;
         float f = 0.53125F;
         float f1 = Math.max(entity.getBbWidth(), entity.getBbHeight());
 
-        if ((double)f1 > 1.0d) {
+        if ((double) f1 > 1.0d) {
             f /= f1 * 1.0;
         }
-        if(entity instanceof LightningBolt bolt){
-            if(bolt.level.getGameTime() % 20 == 0)
+        if (entity instanceof LightningBolt bolt) {
+            if (bolt.level.getGameTime() % 20 == 0)
                 bolt.seed = ClientInfo.ticksInGame;
             f = 0.0075f;
         }
         AtomicReference<Vec3> adjustedScale = new AtomicReference<>(new Vec3(0, 0, 0));
         AtomicReference<Vec3> adjustedTranslation = new AtomicReference<>(new Vec3(0, 0, 0));
         AtomicReference<Boolean> shouldParticlaTick = new AtomicReference<>(false);
-        JarBehaviorRegistry.forEach(entity, jarBehavior ->{
+        JarBehaviorRegistry.forEach(entity, jarBehavior -> {
             Vec3 customScale = jarBehavior.scaleOffset(pBlockEntity);
             adjustedScale.set(adjustedScale.get().add(customScale));
             adjustedTranslation.set(adjustedTranslation.get().add(jarBehavior.translate(pBlockEntity)));
-            if(jarBehavior.shouldUsePartialTicks(pBlockEntity))
+            if (jarBehavior.shouldUsePartialTicks(pBlockEntity))
                 shouldParticlaTick.set(true);
         });
 
-        Vec3 scale = new Vec3(f, f, f).multiply(adjustedScale.get().add(1,1,1));
+        Vec3 scale = new Vec3(f, f, f).multiply(adjustedScale.get().add(1, 1, 1));
         Vec3 translate = new Vec3(0.5, 0, 0.5).add(adjustedTranslation.get());
         pPoseStack.translate(translate.x, translate.y, translate.z);
         pPoseStack.scale((float) scale.x, (float) scale.y, (float) scale.z);
 
         Direction direction = pBlockEntity.getBlockState().getValue(MobJar.FACING);
-        if(direction == Direction.EAST) {
+        if (direction == Direction.EAST) {
             pPoseStack.mulPose(Axis.ZP.rotationDegrees(90.0F));
-        }else if(direction == Direction.WEST){
+        } else if (direction == Direction.WEST) {
             pPoseStack.mulPose(Axis.ZP.rotationDegrees(-90.0F));
-        }else if(direction == Direction.NORTH){
+        } else if (direction == Direction.NORTH) {
             pPoseStack.mulPose(Axis.XP.rotationDegrees(90.0F));
-        }else if(direction == Direction.SOUTH){
+        } else if (direction == Direction.SOUTH) {
             pPoseStack.mulPose(Axis.XP.rotationDegrees(-90.0F));
         }
         pPoseStack.mulPose(pBlockEntity.getBlockState().getValue(MobJar.FACING).getRotation());
-        entity.setDeltaMovement(0,0,0);
-        if(shouldParticlaTick.get()) {
+        entity.setDeltaMovement(0, 0, 0);
+        if (shouldParticlaTick.get()) {
             entity.xo = entity.getX();
             entity.yo = entity.getY();
             entity.zo = entity.getZ();
@@ -85,18 +85,13 @@ public class MobJarRenderer implements BlockEntityRenderer<MobJarTile> {
                 livingEntity.yHeadRotO = livingEntity.yHeadRot;
                 // TODO: readd old animation speed for allay?
             }
-        }else{
+        } else {
             pPartialTick = 0;
         }
 
-        var frustum = Minecraft.getInstance().levelRenderer.getFrustum();
-        var camera = Minecraft.getInstance().gameRenderer.getMainCamera();
-        var camPos = camera.getPosition();
-        if (this.entityRenderer.shouldRender(entity, frustum, camPos.x, camPos.y, camPos.z)) {
-            this.entityRenderer.render(entity, 0.0D, 0.0D, 0.0D, 0.0F, pPartialTick, pPoseStack, pBufferSource, pPackedLight);
-            for (Entity entity1 : entity.getPassengers()) {
-                this.entityRenderer.render(entity1, 0.0D, 0.0D, 0.0D, 0.0F, pPartialTick, pPoseStack, pBufferSource, pPackedLight);
-            }
+        this.entityRenderer.render(entity, 0.0D, 0.0D, 0.0D, 0.0F, pPartialTick, pPoseStack, pBufferSource, pPackedLight);
+        for (Entity entity1 : entity.getPassengers()) {
+            this.entityRenderer.render(entity1, 0.0D, 0.0D, 0.0D, 0.0F, pPartialTick, pPoseStack, pBufferSource, pPackedLight);
         }
     }
 }
