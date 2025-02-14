fbf3474bfbbf0b16edccce44e9487d912a28af2d
Qther <qther@tuta.io>
2025-02-14 23:51:57 +0800

    use ModifyReturnValue instead of WrapMethod

--- src/main/java/com/hollingsworth/arsnouveau/common/mixin/TreeGrowerMixin.java
+++ src/main/java/com/hollingsworth/arsnouveau/common/mixin/TreeGrowerMixin.java
@@ -1,26 +1,24 @@
 package com.hollingsworth.arsnouveau.common.mixin;
 
 import com.hollingsworth.arsnouveau.api.event.SuccessfulTreeGrowthEvent;
-import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.block.grower.TreeGrower;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.chunk.ChunkGenerator;
 import net.neoforged.neoforge.common.NeoForge;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(TreeGrower.class)
 public class TreeGrowerMixin {
-    @WrapMethod(method = "growTree")
-    public boolean growTree(ServerLevel level, ChunkGenerator chunkGenerator, BlockPos pos, BlockState state, RandomSource random, Operation<Boolean> original) {
-        if (original.call(level, chunkGenerator, pos, state, random)) {
+    @ModifyReturnValue(method = "growTree", at = @At("RETURN"))
+    public boolean growTree(boolean original, @Local(argsOnly = true) ServerLevel level, @Local(argsOnly = true) BlockPos pos, @Local(argsOnly = true) BlockState state) {
+        if (original) {
             NeoForge.EVENT_BUS.post(new SuccessfulTreeGrowthEvent(level, pos, state));
-            return true;
         }
 
-        return false;
+        return original;
     }
 }
