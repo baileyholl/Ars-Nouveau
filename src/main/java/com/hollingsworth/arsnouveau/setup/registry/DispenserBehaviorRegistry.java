df3f96ff2630776a1fac2cb188b36d402b66fbdb
Qther <qther@tuta.io>
2025-02-16 00:28:34 +0800

    remove unused direction

--- src/main/java/com/hollingsworth/arsnouveau/setup/registry/DispenserBehaviorRegistry.java
+++ src/main/java/com/hollingsworth/arsnouveau/setup/registry/DispenserBehaviorRegistry.java
@@ -4,7 +4,6 @@ import com.hollingsworth.arsnouveau.common.block.SourceJar;
 import com.hollingsworth.arsnouveau.common.block.tile.SourceJarTile;
 import com.hollingsworth.arsnouveau.common.items.data.BlockFillContents;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
 import net.minecraft.core.dispenser.BlockSource;
 import net.minecraft.core.dispenser.OptionalDispenseItemBehavior;
 import net.minecraft.world.item.ItemStack;
@@ -20,7 +19,6 @@ public class DispenserBehaviorRegistry {
             @SuppressWarnings("resource")
             protected @NotNull ItemStack execute(@NotNull BlockSource blockSource, @NotNull ItemStack item) {
                 Level level = blockSource.level();
-                Direction direction = blockSource.state().getValue(DispenserBlock.FACING);
                 BlockPos pos = blockSource.pos().relative(blockSource.state().getValue(DispenserBlock.FACING));
                 SourceJar jar = BlockRegistry.SOURCE_JAR.get();
                 if (level.isEmptyBlock(pos)) {
