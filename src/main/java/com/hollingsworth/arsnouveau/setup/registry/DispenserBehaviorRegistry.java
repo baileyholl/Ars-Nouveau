27f360b643dc33efa782d5e0926e36ca96515391
Qther <qther@tuta.io>
2025-02-16 00:37:54 +0800

    containment jars too

--- src/main/java/com/hollingsworth/arsnouveau/setup/registry/DispenserBehaviorRegistry.java
+++ src/main/java/com/hollingsworth/arsnouveau/setup/registry/DispenserBehaviorRegistry.java
@@ -3,13 +3,18 @@ package com.hollingsworth.arsnouveau.setup.registry;
 import com.hollingsworth.arsnouveau.api.registry.RitualRegistry;
 import com.hollingsworth.arsnouveau.api.ritual.DispenserRitualBehavior;
 import com.hollingsworth.arsnouveau.common.block.CreativeSourceJar;
+import com.hollingsworth.arsnouveau.common.block.MobJar;
 import com.hollingsworth.arsnouveau.common.block.SourceJar;
+import com.hollingsworth.arsnouveau.common.block.tile.MobJarTile;
 import com.hollingsworth.arsnouveau.common.block.tile.SourceJarTile;
+import com.hollingsworth.arsnouveau.common.items.MobJarItem;
 import com.hollingsworth.arsnouveau.common.items.RitualTablet;
 import com.hollingsworth.arsnouveau.common.items.data.BlockFillContents;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.core.dispenser.BlockSource;
 import net.minecraft.core.dispenser.OptionalDispenseItemBehavior;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.DispenserBlock;
@@ -61,6 +66,37 @@ public class DispenserBehaviorRegistry {
                         var state = jar.defaultBlockState();
                         level.setBlock(pos, state, 3);
                         level.gameEvent(null, GameEvent.BLOCK_PLACE, pos);
+                    }
+
+                    item.shrink(1);
+                    this.setSuccess(true);
+                } else {
+                    this.setSuccess(false);
+                }
+                return item;
+            }
+        });
+
+        DispenserBlock.registerBehavior(BlockRegistry.MOB_JAR.get(), new OptionalDispenseItemBehavior() {
+            @Override
+            @SuppressWarnings("resource")
+            protected @NotNull ItemStack execute(@NotNull BlockSource blockSource, @NotNull ItemStack item) {
+                Level level = blockSource.level();
+                Direction direction = blockSource.state().getValue(DispenserBlock.FACING);
+                BlockPos pos = blockSource.pos().relative(blockSource.state().getValue(DispenserBlock.FACING));
+                MobJar jar = BlockRegistry.MOB_JAR.get();
+                if (level.isEmptyBlock(pos)) {
+                    if (!level.isClientSide) {
+                        var state = jar.defaultBlockState().setValue(MobJar.FACING, direction);
+                        level.setBlock(pos, state, 3);
+                        level.gameEvent(null, GameEvent.BLOCK_PLACE, pos);
+                        var be = level.getBlockEntity(pos);
+                        if (be instanceof MobJarTile tile) {
+                            Entity entity = MobJarItem.fromItem(item, level);
+                            if (entity != null) {
+                                tile.setEntityData(entity);
+                            }
+                        }
                     }
 
                     item.shrink(1);
