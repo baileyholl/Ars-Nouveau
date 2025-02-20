e5748a457a7f5b02d8d8b7e7b3472d5d75f598ff
Qther <qther@tuta.io>
2025-02-20 20:58:48 +0800

    feat: potion jar inventory manipulation

--- src/main/java/com/hollingsworth/arsnouveau/common/block/PotionJar.java
+++ src/main/java/com/hollingsworth/arsnouveau/common/block/PotionJar.java
@@ -1,6 +1,8 @@
 package com.hollingsworth.arsnouveau.common.block;
 
+import com.hollingsworth.arsnouveau.ArsNouveau;
 import com.hollingsworth.arsnouveau.common.block.tile.PotionJarTile;
+import com.hollingsworth.arsnouveau.common.items.data.PotionJarData;
 import com.hollingsworth.arsnouveau.common.util.ItemUtil;
 import com.hollingsworth.arsnouveau.setup.registry.BlockRegistry;
 import com.hollingsworth.arsnouveau.setup.registry.DataComponentRegistry;
@@ -9,14 +11,13 @@ import net.minecraft.core.Direction;
 import net.minecraft.core.component.DataComponents;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.ItemInteractionResult;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
-import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.inventory.ClickAction;
+import net.minecraft.world.item.*;
 import net.minecraft.world.item.alchemy.PotionContents;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
@@ -38,6 +39,9 @@ import net.minecraft.world.phys.shapes.BooleanOp;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.ItemStackedOnOtherEvent;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
@@ -45,13 +49,14 @@ import java.util.stream.Stream;
 
 import static net.minecraft.world.level.block.state.properties.BlockStateProperties.WATERLOGGED;
 
+@EventBusSubscriber(modid = ArsNouveau.MODID)
 public class PotionJar extends ModBlock implements SimpleWaterloggedBlock, EntityBlock {
     public PotionJar(Properties properties) {
         super(properties);
         registerDefaultState(defaultBlockState().setValue(BlockStateProperties.WATERLOGGED, false));
     }
 
-    public PotionJar(){
+    public PotionJar() {
         this(ModBlock.defaultProperties().noOcclusion());
     }
 
@@ -79,7 +84,7 @@ public class PotionJar extends ModBlock implements SimpleWaterloggedBlock, Entit
         PotionContents potion = stack.get(DataComponents.POTION_CONTENTS);
 
         if (stack.getItem() == Items.POTION && potion != null && potion != PotionContents.EMPTY) {
-            if (tile.canAccept(potion,100)) {
+            if (tile.canAccept(potion, 100)) {
                 tile.add(potion, 100);
                 if (!player.hasInfiniteMaterials()) {
                     stack.shrink(1);
@@ -87,18 +92,18 @@ public class PotionJar extends ModBlock implements SimpleWaterloggedBlock, Entit
                 }
             }
             return super.useItemOn(stack, state, worldIn, pos, player, handIn, hit);
-        }else if (stack.getItem() == Items.GLASS_BOTTLE && tile.getAmount() >= 100) {
+        } else if (stack.getItem() == Items.GLASS_BOTTLE && tile.getAmount() >= 100) {
             ItemStack potionStack = new ItemStack(Items.POTION);
             PotionContents contents = tile.getData();
             potionStack.set(DataComponents.POTION_CONTENTS, new PotionContents(contents.potion(), contents.customColor(), contents.customEffects()));
-            if(ItemUtil.shrinkHandAndAddStack(potionStack, handIn, player)){
+            if (ItemUtil.shrinkHandAndAddStack(potionStack, handIn, player)) {
                 tile.remove(100);
             }
-        }else if(stack.getItem() == Items.ARROW && tile.getAmount() >= 10){
+        } else if (stack.getItem() == Items.ARROW && tile.getAmount() >= 10) {
             ItemStack potionStack = new ItemStack(Items.TIPPED_ARROW);
             PotionContents contents = tile.getData();
             potionStack.set(DataComponents.POTION_CONTENTS, new PotionContents(contents.potion(), contents.customColor(), contents.customEffects()));
-            if(ItemUtil.shrinkHandAndAddStack(potionStack, handIn, player)) {
+            if (ItemUtil.shrinkHandAndAddStack(potionStack, handIn, player)) {
                 tile.remove(10);
             }
         }
@@ -124,11 +129,11 @@ public class PotionJar extends ModBlock implements SimpleWaterloggedBlock, Entit
     @Override
     public void appendHoverText(ItemStack stack, Item.TooltipContext pContext, List<Component> tooltip, TooltipFlag pTooltipFlag) {
         var potion = stack.get(DataComponentRegistry.POTION_JAR);
-        if(potion == null)
+        if (potion == null)
             return;
         var fill = potion.fill();
         var data = potion.contents();
-        if(!data.equals(PotionContents.EMPTY)) {
+        if (!data.equals(PotionContents.EMPTY)) {
             ItemStack potionItem = new ItemStack(Items.POTION);
             potionItem.set(DataComponents.POTION_CONTENTS, data);
             tooltip.add(Component.translatable(potionItem.getDescriptionId()));
@@ -142,7 +147,7 @@ public class PotionJar extends ModBlock implements SimpleWaterloggedBlock, Entit
         return state.getValue(WATERLOGGED) ? Fluids.WATER.getSource(false) : Fluids.EMPTY.defaultFluidState();
     }
 
-   @NotNull
+    @NotNull
     @Override
     public BlockState getStateForPlacement(BlockPlaceContext context) {
         FluidState fluidState = context.getLevel().getFluidState(context.getClickedPos());
@@ -153,7 +158,7 @@ public class PotionJar extends ModBlock implements SimpleWaterloggedBlock, Entit
     @Override
     public void tick(BlockState p_222945_, ServerLevel level, BlockPos pos, RandomSource p_222948_) {
         super.tick(p_222945_, level, pos, p_222948_);
-        if(level.getBlockEntity(pos) instanceof PotionJarTile jarTile){
+        if (level.getBlockEntity(pos) instanceof PotionJarTile jarTile) {
             jarTile.updateBlock();
         }
     }
@@ -181,5 +186,205 @@ public class PotionJar extends ModBlock implements SimpleWaterloggedBlock, Entit
     @Override
     public boolean isPathfindable(BlockState pState, PathComputationType pType) {
         return false;
+    }
+
+    enum StackingMode {
+        FillJar,
+        TransferJar,
+        FillBottle,
+        TipArrow,
+    }
+
+    @SubscribeEvent
+    public static void onStack(ItemStackedOnOtherEvent event) {
+        if (event.getClickAction() != ClickAction.SECONDARY) {
+            return;
+        }
+
+        if (!(event.getPlayer() instanceof ServerPlayer player)) {
+            return;
+        }
+
+        ItemStack from;
+        ItemStack to;
+        StackingMode mode;
+
+        // No idea if intentional, but the order of these seem swapped.
+        // See https://github.com/neoforged/NeoForge/blob/1.21.1/patches/net/minecraft/world/inventory/AbstractContainerMenu.java.patch
+        // and the patched net.minecraft.world.inventory.AbstractContainerMenu#tryItemClickBehaviourOverride
+        var carried = event.getStackedOnItem();
+        var stacked = event.getCarriedItem();
+        if (carried.isEmpty() || stacked.isEmpty()) {
+            return;
+        }
+
+        if (carried.getItem() instanceof PotionItem && stacked.is(BlockRegistry.POTION_JAR.asItem())) {
+            from = carried;
+            to = stacked;
+            mode = StackingMode.FillJar;
+        } else if (carried.is(BlockRegistry.POTION_JAR.asItem()) && stacked.getItem() instanceof PotionItem) {
+            from = stacked;
+            to = carried;
+            mode = StackingMode.FillJar;
+        } else if (carried.is(Items.GLASS_BOTTLE) && stacked.is(BlockRegistry.POTION_JAR.asItem())) {
+            from = stacked;
+            to = carried;
+            mode = StackingMode.FillBottle;
+        } else if (carried.is(BlockRegistry.POTION_JAR.asItem()) && stacked.is(Items.GLASS_BOTTLE)) {
+            from = carried;
+            to = stacked;
+            mode = StackingMode.FillBottle;
+        } else if (carried.is(Items.ARROW) && stacked.is(BlockRegistry.POTION_JAR.asItem())) {
+            from = stacked;
+            to = carried;
+            mode = StackingMode.TipArrow;
+        } else if (carried.is(BlockRegistry.POTION_JAR.asItem()) && stacked.is(Items.ARROW)) {
+            from = carried;
+            to = stacked;
+            mode = StackingMode.TipArrow;
+        } else if (carried.is(BlockRegistry.POTION_JAR.asItem()) && stacked.is(BlockRegistry.POTION_JAR.asItem())) {
+            from = carried;
+            to = stacked;
+            mode = StackingMode.TransferJar;
+        } else {
+            return;
+        }
+
+        switch (mode) {
+            case FillJar -> {
+                if (to.getCount() != 1) {
+                    return;
+                }
+
+                var potionData = from.get(DataComponents.POTION_CONTENTS);
+                var jarData = to.getOrDefault(DataComponentRegistry.POTION_JAR, new PotionJarData(0, PotionContents.EMPTY, false));
+                if (potionData == null || potionData.potion().isEmpty()) {
+                    return;
+                }
+
+                var space = PotionJarTile.MAX_FILL - jarData.fill();
+                if (space < 100) {
+                    return;
+                }
+
+                var potions = Math.min(from.getCount(), space / 100);
+                var fill = 100 * potions;
+
+                if (!jarData.canAccept(potionData, fill, PotionJarTile.MAX_FILL)) {
+                    return;
+                }
+
+                var result = jarData.add(potionData, fill, PotionJarTile.MAX_FILL).left();
+                to.set(DataComponentRegistry.POTION_JAR, result);
+                if (!player.hasInfiniteMaterials()) {
+                    from.shrink(potions);
+                    var emptied = new ItemStack(Items.GLASS_BOTTLE, potions);
+                    if (player.containerMenu.getCarried().isEmpty()) {
+                        player.containerMenu.setCarried(emptied);
+                    } else {
+                        player.addItem(emptied);
+                    }
+                }
+
+                event.setCanceled(true);
+            }
+            case TransferJar -> {
+                if (to.getCount() != 1 || ItemStack.isSameItemSameComponents(to, from)) {
+                    return;
+                }
+
+                var fromData = from.getOrDefault(DataComponentRegistry.POTION_JAR, new PotionJarData(0, PotionContents.EMPTY, false));
+                var toData = to.getOrDefault(DataComponentRegistry.POTION_JAR, new PotionJarData(0, PotionContents.EMPTY, false));
+                if (fromData.contents().potion().isEmpty() || fromData.fill() <= 0) {
+                    return;
+                }
+
+                var space = PotionJarTile.MAX_FILL - toData.fill();
+                var fill = Math.min(fromData.fill() * from.getCount(), space);
+
+                if (!toData.canAccept(fromData.contents(), fill, PotionJarTile.MAX_FILL)) {
+                    return;
+                }
+
+                var result = toData.add(fromData.contents(), fill, PotionJarTile.MAX_FILL).left();
+                if (!player.hasInfiniteMaterials()) {
+                    to.set(DataComponentRegistry.POTION_JAR, result);
+                    var fullJars = fill / fromData.fill();
+                    from.shrink(fullJars);
+                    var emptied = new ItemStack(BlockRegistry.POTION_JAR.asItem(), fullJars);
+                    if (player.containerMenu.getCarried().isEmpty()) {
+                        player.containerMenu.setCarried(emptied);
+                    } else {
+                        player.addItem(emptied);
+                    }
+                    var remaining = fill - fullJars * fromData.fill();
+                    if (remaining > 0) {
+                        var reduced = new ItemStack(BlockRegistry.POTION_JAR.asItem());
+                        reduced.set(DataComponentRegistry.POTION_JAR, fromData.withFill(fromData.fill() - remaining));
+                        from.shrink(1);
+                        if (player.containerMenu.getCarried().isEmpty()) {
+                            player.containerMenu.setCarried(reduced);
+                        } else {
+                            player.addItem(reduced);
+                        }
+                    }
+                }
+
+                event.setCanceled(true);
+            }
+            case FillBottle -> {
+                if (to.getCount() != 1) {
+                    return;
+                }
+
+                var fromData = from.getOrDefault(DataComponentRegistry.POTION_JAR, new PotionJarData(0, PotionContents.EMPTY, false));
+                if (fromData.contents().potion().isEmpty() || fromData.fill() <= 100) {
+                    return;
+                }
+
+                ItemStack potionStack = new ItemStack(Items.POTION);
+                PotionContents contents = fromData.contents();
+                potionStack.set(DataComponents.POTION_CONTENTS, new PotionContents(contents.potion(), contents.customColor(), contents.customEffects()));
+
+                if (!player.hasInfiniteMaterials()) {
+                    from.set(DataComponentRegistry.POTION_JAR, fromData.withFill(fromData.fill() - 100));
+                    to.shrink(1);
+                    if (player.containerMenu.getCarried().isEmpty()) {
+                        player.containerMenu.setCarried(potionStack);
+                    } else {
+                        player.addItem(potionStack);
+                    }
+                }
+
+                event.setCanceled(true);
+            }
+            case TipArrow -> {
+                if (to.getCount() != 1) {
+                    return;
+                }
+
+                var fromData = from.getOrDefault(DataComponentRegistry.POTION_JAR, new PotionJarData(0, PotionContents.EMPTY, false));
+                if (fromData.contents().potion().isEmpty() || fromData.fill() <= 10) {
+                    return;
+                }
+
+                ItemStack tippedArrow = new ItemStack(Items.TIPPED_ARROW);
+                PotionContents contents = fromData.contents();
+                tippedArrow.set(DataComponents.POTION_CONTENTS, new PotionContents(contents.potion(), contents.customColor(), contents.customEffects()));
+
+                if (!player.hasInfiniteMaterials()) {
+                    from.set(DataComponentRegistry.POTION_JAR, fromData.withFill(fromData.fill() - 10));
+                    to.shrink(1);
+                    if (player.containerMenu.getCarried().isEmpty()) {
+                        player.containerMenu.setCarried(tippedArrow);
+                    } else {
+                        player.addItem(tippedArrow);
+                    }
+                }
+
+                event.setCanceled(true);
+            }
+            case null, default -> {}
+        }
     }
 }
