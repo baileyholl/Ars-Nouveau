d8ba7b1b3d1f242494169db8efa215346dc560d8
Qther <qther@tuta.io>
2025-02-23 04:45:03 +0800

    feat: directional apparatus (#1658)
    * feat: directional apparatus
    
    * directional pedestals
    
    * proper highlighting
    
    * default pedestals to face up
    
    * update json blockstates
    
    * format json blockstates

--- src/main/java/com/hollingsworth/arsnouveau/common/block/ArcanePlatform.java
+++ src/main/java/com/hollingsworth/arsnouveau/common/block/ArcanePlatform.java
@@ -2,20 +2,15 @@ package com.hollingsworth.arsnouveau.common.block;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
-import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Mirror;
-import net.minecraft.world.level.block.Rotation;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.phys.shapes.BooleanOp;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
 
-import javax.annotation.Nullable;
 import java.util.stream.Stream;
 
 public class ArcanePlatform extends ArcanePedestal{
@@ -23,26 +18,6 @@ public class ArcanePlatform extends ArcanePedestal{
     public ArcanePlatform() {
         super();
         registerDefaultState(defaultBlockState().setValue(BlockStateProperties.WATERLOGGED, false).setValue(BlockStateProperties.FACING, Direction.NORTH));
-    }
-
-    @Nullable
-    @Override
-    public BlockState getStateForPlacement(BlockPlaceContext context) {
-        return this.defaultBlockState().setValue(BlockStateProperties.FACING, context.getClickedFace());
-    }
-
-    @Override
-    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
-        super.createBlockStateDefinition(builder);
-        builder.add(BlockStateProperties.FACING);
-    }
-
-    public BlockState rotate(BlockState state, Rotation rot) {
-        return state.setValue(BlockStateProperties.FACING, rot.rotate(state.getValue(BlockStateProperties.FACING)));
-    }
-
-    public BlockState mirror(BlockState state, Mirror mirrorIn) {
-        return state.rotate(mirrorIn.getRotation(state.getValue(BlockStateProperties.FACING)));
     }
 
     public static final VoxelShape UP = Stream.of(
