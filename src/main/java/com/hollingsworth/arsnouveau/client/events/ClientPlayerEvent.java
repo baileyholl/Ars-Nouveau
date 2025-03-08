8868d5d3ef131e7d5b6d7da8cc71ac7058a9c2c5
Bailey Hollingsworth <baileyholl@gmail.com>
2025-03-08 14:40:21 -0600

    Readd rightClickBlock behavior

--- src/main/java/com/hollingsworth/arsnouveau/client/events/ClientPlayerEvent.java
+++ src/main/java/com/hollingsworth/arsnouveau/client/events/ClientPlayerEvent.java
@@ -2,6 +2,7 @@ package com.hollingsworth.arsnouveau.client.events;
 
 import com.hollingsworth.arsnouveau.ArsNouveau;
 import com.hollingsworth.arsnouveau.client.documentation.DocDataLoader;
+import com.hollingsworth.arsnouveau.common.block.ScribesBlock;
 import com.hollingsworth.arsnouveau.common.items.SpellBook;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.player.Player;
@@ -13,6 +14,17 @@ import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
 
 @EventBusSubscriber(modid = ArsNouveau.MODID)
 public class ClientPlayerEvent {
+
+    @SubscribeEvent
+    public static void onBlock(final PlayerInteractEvent.RightClickBlock event) {
+        Player entity = event.getEntity();
+        if (!event.getLevel().isClientSide || event.getHand() != InteractionHand.MAIN_HAND || event.getLevel().getBlockState(event.getPos()).getBlock() instanceof ScribesBlock)
+            return;
+        if (entity.getItemInHand(event.getHand()).getItem() instanceof SpellBook) {
+            event.setCanceled(true);
+        }
+    }
+
     @SubscribeEvent
     public static void onItem(final PlayerInteractEvent.RightClickItem event) {
         Player entity = event.getEntity();
