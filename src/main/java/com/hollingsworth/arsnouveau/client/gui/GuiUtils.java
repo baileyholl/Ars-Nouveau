83c91d6245ff06e37222120ac52615cde59da76d
Qther <qther@tuta.io>
2025-03-11 21:53:13 +0800

    fix: dont direct to web wiki

--- src/main/java/com/hollingsworth/arsnouveau/client/gui/GuiUtils.java
+++ src/main/java/com/hollingsworth/arsnouveau/client/gui/GuiUtils.java
@@ -1,18 +1,9 @@
 package com.hollingsworth.arsnouveau.client.gui;
 
-import com.hollingsworth.arsnouveau.ArsNouveau;
 import com.hollingsworth.arsnouveau.api.documentation.DocClientUtils;
-import net.minecraft.ChatFormatting;
-import net.minecraft.Util;
 import net.minecraft.client.gui.components.AbstractWidget;
-import net.minecraft.network.chat.ClickEvent;
-import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.Style;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.player.Player;
-
-import java.net.URI;
-import java.net.URISyntaxException;
 
 public class GuiUtils {
 
@@ -25,16 +16,10 @@ public class GuiUtils {
     }
 
     public static void openWiki(Player player) {
-        if (player instanceof ServerPlayer serverPlayer) {
+        if (player instanceof ServerPlayer) {
             return;
         }
 
         DocClientUtils.openBook();
-
-        try {
-            Util.getPlatform().openUri(new URI("https://www.arsnouveau.wiki/"));
-        } catch (URISyntaxException e) {
-            throw new RuntimeException(e);
-        }
     }
 }
