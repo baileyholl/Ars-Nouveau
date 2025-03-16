81560036872b6323dc39f70739473332356fbd85
Qther <qther@tuta.io>
2025-03-16 11:25:09 +0800

    readd onBlock (although im not sure it does anything)

--- src/main/java/com/hollingsworth/arsnouveau/client/events/ClientPlayerEvent.java
+++ src/main/java/com/hollingsworth/arsnouveau/client/events/ClientPlayerEvent.java
@@ -2,15 +2,32 @@ package com.hollingsworth.arsnouveau.client.events;
 
 import com.hollingsworth.arsnouveau.ArsNouveau;
 import com.hollingsworth.arsnouveau.client.documentation.DocDataLoader;
+import com.hollingsworth.arsnouveau.common.block.ScribesBlock;
+import com.hollingsworth.arsnouveau.common.items.SpellBook;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.entity.player.Player;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.event.ClientPlayerNetworkEvent;
+import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
 
 
 @EventBusSubscriber(modid = ArsNouveau.MODID)
 public class ClientPlayerEvent {
+
+    @SubscribeEvent
+    public static void onBlock(final PlayerInteractEvent.RightClickBlock event) {
+        Player entity = event.getEntity();
+        if (!event.getLevel().isClientSide || event.getHand() != InteractionHand.MAIN_HAND || event.getLevel().getBlockState(event.getPos()).getBlock() instanceof ScribesBlock)
+            return;
+        if (entity.getItemInHand(event.getHand()).getItem() instanceof SpellBook) {
+            event.setCanceled(true);
+        }
+    }
+
     @SubscribeEvent
     public static void playerLogout(ClientPlayerNetworkEvent.LoggingOut e) {
         DocDataLoader.writeBookmarks();
     }
+
 }
