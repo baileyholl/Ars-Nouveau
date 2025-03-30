20f4159003a0fac2f565d64ab14a70d22819ac66
Qther <qther@tuta.io>
2025-03-30 14:30:54 +0800

    always allow extraction from null side

--- src/main/java/com/hollingsworth/arsnouveau/setup/registry/CapabilityRegistry.java
+++ src/main/java/com/hollingsworth/arsnouveau/setup/registry/CapabilityRegistry.java
@@ -78,7 +78,7 @@ public class CapabilityRegistry {
         event.registerBlockEntity(MAP_INV_CAP, BlockRegistry.REPOSITORY_CONTROLLER_TILE.get(), (c, side) -> c.getControllerInv());
         event.registerBlockEntity(FILTERSET_CAPABILITY, BlockRegistry.REPOSITORY_CONTROLLER_TILE.get(), (c, side) -> c);
         event.registerBlockEntity(MAP_INV_CAP, BlockRegistry.REPOSITORY_TILE.get(), (c, side) -> c);
-        event.registerBlockEntity(Capabilities.ItemHandler.BLOCK, BlockRegistry.IMBUEMENT_TILE.get(), (c, side) -> new ImbuementTile.ItemHandler(c));
+        event.registerBlockEntity(Capabilities.ItemHandler.BLOCK, BlockRegistry.IMBUEMENT_TILE.get(), ImbuementTile.ItemHandler::new);
 
         var sourceContainers = List.of(BlockRegistry.SOURCE_JAR_TILE, BlockRegistry.CREATIVE_SOURCE_JAR_TILE,
                 BlockRegistry.AGRONOMIC_SOURCELINK_TILE, BlockRegistry.ALCHEMICAL_TILE, BlockRegistry.VITALIC_TILE, BlockRegistry.MYCELIAL_TILE, BlockRegistry.VOLCANIC_TILE,
