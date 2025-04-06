56ca7d84c2cb20fe0304f11bf542e5c8789cc817
Klisz <kennercat@gmail.com>
2025-04-05 21:44:47 -0600

    [1.20] Fix EntityMixin
    The 1.20 version of EntityMixin was crashing on startup due to trying to modify the return value of a void method, so I just backported the 1.21 equivalent

--- src/main/java/com/hollingsworth/arsnouveau/common/mixin/EntityMixin.java
+++ src/main/java/com/hollingsworth/arsnouveau/common/mixin/EntityMixin.java
@@ -6,6 +6,8 @@ import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Entity.class)
 public class EntityMixin {
@@ -18,12 +20,12 @@ public class EntityMixin {
         return original;
     }
 
-    @ModifyReturnValue(method = "setSecondsOnFire", at = @At("RETURN"))
-    private int ars_nouveau$setSecondsOnFire(int original) {
-        if (((Entity) (Object) this) instanceof LivingEntity livingEntity && livingEntity.hasEffect(ModPotions.SOAKED_EFFECT.get())) {
-            return 0;
+    @Inject(method = "setRemainingFireTicks", at = @At("HEAD"), cancellable = true)
+    private void ars_nouveau$setSecondsOnFire(int pRemainingFireTicks, CallbackInfo ci) {
+        if (((Entity) (Object) this) instanceof LivingEntity livingEntity && livingEntity.hasEffect(ModPotions.SOAKED_EFFECT.get()) && pRemainingFireTicks > 0) {
+            livingEntity.clearFire();
+            ci.cancel();
         }
-        return original;
     }
 
 }
