4658ce03fbdea484703fce0083984df12cace8c8
Qther <qther@tuta.io>
2025-04-21 16:39:35 +0800

    fix: scribing with table foot can cause duplication

--- src/main/java/com/hollingsworth/arsnouveau/common/block/ScribesBlock.java
+++ src/main/java/com/hollingsworth/arsnouveau/common/block/ScribesBlock.java
@@ -45,10 +45,13 @@ public class ScribesBlock extends TableBlock {
         }
 
         if (state.getValue(ScribesBlock.PART) != ThreePartBlock.HEAD) {
-            BlockEntity tileEntity = world.getBlockEntity(pos.relative(ScribesBlock.getConnectedDirection(state)));
-            tile = tileEntity instanceof ScribesTile ? (ScribesTile) tileEntity : null;
-            if (tile == null)
-                return ItemInteractionResult.PASS_TO_DEFAULT_BLOCK_INTERACTION;
+            BlockPos headPos = pos.relative(ScribesBlock.getConnectedDirection(state));
+            if (world.getBlockEntity(headPos) instanceof ScribesTile head) {
+                BlockState headState = head.getBlockState();
+                return headState.useItemOn(heldStack, world, player, handIn, hit.withPosition(headPos));
+            }
+
+            return ItemInteractionResult.PASS_TO_DEFAULT_BLOCK_INTERACTION;
         }
 
         if (!player.isShiftKeyDown()) {
@@ -127,7 +130,7 @@ public class ScribesBlock extends TableBlock {
             if(stack.getItem() instanceof DominionWand){
                 return;
             }
-            BlockRegistry.SCRIBES_BLOCK.get().useItemOn(stack, world.getBlockState(pos), world, pos, event.getEntity(), event.getHand(), null);
+            BlockRegistry.SCRIBES_BLOCK.get().useItemOn(stack, world.getBlockState(pos), world, pos, event.getEntity(), event.getHand(), event.getHitVec());
             event.setCanceled(true);
         }
     }
