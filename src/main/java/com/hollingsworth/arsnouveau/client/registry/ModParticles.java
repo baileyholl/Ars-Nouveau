b3a68ae7b1dc28fb89dee8887a8371ab33e3f858
Bailey Hollingsworth <baileyholl@gmail.com>
2025-04-22 18:25:21 -0500

    Add bubble type

--- src/main/java/com/hollingsworth/arsnouveau/client/registry/ModParticles.java
+++ src/main/java/com/hollingsworth/arsnouveau/client/registry/ModParticles.java
@@ -4,7 +4,6 @@ import com.hollingsworth.arsnouveau.api.particle.PropertyParticleType;
 import com.hollingsworth.arsnouveau.api.particle.configurations.properties.ParticleTypeProperty;
 import com.hollingsworth.arsnouveau.client.particle.*;
 import net.minecraft.core.particles.ParticleType;
-import net.minecraft.core.particles.SimpleParticleType;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
@@ -26,8 +25,9 @@ public class ModParticles {
     public static final DeferredHolder<ParticleType<?>, ParticleType<ColoredDynamicTypeData>> SPARKLE_TYPE = PARTICLES.register("sparkle", SparkleParticleType::new);
     public static final DeferredHolder<ParticleType<?>, ParticleType<HelixParticleTypeData>> HELIX_TYPE = PARTICLES.register(HelixParticleData.NAME, HelixParticleType::new);
 
-    public static final DeferredHolder<ParticleType<?>, SimpleParticleType> BUBBLE_TYPE = PARTICLES.register("bubble", () -> new SimpleParticleType(false));
-    public static final DeferredHolder<ParticleType<?>, SimpleParticleType> CUSTOM_TYPE = PARTICLES.register("custom", () -> new SimpleParticleType(false));
+    public static final DeferredHolder<ParticleType<?>, PropertyParticleType> ALAKARK_BUBBLE_TYPE = PARTICLES.register("bubble", PropertyParticleType::new);
+    public static final DeferredHolder<ParticleType<?>, PropertyParticleType> BUBBLE_CLONE_TYPE = PARTICLES.register("an_bubble", PropertyParticleType::new);
+    public static final DeferredHolder<ParticleType<?>, PropertyParticleType> CUSTOM_TYPE = PARTICLES.register("custom", PropertyParticleType::new);
     public static final DeferredHolder<ParticleType<?>, PropertyParticleType> SMOKE_TYPE = PARTICLES.register("smoke", PropertyParticleType::new);
     public static final DeferredHolder<ParticleType<?>, PropertyParticleType> SNOW_TYPE = PARTICLES.register("snow", PropertyParticleType::new);
     public static final DeferredHolder<ParticleType<?>, PropertyParticleType> NEW_GLOW_TYPE = PARTICLES.register("new_glow", PropertyParticleType::new);
@@ -39,14 +39,17 @@ public class ModParticles {
         evt.registerSpriteSet(LINE_TYPE.get(), LineParticleProvider::new);
         evt.registerSpriteSet(SPARKLE_TYPE.get(), SparkleParticleProvider::new);
         evt.registerSpriteSet(HELIX_TYPE.get(), HelixParticleData::new);
-        evt.registerSpriteSet(BUBBLE_TYPE.get(), BubbleParticle.Provider::new);
+        evt.registerSpriteSet(ALAKARK_BUBBLE_TYPE.get(), BubbleParticle.Provider::new);
         evt.registerSpriteSet(CUSTOM_TYPE.get(), CustomParticle.Provider::new);
         evt.registerSpriteSet(SMOKE_TYPE.get(), SmokeParticle.Provider::new);
         evt.registerSpriteSet(SNOW_TYPE.get(), SnowParticle.Provider::new);
         evt.registerSpriteSet(NEW_GLOW_TYPE.get(), NewGlowParticleProvider::new);
+        evt.registerSpriteSet(BUBBLE_CLONE_TYPE.get(), ANBubbleParticle.Provider::new);
 
+        ParticleTypeProperty.addType(new ParticleTypeProperty.ParticleData(BUBBLE_CLONE_TYPE.get(), false));
         ParticleTypeProperty.addType(new ParticleTypeProperty.ParticleData(SMOKE_TYPE.get(), false));
         ParticleTypeProperty.addType(new ParticleTypeProperty.ParticleData(SNOW_TYPE.get(), false));
         ParticleTypeProperty.addType(new ParticleTypeProperty.ParticleData(NEW_GLOW_TYPE.get(), true));
+        ParticleTypeProperty.addType(new ParticleTypeProperty.ParticleData(CUSTOM_TYPE.get(), true));
     }
 }
