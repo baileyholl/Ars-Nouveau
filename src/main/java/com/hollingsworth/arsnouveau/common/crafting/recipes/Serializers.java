236bca5647b88ade293950d9d5dbbc6a15cb7fc9
Bailey Hollingsworth <baileyholl@gmail.com>
2025-06-27 16:34:44 -0500

    Readd serializers for ars elemental

---
+++ src/main/java/com/hollingsworth/arsnouveau/common/crafting/recipes/Serializers.java
@@ -0,0 +1,14 @@
+package com.hollingsworth.arsnouveau.common.crafting.recipes;
+
+import net.minecraft.network.RegistryFriendlyByteBuf;
+import net.minecraft.network.codec.ByteBufCodecs;
+import net.minecraft.network.codec.StreamCodec;
+import net.minecraft.world.item.crafting.Ingredient;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Serializers {
+    @Deprecated(forRemoval = true)
+    public static final StreamCodec<RegistryFriendlyByteBuf, List<Ingredient>> INGREDIENT_LIST_STREAM = Ingredient.CONTENTS_STREAM_CODEC.apply(ByteBufCodecs.collection(ArrayList::new));
+}
