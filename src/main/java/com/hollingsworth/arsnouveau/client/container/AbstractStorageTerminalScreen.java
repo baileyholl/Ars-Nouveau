dedfe3b8195e704c8ecceaeb511132524c1c9975
Qther <qther@tuta.io>
2026-02-15 07:00:35 +0800

    feat: bundle interactions with lectern menu (#2030)
    * feat: bundle interactions with lectern menu
    
    * add sounds
    
    * change control scheme
    
    * change controls again
    
    * remove accidental line
    
    ---------
    
    Co-authored-by: Bailey Hollingsworth <baileyholl@gmail.com>

--- src/main/java/com/hollingsworth/arsnouveau/client/container/AbstractStorageTerminalScreen.java
+++ src/main/java/com/hollingsworth/arsnouveau/client/container/AbstractStorageTerminalScreen.java
@@ -493,14 +493,11 @@ public abstract class AbstractStorageTerminalScreen<T extends StorageTerminalMen
         if (slotIDUnderMouse > -1) {
             SlotStorage slot = getMenu().getSlotByID(slotIDUnderMouse);
             if (isPullOne(mouseButton)) {
-                if (slot.stack() != null && slot.stack().getQuantity() > 0) {
-                    storageSlotClick(slot.stack(), PULL_ONE, isTransferOne(mouseButton));
-                    return true;
-                }
+                storageSlotClick(slot.stack(), PULL_ONE, isTransferOne(mouseButton));
                 return true;
             } else if (pullHalf(mouseButton)) {
                 if (!menu.getCarried().isEmpty()) {
-                    storageSlotClick(null, hasControlDown() ? GET_QUARTER : GET_HALF, false);
+                    storageSlotClick(slot.stack(), hasControlDown() ? GET_QUARTER : GET_HALF, false);
                 } else {
                     if (slot.stack() != null && slot.stack().getQuantity() > 0) {
                         storageSlotClick(slot.stack(), hasControlDown() ? GET_QUARTER : GET_HALF, false);
