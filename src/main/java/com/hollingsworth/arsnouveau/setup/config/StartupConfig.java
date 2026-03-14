4a5f018830b80a6a5c2a974fcb157c067fd5ef94
Bailey Hollingsworth <baileyholl@gmail.com>
2026-03-14 13:43:59 -0500

    feat: add config to disable shady villager

--- src/main/java/com/hollingsworth/arsnouveau/setup/config/StartupConfig.java
+++ src/main/java/com/hollingsworth/arsnouveau/setup/config/StartupConfig.java
@@ -6,12 +6,14 @@ public class StartupConfig {
 
     public static ModConfigSpec STARTUP_CONFIG;
     public static ModConfigSpec.IntValue MANA_REGEN_POTION;
+    public static ModConfigSpec.BooleanValue SHADY_VILLAGER_ENABLED;
 
 
     static {
         ModConfigSpec.Builder SERVER_BUILDER = new ModConfigSpec.Builder();
         SERVER_BUILDER.comment("Mana").push("mana");
         MANA_REGEN_POTION = SERVER_BUILDER.comment("Regen bonus per potion level").defineInRange("potionRegen", 10, 0, Integer.MAX_VALUE);
+        SHADY_VILLAGER_ENABLED = SERVER_BUILDER.comment("If shady villagers can be converted by placing an Arcane Core near a villager. Does not remove or disable existing villagers.").define("shadyVillagerEnabled", true);
         SERVER_BUILDER.pop();
 
         STARTUP_CONFIG = SERVER_BUILDER.build();
