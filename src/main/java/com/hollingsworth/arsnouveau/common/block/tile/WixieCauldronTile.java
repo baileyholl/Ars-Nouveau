6f995731ebba176428a54502ee9755f17f0e8b13
xEricL <37921711+xEricL@users.noreply.github.com>
2026-04-25 20:38:12 -0400

    Fix NullPointerException in WixieCauldronTile.java (#2123)
    * Fix NullPointerException in WixieCauldronTile.java
    
    * fix: return if needed potion is null
    
    ---------
    
    Co-authored-by: Bailey Hollingsworth <13970203+baileyholl@users.noreply.github.com>

--- src/main/java/com/hollingsworth/arsnouveau/common/block/tile/WixieCauldronTile.java
+++ src/main/java/com/hollingsworth/arsnouveau/common/block/tile/WixieCauldronTile.java
@@ -166,8 +166,14 @@ public class WixieCauldronTile extends SummoningTile implements ITooltipProvider
         if (!recipeWrapper.isEmpty() && instructions.recipe().recipeIngredients.get(0).getCustomIngredient() instanceof DataComponentIngredient custom) {
             Ingredient itemIngred = instructions.recipe().recipeIngredients.get(1);
             List<ItemStack> needed = new ArrayList<>(Arrays.asList(itemIngred.getItems()));
+            if(needed.isEmpty()){
+                return;
+            }
             PotionContents potionNeeded = custom.getItems().toList().get(0).get(DataComponents.POTION_CONTENTS);
             PotionContents potionOutput = PotionUtil.getContents(instructions.recipe().outputStack);
+            if(potionNeeded == null){
+                return;
+            }
             boolean foundInput = potionNeeded.is(Potions.WATER) || findNeededPotion(potionNeeded, 300, level, worldPosition) != null;
             boolean foundRoomForOutput = findPotionStorage(level, worldPosition, potionOutput) != null;
             if (!foundRoomForOutput || !foundInput) {
