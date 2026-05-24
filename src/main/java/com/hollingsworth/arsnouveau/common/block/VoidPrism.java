61b066a6853d93468567fa62da73723da79527d7
Bailey Hollingsworth <13970203+baileyholl@users.noreply.github.com>
2026-05-24 15:30:26 -0500

    refactor: ease type strictness on IPrismaticBlock

--- src/main/java/com/hollingsworth/arsnouveau/common/block/VoidPrism.java
+++ src/main/java/com/hollingsworth/arsnouveau/common/block/VoidPrism.java
@@ -4,8 +4,9 @@ import com.hollingsworth.arsnouveau.api.block.IPrismaticBlock;
 import com.hollingsworth.arsnouveau.api.util.BlockUtil;
 import com.hollingsworth.arsnouveau.common.entity.EntityProjectileSpell;
 import net.minecraft.core.BlockPos;
-import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.BlockState;
 
 public class VoidPrism extends ModBlock implements IPrismaticBlock {
 
@@ -14,8 +15,8 @@ public class VoidPrism extends ModBlock implements IPrismaticBlock {
     }
 
     @Override
-    public void onHit(ServerLevel world, BlockPos pos, EntityProjectileSpell spell) {
+    public void onHit(Level level, BlockState state, BlockPos pos, EntityProjectileSpell spell) {
         spell.remove(Entity.RemovalReason.DISCARDED);
-        BlockUtil.updateObservers(world, pos);
+        BlockUtil.updateObservers(level, pos);
     }
 }
